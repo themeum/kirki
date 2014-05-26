3cc5fa7eee61e3454c9a3b4aaec4f9e242decaab
Aristeides Stathopoulos <aristath@gmail.com>
2014-05-26 19:28:16 +0300

    Added background control & output functions

---
+++ includes/functions/color-functions.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Sanitize hex colors
+ */
+function kirki_sanitize_hex( $color ) {
+	// Remove any spaces and special characters before and after the string
+	$color = trim( $color. ' \t\n\r\0\x0B' );
+
+	// Remove any trailing '#' symbols from the color value
+	$color = str_replace( '#', '', $color );
+
+	// If there are more than 6 characters, only keep the first 6.
+	if ( strlen( $color ) > 6 ) {
+
+		$color = substr( $color, 0, 6 );
+
+	}
+
+	if ( strlen( $color ) == 6 ) {
+
+		$hex = $color; // If string consists of 6 characters, then this is our color
+
+	} else {
+
+		// String is shorter than 6 characters.
+		// We will have to do some calculations below to get the actual 6-digit hex value.
+
+		// If the string is longer than 3 characters, only keep the first 3.
+		if ( strlen( $color ) > 3 ) {
+			$color = substr( $color, 0, 3 );
+		}
+
+		// If this is a 3-character string, format it to 6 characters.
+		if ( strlen( $color ) == 3 ) {
+
+			$red    = substr( $color, 0, 1 ) . substr( $color, 0, 1 );
+			$green  = substr( $color, 1, 1 ) . substr( $color, 1, 1 );
+			$blue   = substr( $color, 2, 1 ) . substr( $color, 2, 1 );
+
+			$hex = $red . $green . $blue;
+
+		}
+
+		// If this is shorter than 3 characters, do some voodoo.
+		if ( strlen( $color ) == 2 ) {
+
+			$hex = $color . $color . $color;
+
+		} else if ( strlen( $color ) == 1 ) {
+
+			$hex = $color . $color . $color . $color . $color . $color;
+
+		}
+
+	}
+
+	return '#' . $hex;
+}
+
+/*
+ * Gets the rgb value of the $hex color.
+ * Returns an array.
+ */
+function kirki_get_rgb( $hex, $implode = false ) {
+	// Remove any trailing '#' symbols from the color value
+	$hex = str_replace( '#', '', kirki_sanitize_hex( $hex ) );
+
+	if ( strlen( $hex ) == 3 ) {
+		// If the color is entered using a short, 3-character format,
+		// then find the rgb values from them
+		$red    = hexdec( substr( $hex, 0, 1 ) . substr( $hex, 0, 1 ) );
+		$green  = hexdec( substr( $hex, 1, 1 ) . substr( $hex, 1, 1 ) );
+		$blue   = hexdec( substr( $hex, 2, 1 ) . substr( $hex, 2, 1 ) );
+	} else {
+		// If the color is entered using a 6-character format,
+		// then find the rgb values from them
+		$red    = hexdec( substr( $hex, 0, 2 ) );
+		$green  = hexdec( substr( $hex, 2, 2 ) );
+		$blue   = hexdec( substr( $hex, 4, 2 ) );
+	}
+
+	// rgb is an array
+	$rgb = array( $red, $green, $blue );
+	if ( $implode ) {
+		return implode( ',', $rgb );
+	} else {
+		return $rgb;
+	}
+}
+
+/*
+ * Gets the rgba value of a color.
+ */
+function kirki_get_rgba( $hex = '#fff', $opacity = 100 ) {
+	$hex = kirki_sanitize_hex( $hex );
+	// Make sure that opacity is properly formatted :
+	// Set the opacity to 100 if a larger value has been entered by mistake.
+	// If a negative value is used, then set to 0.
+	// If an opacity value is entered in a decimal form (for example 0.25), then multiply by 100.
+	if ( $opacity >= 100 ) {
+
+		$opacity = 100;
+
+	} elseif ( $opacity < 0 ) {
+
+		$opacity = 0;
+
+	} elseif ( $opacity < 1 && $opacity != 0 ) {
+
+		$opacity = ( $opacity * 100 );
+
+	} else {
+
+		$opacity = $opacity;
+
+	}
+
+	// Divide the opacity by 100 to end-up with a CSS value for the opacity
+	$opacity = ( $opacity / 100 );
+
+	$color = 'rgba(' . kirki_get_rgb( $hex, true ) . ', ' . $opacity . ')';
+
+	return $color;
+
+}
--- kirki.php
+++ kirki.php
@@ -29,6 +29,9 @@ function kirki_customizer_include_files() {
 }
 add_action( 'customize_register', 'kirki_customizer_include_files', 1 );
 
+include_once( dirname( __FILE__ ) . '/includes/functions/color-functions.php' );
+include_once( dirname( __FILE__ ) . '/includes/functions/background-css.php' );
+
 
 /**
  * Build the controls
@@ -40,12 +43,63 @@ function kirki_customizer_controls( $wp_customize ) {
 	if ( isset( $controls ) ) {
 		foreach ( $controls as $control ) {
 
-			// Add settings
-			$wp_customize->add_setting( $control['setting'], array(
-				'default'    => $control['default'],
-				'type'       => 'theme_mod',
-				'capability' => 'edit_theme_options'
-			) );
+			if ( 'background' == $control['type'] ) {
+
+				$wp_customize->add_setting( $control['setting'] . '_color', array(
+					'default'    => $control['default']['color'],
+					'type'       => 'theme_mod',
+					'capability' => 'edit_theme_options'
+				) );
+
+				$wp_customize->add_setting( $control['setting'] . '_image', array(
+					'default'    => $control['default']['image'],
+					'type'       => 'theme_mod',
+					'capability' => 'edit_theme_options'
+				) );
+
+				$wp_customize->add_setting( $control['setting'] . '_repeat', array(
+					'default'    => $control['default']['repeat'],
+					'type'       => 'theme_mod',
+					'capability' => 'edit_theme_options'
+				) );
+
+				$wp_customize->add_setting( $control['setting'] . '_size', array(
+					'default'    => $control['default']['size'],
+					'type'       => 'theme_mod',
+					'capability' => 'edit_theme_options'
+				) );
+
+				$wp_customize->add_setting( $control['setting'] . '_attach', array(
+					'default'    => $control['default']['attach'],
+					'type'       => 'theme_mod',
+					'capability' => 'edit_theme_options'
+				) );
+
+				$wp_customize->add_setting( $control['setting'] . '_position', array(
+					'default'    => $control['default']['position'],
+					'type'       => 'theme_mod',
+					'capability' => 'edit_theme_options'
+				) );
+
+				if ( false != $control['default']['opacity'] ) {
+
+					$wp_customize->add_setting( $control['setting'] . '_opacity', array(
+						'default'    => $control['default']['opacity'],
+						'type'       => 'theme_mod',
+						'capability' => 'edit_theme_options'
+					) );
+
+				}
+			} else {
+
+				// Add settings
+				$wp_customize->add_setting( $control['setting'], array(
+					'default'    => $control['default'],
+					'type'       => 'theme_mod',
+					'capability' => 'edit_theme_options'
+				) );
+
+			}
 
 			// Checkbox controls
 			if ( 'checkbox' == $control['type'] ) {
@@ -59,8 +113,139 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
+
+			// Background Controls
+			} elseif ( 'background' == $control['type'] ) {
+
+				$wp_customize->add_control( new Kirki_Customize_Color_Control( $wp_customize, $control['setting'] . '_color', array(
+						'label'       => isset( $control['label'] ) ? $control['label'] : '',
+						'section'     => $control['section'],
+						'settings'    => $control['setting'] . '_color',
+						'priority'    => $control['priority'],
+						'description' => isset( $control['description'] ) ? $control['description'] : null,
+						'subtitle'    => __( 'Background Color', 'kirki' ),
+						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
+					) )
+				);
+
+				$wp_customize->add_control( new Kirki_Customize_Image_Control( $wp_customize, $control['setting'] . '_image', array(
+						'label'       => null,
+						'section'     => $control['section'],
+						'settings'    => $control['setting'] . '_image',
+						'priority'    => $control['priority'] + 1,
+						'description' => null,
+						'subtitle'    => __( 'Background Image', 'kirki' ),
+						'separator'   => false,
+						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
+					) )
+				);
+
+				$wp_customize->add_control( new Kirki_Select_Control( $wp_customize, $control['setting'] . '_repeat', array(
+						'label'       => null,
+						'section'     => $control['section'],
+						'settings'    => $control['setting'] . '_repeat',
+						'priority'    => $control['priority'] + 2,
+						'choices'     => array(
+							'no-repeat' => __( 'No Repeat', 'kirki' ),
+							'repeat'    => __( 'Repeat All', 'kirki' ),
+							'repeat-x'  => __( 'Repeat Horizontally', 'kirki' ),
+							'repeat-y'  => __( 'Repeat Vertically', 'kirki' ),
+							'inherit'   => __( 'Inherit', 'kirki' )
+						),
+						'description' => null,
+						'subtitle'    => __( 'Background Repeat', 'kirki' ),
+						'separator'   => false,
+						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
+					) )
+				);
+
+				$wp_customize->add_control( new Kirki_Customize_Radio_Control( $wp_customize, $control['setting'] . '_size', array(
+						'label'       => null,
+						'section'     => $control['section'],
+						'settings'    => $control['setting'] . '_size',
+						'priority'    => $control['priority'] + 3,
+						'choices'     => array(
+							'inherit' => __( 'Inherit', 'kirki' ),
+							'cover'   => __( 'Cover', 'kirki' ),
+							'contain' => __( 'Contain', 'kirki' ),
+						),
+						'description' => null,
+						'mode'        => 'buttonset',
+						'subtitle'    => __( 'Background Size', 'kirki' ),
+						'separator'   => false,
+						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
+					) )
+				);
+
+				$wp_customize->add_control( new Kirki_Customize_Radio_Control( $wp_customize, $control['setting'] . '_attach', array(
+						'label'       => null,
+						'section'     => $control['section'],
+						'settings'    => $control['setting'] . '_attach',
+						'priority'    => $control['priority'] + 4,
+						'choices'     => array(
+							'inherit' => __( 'Inherit', 'kirki' ),
+							'fixed'   => __( 'Fixed', 'kirki' ),
+							'scroll'  => __( 'Scroll', 'kirki' ),
+						),
+						'description' => null,
+						'mode'        => 'buttonset',
+						'subtitle'    => __( 'Background Attachment', 'kirki' ),
+						'separator'   => false,
+						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
+					) )
+				);
+
+				$wp_customize->add_control( new Kirki_Select_Control( $wp_customize, $control['setting'] . '_position', array(
+						'label'       => null,
+						'section'     => $control['section'],
+						'settings'    => $control['setting'] . '_position',
+						'priority'    => $control['priority'] + 5,
+						'choices'     => array(
+							'left-top'      => __( 'Left Top', 'kirki' ),
+							'left-center'   => __( 'Left Center', 'kirki' ),
+							'left-bottom'   => __( 'Left Bottom', 'kirki' ),
+							'right-top'     => __( 'Right Top', 'kirki' ),
+							'right-center'  => __( 'Right Center', 'kirki' ),
+							'right-bottom'  => __( 'Right Bottom', 'kirki' ),
+							'center-top'    => __( 'Center Top', 'kirki' ),
+							'center-center' => __( 'Center Center', 'kirki' ),
+							'center-bottom' => __( 'Center Bottom', 'kirki' ),
+						),
+						'description' => null,
+						'subtitle'    => __( 'Background Position', 'kirki' ),
+						'separator'   => ( false != $control['default']['opacity'] ) ? false : true,
+						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
+					) )
+				);
+
+				if ( false != $control['default']['opacity'] ) {
+					$wp_customize->add_control( new Kirki_Customize_Sliderui_Control( $wp_customize, $control['setting'] . '_opacity', array(
+							'label'       => null,
+							'section'     => $control['section'],
+							'settings'    => $control['setting'] . '_opacity',
+							'priority'    => $control['priority'] + 6,
+							'choices'  => array(
+								'min'  => 0,
+								'max'  => 100,
+								'step' => 1,
+							),
+							'description' => null,
+							'subtitle'    => __( 'Background Opacity', 'kirki' ),
+							'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
+							'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+							// 'transport'   => 'postMessage',
+						) )
+					);
+				}
 
 			// Color Controls
 			} elseif ( 'color' == $control['type'] ) {
@@ -74,6 +259,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -89,6 +275,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -106,6 +293,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -122,6 +310,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -138,6 +327,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -153,6 +343,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -168,6 +359,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -184,6 +376,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -198,6 +391,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'description' => isset( $control['description'] ) ? $control['description'] : null,
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
+						// 'transport'   => 'postMessage',
 					) )
 				);
 
@@ -214,6 +408,7 @@ function kirki_customizer_controls( $wp_customize ) {
 						'subtitle'    => isset( $control['subtitle'] ) ? $control['subtitle'] : '',
 						'separator'   => isset( $control['separator'] ) ? $control['separator'] : false,
 						'required'    => isset( $control['required'] ) ? $control['required'] : array(),
+						// 'transport'   => 'postMessage',
 					) )
 				);
 			}
