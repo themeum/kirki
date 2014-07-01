50f0e7ffb0f75fb8fd60812d636fb6c6510c6ddd
Ciaran Whelan <cmwwebfx@gmail.com>
2014-07-01 22:37:23 +0930

    Updated for input:checked
    The problem with the Kirki is that once you make changes and save these changes the required does not stick if you refresh the page. We have added the input:checked from line 46 onward.

--- includes/functions/required.php
+++ includes/functions/required.php
@@ -17,21 +17,54 @@ function kirki_required_script() {
 
 					<script>
 						jQuery(document).ready(function($) {
-							<?php if ( isset( $id ) && isset( $value ) && $value == intval( get_theme_mod( $id ) ) ) : ?>
-								$("#customize-control-<?php echo $control['setting']; ?>").fadeIn(300);
-							<?php elseif ( isset( $id ) && isset( $value ) && $value != intval( get_theme_mod( $id ) ) ) : ?>
-								$("#customize-control-<?php echo $control['setting']; ?>").fadeOut(300);
-							<?php endif; ?>
+						
 
 							$( "#input_<?php echo $id; ?> input" ).each(function(){
 								$(this).click(function(){
-									if ( $(this).val() == <?php echo $value; ?> ) {
+									if ( $(this).val() == "<?php echo $value; ?>" ) {
 										$("#customize-control-<?php echo $control['setting']; ?>").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_color").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_image").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_repeat").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_size").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_position").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_attach").fadeIn(300);
 									} else {
 										$("#customize-control-<?php echo $control['setting']; ?>").fadeOut(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_color").fadeOut(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_image").fadeOut(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_repeat").fadeOut(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_size").fadeOut(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_position").fadeOut(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_attach").fadeOut(300);
 									}
+								
 								});
-							});
+								
+							
+							})	;
+							//alert ($('#input_<?php echo $id; ?> input:checked').val());
+							if ( $('#input_<?php echo $id; ?> input:checked').val() == "<?php echo $value; ?>" ) {
+								
+										$("#customize-control-<?php echo $control['setting']; ?>").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_color").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_image").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_repeat").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_size").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_position").fadeIn(300);
+										$("#customize-control-<?php echo $control['setting']; ?>_attach").fadeIn(300);
+
+									} else {
+                                 //alert ("failure");								
+								$("#customize-control-<?php echo $control['setting']; ?>").fadeOut(300);
+								$("#customize-control-<?php echo $control['setting']; ?>_color").fadeOut(300);
+								$("#customize-control-<?php echo $control['setting']; ?>_image").fadeOut(300);
+								$("#customize-control-<?php echo $control['setting']; ?>_repeat").fadeOut(300);
+								$("#customize-control-<?php echo $control['setting']; ?>_size").fadeOut(300);
+								$("#customize-control-<?php echo $control['setting']; ?>_position").fadeOut(300);
+								$("#customize-control-<?php echo $control['setting']; ?>_attach").fadeOut(300);
+									}
+								
 						});
 					</script>
 					<?php
