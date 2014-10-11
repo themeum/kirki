7edfa9d7d24c36ea357bd57213e935cca7a3ae6a
Dimitris Kalliris <kalliris.d@gmail.com>
2014-10-11 11:49:41 +0300

    Update required.php
    using jQuery Attribute Equals Selector (#24)

--- includes/functions/required.php
+++ includes/functions/required.php
@@ -18,23 +18,24 @@ function kirki_required_script() {
 					<script>
 						jQuery(document).ready(function($) {
 							<?php if ( isset( $id ) && isset( $value ) && $value == intval( get_theme_mod( $id ) ) ) : ?>
-								$("#customize-control-<?php echo $control['setting']; ?>").fadeIn(300);
+								$( 'id="customize-control-<?php echo $control['setting']; ?>"' ).fadeIn(300);
+								$( 'id="customize-control-<?php echo $control['setting']; ?>"' ).fadeIn(300);
 							<?php elseif ( isset( $id ) && isset( $value ) && $value != intval( get_theme_mod( $id ) ) ) : ?>
-								$("#customize-control-<?php echo $control['setting']; ?>").fadeOut(300);
+								$( 'id="customize-control-<?php echo $control['setting']; ?>"' ).fadeOut(300);
 							<?php endif; ?>
 
 							$( "#input_<?php echo $id; ?> input" ).each(function(){
 								$(this).click(function(){
 									if ( $(this).val() == "<?php echo $value; ?>" ) {
-										$("#customize-control-<?php echo $control['setting']; ?>").fadeIn(300);
+										$( 'id="customize-control-<?php echo $control['setting']; ?>"' ).fadeIn(300);
 									} else {
-										$("#customize-control-<?php echo $control['setting']; ?>").fadeOut(300);
+										$( 'id="customize-control-<?php echo $control['setting']; ?>"' ).fadeOut(300);
 									}
 								});
 								if ( $(this).val() == "<?php echo $value; ?>" ) {
-										$("#customize-control-<?php echo $control['setting']; ?>").fadeIn(300);
+										$( 'id="customize-control-<?php echo $control['setting']; ?>"' ).fadeIn(300);
 									} else {
-										$("#customize-control-<?php echo $control['setting']; ?>").fadeOut(300);
+										$( 'id="customize-control-<?php echo $control['setting']; ?>"' ).fadeOut(300);
 									}
 							});
 						});
