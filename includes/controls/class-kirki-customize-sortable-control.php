85635568151fe64bc9c830059fc4339cf1d7bf91
Aristeides Stathopoulos <aristath@gmail.com>
2015-02-16 19:41:11 +0200

    working, needs cleaning-up, popups etc.

--- includes/controls/class-kirki-customize-sortable-control.php
+++ includes/controls/class-kirki-customize-sortable-control.php
@@ -3,31 +3,34 @@
 class Kirki_Customize_Sortable_Control extends WP_Customize_Control {
 	public $description;
 	public $choices;
-	public $visible_btn;
-	public static $firstLoad;
+	public $visible_button;
 
 	public function enqueue() {
 		wp_enqueue_script( 'jquery-ui-core' );
 		wp_enqueue_script( 'jquery-ui-sortable' );
 	}
 
+
 	public function render_content() {
+		global $kirki;
+		$kirki->scripts->sortable_script();
+
 		if ( ! is_array( $this->choices ) ) {
 			return;
 		}
 		if ( ! count( $this->choices ) ) {
 			return;
 		}
+
 		?>
 		<label class='kirki-sortable'>
 			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
 			<?php
-				// $values = $this->value();
-				// $values = empty( $values ) ? array_keys( $this->choices ) : $values;
-				$values = array_keys( $this->choices );
+				$values = $this->value();
+				$values = $values == '' ? array_keys( $this->choices ) : $values;
 				$values = is_serialized( $values ) ? unserialize( $values ) : $values;
-				$this->visible_btn = count( $values ) != count( $this->choices ) ? true : $this->visible_btn;
-				$visibleButton = ( $this->visible_btn === true ) ? '<i class="dashicons dashicons-visibility visibility"></i>' : '';
+				$this->visible_button = count( $values ) != count( $this->choices ) ? true : '';
+				$visibleButton = ( $this->visible_button === true ) ? "<i class='dashicons dashicons-visibility visibility'></i>" : '';
 			?>
 			<ul>
 				<?php foreach ( $values as $dummy => $value ) : ?>
@@ -40,11 +43,9 @@ class Kirki_Customize_Sortable_Control extends WP_Customize_Control {
 			</ul>
 			<div style='clear: both'></div>
 			<?php $values = ! is_serialized( $values ) ? serialize( $values ) : $values; ?>
-			<input type='hidden' <?php $this->link(); ?> value="<?php echo esc_attr( $values )  ?>"/>
+			<input type='hidden' <?php $this->link(); ?> value='<?php echo esc_attr( $values )  ?>'/>
 		</label>
 		<p class='description'><?php echo $this->description; ?></p>
 		<?php
-
 	}
-
 }
