cb0641bc4ce83a3faa49601fc61e6ba20acf8946
Aristeides Stathopoulos <aristath@gmail.com>
2015-02-24 13:32:32 +0200

    1st pass

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -35,6 +35,42 @@ jQuery(document).ready(function($) {
 			kirkiUpdateSortable( $(this).parents('ul:eq(0)') );
 		})
 	});
+
+
+	// Switch Click
+	$('.Switch').click(function() {
+		// Check If Enabled (Has 'On' Class)
+		if ($(this).hasClass('On')){
+			// Try To Find Checkbox Within Parent Div, And Check It
+			$(this).parent().find('input:checkbox').attr('checked', true);
+			// Change Button Style - Remove On Class, Add Off Class
+			$(this).removeClass('On').addClass('Off');
+		} else { // If Button Is Disabled (Has 'Off' Class)
+			// Try To Find Checkbox Within Parent Div, And Uncheck It
+			$(this).parent().find('input:checkbox').attr('checked', false);
+			// Change Button Style - Remove Off Class, Add On Class
+			$(this).removeClass('Off').addClass('On');
+		}
+	});
+
+	// Loops Through Each Toggle Switch On Page
+	$('.Switch').each(function() {
+		// Search of a checkbox within the parent
+		if ($(this).parent().find('input:checkbox').length){
+			// This just hides all Toggle Switch Checkboxs
+			// Uncomment line below to hide all checkbox's after Toggle Switch is Found
+			$(this).parent().find('input:checkbox').hide();
+			// Look at the checkbox's checkked state
+			if ($(this).parent().find('input:checkbox').is(':checked')){
+				// Checkbox is not checked, Remove the 'On' Class and Add the 'Off' Class
+				$(this).removeClass('On').addClass('Off');
+			} else {
+				// Checkbox Is Checked Remove 'Off' Class, and Add the 'On' Class
+				$(this).removeClass('Off').addClass('On');
+			}
+		}
+	});
+
 });
 function kirkiUpdateSortable(ul) {
 	"use strict";
--- includes/controls/class-kirki-customize-checkbox-control.php
+++ includes/controls/class-kirki-customize-checkbox-control.php
@@ -4,13 +4,22 @@ class Kirki_Customize_Checkbox_Control extends Kirki_Customize_Control {
 
 	public function __construct( $manager, $id, $args = array() ) {
 		$this->type = 'checkbox';
+		$this->mode = ( ! isset( $this->mode ) || empty( $this->mode ) ) ? 'checkbox' : $this->mode;
 		parent::__construct( $manager, $id, $args );
 	}
 
 	public function render_content() { ?>
+		<?php $display = 'switch' == $this->mode ? ' style="display: none;"' : ''; ?>
 		<label class="customizer-checkbox">
-			<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" id="<?php echo $this->id . esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
+			<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" id="<?php echo $this->id . esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?><?php echo $display; ?> />
 			<strong><?php echo esc_html( $this->label ); ?></strong>
+			<?php if ( 'switch' == $this->mode ) : ?>
+				<div class="Switch Off">
+					<div class="Toggle"></div>
+					<span class="On"><?php _e( 'ON', 'kirki' ); ?></span>
+					<span class="Off"><?php _e( 'OFF', 'kirki' ); ?></span>
+				</div>
+			<?php endif; ?>
 			<?php $this->description(); ?>
 			<?php $this->subtitle(); ?>
 		</label>
