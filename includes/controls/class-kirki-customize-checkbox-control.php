fd101a99c6aef650b600851026a317162faa8f5a
Aristeides Stathopoulos <aristath@gmail.com>
2015-02-24 14:04:11 +0200

    switches now work

--- includes/controls/class-kirki-customize-checkbox-control.php
+++ includes/controls/class-kirki-customize-checkbox-control.php
@@ -11,10 +11,12 @@ class Kirki_Customize_Checkbox_Control extends Kirki_Customize_Control {
 	public function render_content() { ?>
 		<?php $display = 'switch' == $this->mode ? ' style="display: none;"' : ''; ?>
 		<label class="customizer-checkbox">
+			<?php if ( 'switch' == $this->mode ) : ?><div class="info"><?php endif; ?>
 			<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" id="<?php echo $this->id . esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?><?php echo $display; ?> />
+			<?php if ( 'switch' == $this->mode ) : ?></div><?php endif; ?>
 			<strong><?php echo esc_html( $this->label ); ?></strong>
 			<?php if ( 'switch' == $this->mode ) : ?>
-				<div class="Switch Off">
+				<div class="Switch <?php if ( esc_attr( $this->value() ) ) : ?>On<?php else : ?>Off<?php endif; ?>" style="float: right;">
 					<div class="Toggle"></div>
 					<span class="On"><?php _e( 'ON', 'kirki' ); ?></span>
 					<span class="Off"><?php _e( 'OFF', 'kirki' ); ?></span>
