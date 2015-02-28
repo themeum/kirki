4533a58f2a2fae83bee44f5bdcf96909ed6f0bae
Aristeides Stathopoulos <aristath@gmail.com>
2015-02-28 20:31:46 +0200

    Revert "Fix required setting."
    This reverts commit 0cc3cc440e90637ec6d0424c5f4f3342aecaced7.

--- includes/required.php
+++ includes/required.php
@@ -19,24 +19,24 @@ function kirki_required_script() {
 						jQuery(document).ready(function($) {
 							<?php if ( isset( $id ) && isset( $value ) ) : ?>
 							 	<?php if ( $value == get_theme_mod( $id ) ) : ?>
-									$( '[id="customize-control-<?php echo $control['setting']; ?>_opacity"]' ).fadeIn(300);
+									$( '[id="customize-control-<?php echo $control['setting']; ?>"]' ).fadeIn(300);
 								<?php else : ?>
-									$( '[id="customize-control-<?php echo $control['setting']; ?>_opacity"]' ).fadeOut(300);
+									$( '[id="customize-control-<?php echo $control['setting']; ?>"]' ).fadeOut(300);
 								<?php endif; ?>
 							<?php endif; ?>
 
 							$( "#input_<?php echo $id; ?> input" ).each(function(){
 								$(this).click(function(){
 									if ( $(this).val() == "<?php echo $value; ?>" ) {
-										$( '[id="customize-control-<?php echo $control['setting']; ?>_opacity"]' ).fadeIn(300);
+										$( '[id="customize-control-<?php echo $control['setting']; ?>"]' ).fadeIn(300);
 									} else {
-										$( '[id="customize-control-<?php echo $control['setting']; ?>_opacity"]' ).fadeOut(300);
+										$( '[id="customize-control-<?php echo $control['setting']; ?>"]' ).fadeOut(300);
 									}
 								});
 								if ( $(this).val() == "<?php echo $value; ?>" ) {
-										$( '[id="customize-control-<?php echo $control['setting']; ?>_opacity"]' ).fadeIn(300);
+										$( '[id="customize-control-<?php echo $control['setting']; ?>"]' ).fadeIn(300);
 									} else {
-										$( '[id="customize-control-<?php echo $control['setting']; ?>_opacity"]' ).fadeOut(300);
+										$( '[id="customize-control-<?php echo $control['setting']; ?>"]' ).fadeOut(300);
 									}
 							});
 						});
