88e4e0fe05826ebfab7652f471d36a23ec7fd01b
Aristeides Stathopoulos <aristath@gmail.com>
2015-03-13 19:37:38 +0200

    fixes #75

--- includes/controls/class-kirki-customize-switch-control.php
+++ includes/controls/class-kirki-customize-switch-control.php
@@ -10,7 +10,7 @@ class Kirki_Customize_Switch_Control extends WP_Customize_Control {
 	protected function render_content() { ?>
 		<label>
 			<div class="switch-info">
-				<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
+				<input style="display: none;" type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
 			</div>
 			<?php echo esc_html( $this->label ); ?>
 			<?php if ( ! empty( $this->description ) ) : ?>
@@ -19,6 +19,8 @@ class Kirki_Customize_Switch_Control extends WP_Customize_Control {
 			<?php $classes = ( esc_attr( $this->value() ) ) ? ' On' : ' Off'; ?>
 			<div class="Switch <?php echo $classes; ?>">
 				<div class="Toggle"></div>
+				<span class="On"><?php _e( 'ON', 'kirki' ); ?></span>
+				<span class="Off"><?php _e( 'OFF', 'kirki' ); ?></span>
 			</div>
 		</label>
 		<?php
