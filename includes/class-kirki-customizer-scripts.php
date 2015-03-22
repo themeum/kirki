aa61eac4a5b1b62e2cd3f4d1535d2160220bb392
Aristeides Stathopoulos <aristath@gmail.com>
2015-03-22 02:59:38 +0200

    adding required script

--- includes/class-kirki-customizer-scripts.php
+++ includes/class-kirki-customizer-scripts.php
@@ -5,6 +5,8 @@ class Kirki_Customizer_Scripts {
 	function __construct() {
 		add_action( 'customize_controls_print_scripts', array( $this, 'custom_js' ), 999 );
 		add_action( 'customize_controls_enqueue_scripts', array( $this, 'customizer_scripts' ) );
+		add_action( 'customize_controls_print_footer_scripts', array( $this, 'required_script' ) );
+
 	}
 
 	/**
@@ -41,6 +43,59 @@ class Kirki_Customizer_Scripts {
 		wp_enqueue_script( 'serialize-js', $kirki_url . 'assets/js/serialize.js');
 		wp_enqueue_script( 'jquery-ui-core' );
 		wp_enqueue_script( 'jquery-ui-tooltip' );
+
+	}
+
+	/**
+	 * Add the required script.
+	 */
+	function required_script() {
+
+		$controls = kirki_get_controls();
+
+		if ( isset( $controls ) ) {
+
+			foreach ( $controls as $control ) {
+
+				if ( isset( $control['required'] ) && ! is_null( $control['required'] && is_array( $control['required'] ) ) ) {
+
+					foreach ( $control['required'] as $id => $value ) : ?>
+
+						<script>
+							jQuery(document).ready(function($) {
+								<?php if ( isset( $id ) && isset( $value ) ) : ?>
+								 	<?php if ( $value == get_theme_mod( $id ) ) : ?>
+										$( '[id="customize-control-<?php echo $control['settings']; ?>"]' ).fadeIn(300);
+									<?php else : ?>
+										$( '[id="customize-control-<?php echo $control['settings']; ?>"]' ).fadeOut(300);
+									<?php endif; ?>
+								<?php endif; ?>
+
+								$( "#input_<?php echo $id; ?> input" ).each(function(){
+									$(this).click(function(){
+										if ( $(this).val() == "<?php echo $value; ?>" ) {
+											$( '[id="customize-control-<?php echo $control['settings']; ?>"]' ).fadeIn(300);
+										} else {
+											$( '[id="customize-control-<?php echo $control['settings']; ?>"]' ).fadeOut(300);
+										}
+									});
+									if ( $(this).val() == "<?php echo $value; ?>" ) {
+											$( '[id="customize-control-<?php echo $control['settings']; ?>"]' ).fadeIn(300);
+										} else {
+											$( '[id="customize-control-<?php echo $control['settings']; ?>"]' ).fadeOut(300);
+										}
+								});
+							});
+						</script>
+						<?php
+
+					endforeach;
+
+				}
+
+			}
+
+		}
 
 	}
 
