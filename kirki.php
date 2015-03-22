780b479fa05bc0e74ad45949f83c52c9575b052c
Aristeides Stathopoulos <aristath@gmail.com>
2015-03-22 02:42:13 +0200

    cleaning up main file

--- kirki.php
+++ kirki.php
@@ -23,26 +23,8 @@ include_once( KIRKI_PATH . '/includes/deprecated.php' );
 
 include_once( KIRKI_PATH . '/includes/class-kirki-customizer-help-tooltips.php' );
 include_once( KIRKI_PATH . '/includes/class-kirki-customizer-postmessage.php' );
-
-class Kirki {
-
-}
-
-class Kirki_Controls extends Kirki {
-
-}
-
-class Kirki_Settings extends Kirki {
-
-}
-
-class Kirki_Customizer_Styles extends Kirki {
-
-}
-
-class Kirki_Config extends Kirki {
-
-}
+include_once( KIRKI_PATH . '/includes/class-kirki-customizer-styles.php' );
+include_once( KIRKI_PATH . '/includes/class-kirki-customizer-scripts.php' );
 
 /**
  * Build the controls
@@ -1038,23 +1020,6 @@ function kirki_customizer_styles() {
 add_action( 'customize_controls_print_styles', 'kirki_customizer_styles' );
 
 /**
- * Enqueue the scripts required.
- */
-function kirki_customizer_scripts() {
-
-	$config = kirki_get_config();
-
-	$kirki_url = isset( $config['url_path'] ) ? $config['url_path'] : KIRKI_URL;
-
-	wp_enqueue_script( 'kirki_customizer_js', $kirki_url . 'assets/js/customizer.js', array( 'jquery', 'customize-controls' ) );
-	wp_enqueue_script( 'serialize-js', $kirki_url . 'assets/js/serialize.js');
-	wp_enqueue_script( 'jquery-ui-core' );
-	wp_enqueue_script( 'jquery-ui-tooltip' );
-
-}
-add_action( 'customize_controls_enqueue_scripts', 'kirki_customizer_scripts' );
-
-/**
  * Add a dummy, empty stylesheet if no stylesheet_id has been defined and we need one.
  */
 function kirki_frontend_styles() {
@@ -1079,28 +1044,6 @@ $styles_priority = ( isset( $options['styles_priority'] ) ) ? $styles_priority :
 add_action( 'wp_enqueue_scripts', 'kirki_frontend_styles', $styles_priority );
 
 /**
- * If we've specified an image to be used as logo,
- * replace the default theme description with a div that will include our logo.
- */
-function kirki_customizer_custom_js() {
-
-	$options = apply_filters( 'kirki/config', array() ); ?>
-
-	<?php if ( isset( $options['logo_image'] ) || isset( $options['description'] ) ) : ?>
-		<script>jQuery(document).ready(function($) { "use strict";
-			<?php if ( isset( $options['logo_image'] ) ) : ?>
-				$( 'div#customize-info .preview-notice' ).replaceWith( '<img src="<?php echo $options['logo_image']; ?>">' );
-			<?php endif; ?>
-			<?php if ( isset( $options['description'] ) ) : ?>
-				$( 'div#customize-info .accordion-section-content' ).replaceWith( '<div class="accordion-section-content"><div class="theme-description"><?php echo $options['description']; ?></div></div>' );
-			<?php endif; ?>
-		});</script>
-	<?php endif;
-
-}
-add_action( 'customize_controls_print_scripts', 'kirki_customizer_custom_js', 999 );
-
-/**
  * Get the admin color theme
  */
 function kirki_get_admin_colors() {
@@ -1120,176 +1063,3 @@ function kirki_get_admin_colors() {
 	return $color;
 
 }
-
-/**
- * Add custom CSS rules to the head, applying our custom styles
- */
-function kirki_customizer_custom_css() {
-
-	global $kirki;
-
-	$color   = kirki_get_admin_colors();
-	$options = kirki_get_config();
-
-	$color_font    = false;
-	$color_active  = isset( $options['color_active'] )  ? $options['color_active']  : $color['colors'][3];
-	$color_light   = isset( $options['color_light'] )   ? $options['color_light']   : $color['colors'][2];
-	$color_select  = isset( $options['color_select'] )  ? $options['color_select']  : $color['colors'][3];
-	$color_accent  = isset( $options['color_accent'] )  ? $options['color_accent']  : $color['icon_colors']['focus'];
-	$color_back    = isset( $options['color_back'] )    ? $options['color_back']    : false;
-
-	if ( $color_back ) {
-		$color_font = ( 170 > kirki_get_brightness( $color_back ) ) ? '#f2f2f2' : '#222';
-	}
-
-	?>
-
-	<style>
-	.wp-core-ui .button.tooltip {
-		background: <?php echo $color_select; ?>;
-		color: #fff;
-	}
-
-	.image.ui-buttonset label.ui-button.ui-state-active {
-		background: <?php echo $color_select; ?>;
-	}
-
-	<?php if ( $color_back ) : ?>
-
-		.wp-full-overlay-sidebar,
-		#customize-info .accordion-section-title,
-		#customize-info .accordion-section-title:hover,
-		#customize-theme-controls .accordion-section-title,
-		#customize-theme-controls .control-section .accordion-section-title {
-			background: <?php echo $color_back; ?>;
-			<?php if ( $color_font ) : ?>color: <?php echo $color_font; ?>;<?php endif; ?>
-		}
-		<?php if ( $color_font ) : ?>
-			#customize-theme-controls .control-section .accordion-section-title:focus,
-			#customize-theme-controls .control-section .accordion-section-title:hover,
-			#customize-theme-controls .control-section.open .accordion-section-title,
-			#customize-theme-controls .control-section:hover .accordion-section-title {
-				color: <?php echo $color_font; ?>;
-			}
-			<?php endif; ?>
-
-		<?php if ( 170 > Kirki_Color::get_brightness( $color_back ) ) : ?>
-			.control-section.control-panel>.accordion-section-title:after {
-				background: #111;
-				color: #f5f5f5;
-				border-left: 1px solid #000;
-			}
-			#customize-theme-controls .control-section.control-panel>h3.accordion-section-title:focus:after,
-			#customize-theme-controls .control-section.control-panel>h3.accordion-section-title:hover:after {
-				background: #222;
-				color: #fff;
-				border: 1px solid #222;
-			}
-
-			.control-panel-back,
-			.customize-controls-close {
-				background: #111 !important;
-				border-right: 1px solid #111 !important;
-			}
-			.control-panel-back:before,
-			.control-panel-back:after,
-			.customize-controls-close:before,
-			.customize-controls-close:after {
-				color: #f2f2f2 !important;
-			}
-			.control-panel-back:focus:before,
-			.control-panel-back:hover:before,
-			.customize-controls-close:focus:before,
-			.customize-controls-close:hover:before {
-				background: #000;
-				color: #fff;
-			}
-			#customize-header-actions {
-				border-bottom: 1px solid #111;
-			}
-		<?php endif; ?>
-
-	<?php endif; ?>
-
-	.ui-state-default,
-	.ui-widget-content .ui-state-default,
-	.ui-widget-header .ui-state-default,
-	.ui-state-active.ui-button.ui-widget.ui-state-default {
-		background-color: <?php echo $color_active; ?>;
-		border: 1px solid rgba(0,0,0,.05);
-	}
-
-	.ui-button.ui-widget.ui-state-default {
-		background-color: #f2f2f2;
-	}
-
-	#customize-theme-controls .accordion-section-title {
-		border-bottom: 1px solid rgba(0,0,0,.1);
-	}
-
-	#customize-theme-controls .control-section .accordion-section-title:focus,
-	#customize-theme-controls .control-section .accordion-section-title:hover,
-	#customize-theme-controls .control-section.open .accordion-section-title,
-	#customize-theme-controls .control-section:hover .accordion-section-title {
-		background: <?php echo $color_active; ?>;
-	}
-	ul.ui-sortable li.kirki-sortable-item {
-		border: 1px solid <?php echo $color_active; ?>;
-	}
-
-	.Switch span.On,
-	ul.ui-sortable li.kirki-sortable-item .visibility {
-		color: <?php echo $color_active; ?>;
-	}
-
-	#customize-theme-controls .control-section.control-panel.current-panel:hover .accordion-section-title{
-		background: none;
-	}
-
-	.Switch.Round.On .Toggle,
-	#customize-theme-controls .control-section.control-panel.current-panel .accordion-section-title:hover{
-		background: <?php echo $color_active; ?>;
-	}
-
-	.wp-core-ui .button-primary {
-		background: <?php echo $color_active; ?>;
-	}
-
-	.wp-core-ui .button-primary.focus,
-	.wp-core-ui .button-primary.hover,
-	.wp-core-ui .button-primary:focus,
-	.wp-core-ui .button-primary:hover {
-		background: <?php echo $color_select; ?>;
-	}
-
-	.wp-core-ui .button-primary-disabled,
-	.wp-core-ui .button-primary.disabled,
-	.wp-core-ui .button-primary:disabled,
-	.wp-core-ui .button-primary[disabled] {
-		background: <?php echo $color_light; ?> !important;
-		color: <?php echo $color_select; ?> !important;
-	}
-
-	.customize-control input[type="text"]:focus {
-		border-color: <?php echo $color_active; ?>;
-	}
-
-	.wp-core-ui.wp-customizer .button,
-	.press-this.wp-core-ui .button,
-	.press-this input#publish,
-	.press-this input#save-post,
-	.press-this a.preview {
-		background-color: <?php echo $color_accent; ?>;
-	}
-
-	.wp-core-ui.wp-customizer .button:hover,
-	.press-this.wp-core-ui .button:hover,
-	.press-this input#publish:hover,
-	.press-this input#save-post:hover,
-	.press-this a.preview:hover {
-		background-color: <?php echo $color_accent; ?>;
-	}
-	</style>
-	<?php
-}
-add_action( 'customize_controls_print_styles', 'kirki_customizer_custom_css', 999 );
