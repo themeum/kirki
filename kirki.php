97e9d1ed162e786a0a5256f5e22c763c0cbd1b9d
Aristeides Stathopoulos <aristath@gmail.com>
2015-03-22 03:53:08 +0200

    adding Kirki and instantiating it

--- kirki.php
+++ kirki.php
@@ -19,15 +19,38 @@ if ( ! defined( 'KIRKI_URL' ) ) {
 include_once( KIRKI_PATH . '/includes/libraries/class-kirki-fonts.php' );
 include_once( KIRKI_PATH . '/includes/libraries/class-kirki-color.php' );
 include_once( KIRKI_PATH . '/includes/libraries/class-kirki-colourlovers.php' );
+
 include_once( KIRKI_PATH . '/includes/deprecated.php' );
 include_once( KIRKI_PATH . '/includes/controls.php' );
 include_once( KIRKI_PATH . '/includes/settings.php' );
 include_once( KIRKI_PATH . '/includes/google-fonts.php' );
 include_once( KIRKI_PATH . '/includes/sanitize.php' );
 include_once( KIRKI_PATH . '/includes/style.php' );
-include_once( KIRKI_PATH . '/includes/config.php' );
 
+include_once( KIRKI_PATH . '/includes/class-kirki-config.php' );
 include_once( KIRKI_PATH . '/includes/class-kirki-customizer-help-tooltips.php' );
 include_once( KIRKI_PATH . '/includes/class-kirki-customizer-postmessage.php' );
 include_once( KIRKI_PATH . '/includes/class-kirki-customizer-styles.php' );
 include_once( KIRKI_PATH . '/includes/class-kirki-customizer-scripts.php' );
+
+
+class Kirki {
+
+	public $config;
+	public $customizer_scripts;
+	public $customizer_styles;
+	public $scripts;
+	public $styles;
+
+}
+
+$kirki = new Kirki();
+$kirki->config = new Kirki_Config();
+$kirki->customizer_scripts = new Kirki_Customizer_Scripts();
+$kirki->customizer_styles = new Kirki_Customizer_Styles();
+$kirki->styles = array(
+	$styles = new Kirki_Styles(),
+);
+$kirki->scripts = array(
+	$google_fonts = new Kirki_Google_Fonts_Script(),
+);
