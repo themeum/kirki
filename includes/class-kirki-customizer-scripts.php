20dc78cdc27d259190e4ea182f7f51aee176fe9c
Aristeides Stathopoulos <aristath@gmail.com>
2015-03-22 02:41:23 +0200

    more customizer scripts

--- includes/class-kirki-customizer-scripts.php
+++ includes/class-kirki-customizer-scripts.php
@@ -4,6 +4,7 @@ class Kirki_Customizer_Scripts {
 
 	function __construct() {
 		add_action( 'customize_controls_print_scripts', array( $this, 'custom_js' ), 999 );
+		add_action( 'customize_controls_enqueue_scripts', array( $this, 'customizer_scripts' ) );
 	}
 
 	/**
@@ -24,6 +25,22 @@ class Kirki_Customizer_Scripts {
 				<?php endif; ?>
 			});</script>
 		<?php endif;
+
+	}
+
+	/**
+	 * Enqueue the scripts required.
+	 */
+	function customizer_scripts() {
+
+		$config = kirki_get_config();
+
+		$kirki_url = isset( $config['url_path'] ) ? $config['url_path'] : KIRKI_URL;
+
+		wp_enqueue_script( 'kirki_customizer_js', $kirki_url . 'assets/js/customizer.js', array( 'jquery', 'customize-controls' ) );
+		wp_enqueue_script( 'serialize-js', $kirki_url . 'assets/js/serialize.js');
+		wp_enqueue_script( 'jquery-ui-core' );
+		wp_enqueue_script( 'jquery-ui-tooltip' );
 
 	}
 
