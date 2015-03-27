015f07c3378f6c5512f63197d8453d0b72193dbc
Aristeides Stathopoulos <aristath@gmail.com>
2015-03-28 01:43:53 +0200

    Bugfix number control + Underscore.JS template

--- includes/Controls/NumberControl.php
+++ includes/Controls/NumberControl.php
@@ -6,19 +6,19 @@ class NumberControl extends \WP_Customize_Control {
 
 	public $type = 'number';
 
-	public function render_content() { ?>
+	public function content_template() { ?>
 
 		<label class="customizer-text">
 			<span class="customize-control-title">
-				<?php echo esc_html( $this->label ); ?>
-				<?php if ( ! empty( $this->description ) ) : ?>
-					<span class="description customize-control-description"><?php echo $this->description; ?></span>
-				<?php endif; ?>
+				<# if ( data.label ) { #>{{ data.label }}<# } #>
+				<# if ( data.description ) { #>
+					<span class="description customize-control-description">{{ data.description }}</span>
+				<# } #>
 			</span>
 			<input type="number" <?php $this->link(); ?> value="<?php echo intval( $this->value() ); ?>"/>
-			<?php if ( isset( $this->description ) && '' != $this->description ) { ?>
-				<a href="#" class="button tooltip hint--left" data-hint="<?php echo strip_tags( esc_html( $this->description ) ); ?>">?</a>
-			<?php } ?>
+			<# if ( data.help ) { #>
+				<a href="#" class="button tooltip hint--left" data-hint="{{ data.help }}">?</a>
+			<# } #>
 		</label>
 		<?php
 	}
--- includes/class-kirki-setting.php
+++ includes/class-kirki-setting.php
@@ -102,7 +102,7 @@ class Kirki_Setting {
 			} elseif ( 'upload' == $control['type'] ) {
 				$sanitize_callback = 'esc_url_raw';
 			} elseif ( 'number' == $control['type'] ) {
-				$sanitize_callback = 'intval';
+				$sanitize_callback = 'kirki_sanitize_number';
 			} elseif ( 'multicheck' == $control['type'] ) {
 				$sanitize_callback = 'esc_attr';
 			} else {
