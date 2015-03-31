04492075d19ba4d0546bd7fa37384249f224c353
Aristeides Stathopoulos <aristath@gmail.com>
2015-04-01 01:32:37 +0300

    bugfixes

--- includes/Styles/Frontend.php
+++ includes/Styles/Frontend.php
@@ -22,11 +22,10 @@ class Frontend {
 	 * Add a dummy, empty stylesheet if no stylesheet_id has been defined and we need one.
 	 */
 	function frontend_styles() {
-        $config   = Kirki::config();
+        $config = Kirki::config()->get_all();
 		$fields = Kirki::controls()->get_all();
 
-        $kirki_url = $config->get('url_path', KIRKI_URL);
-        $kirki_stylesheet = $config->getOrThrow('stylesheet_id');
+        $kirki_stylesheet = Kirki::config()->getOrThrow( 'stylesheet_id' );
 
 		foreach( $fields as $field ) {
 			if ( isset( $field['output'] ) ) {
@@ -34,8 +33,10 @@ class Frontend {
 			}
 		}
 
+		$root = ( isset( $config['url_path'] ) ) ? $config['url_path'] : KIRKI_URL;
+
 		if ( isset( $uses_output )  && $uses_output && $kirki_stylesheet === 'kirki-styles' ) {
-			wp_enqueue_style( 'kirki-styles', $kirki_url . 'assets/css/kirki-styles.css', NULL, NULL );
+			wp_enqueue_style( 'kirki-styles', trailingslashit( $root ) . 'assets/css/kirki-styles.css', NULL, NULL );
 		}
 
 	}
