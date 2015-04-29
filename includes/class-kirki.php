84ff7acd102a2afe241164df6181a3b7f9f44e28
Aristeides Stathopoulos <aristath@gmail.com>
2015-04-29 14:07:55 +0300

    add static methods to the Kirki class

--- includes/class-kirki.php
+++ includes/class-kirki.php
@@ -15,9 +15,9 @@ class Kirki {
 	public $config  = null;
 	public $fonts   = null;
 	public $scripts = null;
-	public $fields  = null;
 	public $builder = null;
 
+	public $fields  = null;
 	/**
 	 * Access the single instance of this class
 	 * @return Kirki
@@ -63,11 +63,41 @@ class Kirki {
 	}
 
 	/**
+	 * Adds configution for an instance
+	 */
+	public static function add_config( $instance_id = '', $config = array() ) {
+		self::$instances[$instance_id] = ( isset( self::$instances[$instance_id] ) ) ? self::$instances[$instance_id] : array();
+		self::$instances[$instance_id][$config] = $config;
+	}
+
+	/**
+	 * Adds panel to an instance
+	 */
+	public static function add_panel( $instance_id = '', $panel = array() ) {
+		self::$instances[$instance_id] = ( isset( self::$instances[$instance_id] ) ) ? self::$instances[$instance_id] : array();
+		self::$instances[$instance_id][$panels] = $panel;
+	}
+
+	/**
+	 * Adds a section to an instance
+	 */
+	public static function add_section( $instance_id = '', $section = array() ) {
+		self::$instances[$instance_id] = ( isset( self::$instances[$instance_id] ) ) ? self::$instances[$instance_id] : array();
+		self::$instances[$instance_id][$sections] = $section;
+	}
+
+	/**
+	 * Adds a field to an instance
+	 */
+	public static function add_field( $instance_id = '', $field = array() ) {
+		self::$instances[$instance_id] = ( isset( self::$instances[$instance_id] ) ) ? self::$instances[$instance_id] : array();
+		self::$instances[$instance_id][$fields] = $field;
+	}
+
+	/**
 	 * Constructor is private, should only be called by get_instance()
 	 */
 	private function __construct() {
-
-
 	}
 
 }
