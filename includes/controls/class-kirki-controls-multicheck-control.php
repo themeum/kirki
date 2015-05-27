96143b0309d085c19e8a2ff4035377c269f72088
Aristeides Stathopoulos <aristath@gmail.com>
2015-05-27 21:42:32 +0300

    fix checkbox CSS URL.

--- includes/controls/class-kirki-controls-multicheck-control.php
+++ includes/controls/class-kirki-controls-multicheck-control.php
@@ -14,7 +14,7 @@ class Kirki_Controls_MultiCheck_Control extends WP_Customize_Control {
 		$root_url = ( '' != $config['url_path'] ) ? esc_url_raw( $config['url_path'] ) : KIRKI_URL;
 
 		wp_enqueue_script( 'kirki-multicheck', trailingslashit( $root_url ) . 'assets/js/kirki-multicheck.js', array( 'jquery' ) );
-		wp_enqueue_style( 'kirki-multicheck', trailingslashit( $root_url ) . 'assets/css/multicheck.css' );
+		wp_enqueue_style( 'kirki-multicheck', trailingslashit( $root_url ) . 'assets/css/checkbox.css' );
 
     }
 
