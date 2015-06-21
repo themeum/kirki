45dc6ec9d9c6ce9202c00904e89cc25ed34fb0ef
Aristeides Stathopoulos <aristath@gmail.com>
2015-06-22 00:22:13 +0300

    more tests + minor bugfixes

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -124,25 +124,25 @@ if ( ! function_exists( 'kirki_get_option' ) ) {
 
 if ( ! function_exists( 'kirki_sanitize_hex' ) ) {
 	function kirki_sanitize_hex( $color ) {
-		Kirki_Color::sanitize_hex( $color );
+		return Kirki_Color::sanitize_hex( $color );
 	}
 }
 
 if ( ! function_exists( 'kirki_get_rgb' ) ) {
 	function kirki_get_rgb( $hex, $implode = false ) {
-		Kirki_Color::get_rgb( $hex, $implode );
+		return Kirki_Color::get_rgb( $hex, $implode );
 	}
 }
 
 if ( ! function_exists( 'kirki_get_rgba' ) ) {
 	function kirki_get_rgba( $hex = '#fff', $opacity = 100 ) {
-		Kirki_Color::get_rgba( $hex, $opacity );
+		return Kirki_Color::get_rgba( $hex, $opacity );
 	}
 }
 
 if ( ! function_exists( 'kirki_get_brightness' ) ) {
 	function kirki_get_brightness( $hex ) {
-		Kirki_Color::get_brightness( $hex );
+		return Kirki_Color::get_brightness( $hex );
 	}
 }
 
