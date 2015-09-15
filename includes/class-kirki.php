864781eb187fd59c38054aa4ac91e67cc85093bb
Aristeides Stathopoulos <aristath@gmail.com>
2015-09-15 15:56:29 +0300

    JS template for color-alpha control. See #348

--- includes/class-kirki.php
+++ includes/class-kirki.php
@@ -86,6 +86,7 @@ class Kirki {
 		$wp_customize->register_control_type( 'Kirki_Controls_Switch_Control' );
 		$wp_customize->register_control_type( 'Kirki_Controls_Toggle_Control' );
 		$wp_customize->register_control_type( 'Kirki_Controls_Code_Control' );
+		$wp_customize->register_control_type( 'Kirki_Controls_Color_Alpha_Control' );
 	}
 
 	/**
