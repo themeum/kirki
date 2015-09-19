5e2f38a0b2bde0b05bee835a7a70b9447938b2a7
Aristeides Stathopoulos <aristath@gmail.com>
2015-09-19 19:26:41 +0300

    improve styles for select items

--- includes/controls/select/class-kirki-controls-select-control.php
+++ includes/controls/select/class-kirki-controls-select-control.php
@@ -37,8 +37,6 @@ class Kirki_Controls_Select_Control extends WP_Customize_Control {
 	public function enqueue() {
 		wp_enqueue_script( 'selectize', trailingslashit( kirki_url() ) . 'includes/controls/select/selectize.js', array( 'jquery' ) );
 		wp_enqueue_script( 'kirki-select', trailingslashit( kirki_url() ) . 'includes/controls/select/script.js', array( 'jquery', 'selectize' ) );
-		wp_enqueue_style( 'kirki-select', trailingslashit( kirki_url() ) . 'includes/controls/select/selectize.css' );
-		wp_enqueue_style( 'kirki-selectt', trailingslashit( kirki_url() ) . 'includes/controls/select/selectize.default.css' );
 	}
 
 	protected function content_template() { ?>
