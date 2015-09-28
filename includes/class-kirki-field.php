240e95076a0155077867a2eb4400abd849f87e68
Aristeides Stathopoulos <aristath@gmail.com>
2015-09-29 01:34:02 +0300

    Move code from Kirki::add_field to Kirki_Field::add_field

--- includes/class-kirki-field.php
+++ includes/class-kirki-field.php
@@ -23,4 +23,71 @@ class Kirki_Field extends Kirki_Customizer {
 		$control  = new Kirki_Control( $this->args );
 	}
 
+	public static function add_field( $config_id, $args ) {
+
+		if ( is_array( $config_id ) && empty( $args ) ) {
+			$args      = $config_id;
+			$config_id = 'global';
+		}
+
+		$config_id = ( '' == $config_id ) ? 'global' : $config_id;
+
+		/**
+		 * Get the configuration options
+		 */
+		$config = Kirki::$config[ $config_id ];
+
+		/**
+		 * If we've set an option in the configuration
+		 * then make sure we're using options and not theme_mods
+		 */
+		if ( '' != $config['option_name'] ) {
+			$config['option_type'] = 'option';
+		}
+
+		/**
+		 * If no option name has been set for the field,
+		 * use the one from the configuration
+		 */
+		if ( ! isset( $args['option_name'] ) ) {
+			$args['option_name'] = $config['option_name'];
+		}
+
+		/**
+		 * If no capability has been set for the field,
+		 * use the one from the configuration
+		 */
+		if ( ! isset( $args['capability'] ) ) {
+			$args['capability'] = $config['capability'];
+		}
+
+		/**
+		 * Check if [settings] is set.
+		 * If not set, check for [setting]
+		 */
+		if ( ! isset( $args['settings'] ) && isset( $args['setting'] ) ) {
+			$args['settings'] = $args['setting'];
+		}
+
+		/**
+		 * If no option-type has been set for the field,
+		 * use the one from the configuration
+		 */
+		if ( ! isset( $args['option_type'] ) ) {
+			$args['option_type'] = $config['option_type'];
+		}
+
+		/**
+		 * Add the field to the static $fields variable properly indexed
+		 */
+		Kirki::$fields[ Kirki_Field_Sanitize::sanitize_settings( $args ) ] = $args;
+
+		if ( 'background' == $args['type'] ) {
+			/**
+			 * Build the background fields
+			 */
+			Kirki::$fields = Kirki_Explode_Background_Field::process_fields( Kirki::$fields );
+		}
+
+	}
 }
