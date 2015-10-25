c2ccb0ea233d2c2a1d2bbe24e2dc7925fbc183c1
Ignacio Cruz <igmoweb@gmail.com>
2015-10-25 10:25:39 +0100

    Kirki "grunted"

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -1031,51 +1031,82 @@ wp.customize.controlConstructor['slider'] = wp.customize.Control.extend( {
 /**
  * KIRKI CONTROL: SORTABLE
  */
-jQuery(document).ready(function($) {
-	"use strict";
-	// initialize
-	$('.kirki-sortable > ul ~ input').each(function() {
-		var value = $(this).val();
-		try {
-			value = unserialize(value);
-		} catch (err) {
-			return;
-		}
-		var ul = $(this).siblings('ul:eq(0)');
-		ul.find('li').addClass('invisible').find('i.visibility').toggleClass('dashicons-visibility-faint');
-		$.each(value, function(i, val) {
-			ul.find('li[data-value=' + val + ']').removeClass('invisible').find('i.visibility').toggleClass('dashicons-visibility-faint');
-		});
-	});
-	$('.kirki-sortable > ul').each(function() {
-		$(this).sortable()
+wp.customize.controlConstructor['sortable'] = wp.customize.Control.extend({
+	ready: function() {
+		var control = this;
+
+		// The hidden field that keeps the data saved
+		this.settingField = this.container.find('[data-customize-setting-link]').first();
+
+		// The sortable container
+		this.sortableContainer = this.container.find( 'ul.sortable').first();
+
+		// Set the field value for the first time
+		this.setValue( this.setting.get(), false );
+
+
+		// Init the sortable container
+		this.sortableContainer.sortable()
 			.disableSelection()
 			.on("sortstop", function(event, ui) {
-				kirkiUpdateSortable(ui.item.parent());
+				control.sort();
 			})
 			.find('li').each(function() {
-				$(this).find('i.visibility').click(function() {
-					$(this).toggleClass('dashicons-visibility-faint').parents('li:eq(0)').toggleClass('invisible');
+				jQuery(this).find('i.visibility').click(function() {
+					jQuery(this).toggleClass('dashicons-visibility-faint').parents('li:eq(0)').toggleClass('invisible');
 				});
 			})
 			.click(function() {
-				kirkiUpdateSortable($(this).parents('ul:eq(0)'));
-			})
-	});
+				control.sort();
+			});
+	},
+
+	/**
+	 * Updates the sorting list
+	 */
+	sort: function() {
+		var newValue = [];
+		this.sortableContainer.find( 'li' ).each( function() {
+			var $this = jQuery(this);
+			if ( ! $this.is( '.invisible' ) ) {
+				newValue.push( $this.data('value' ) );
+			}
+		});
+
+		this.setValue( newValue, true );
+	},
+
+	/**
+	 * Get the current value of the setting
+	 *
+	 * @return Object
+	 */
+	getValue: function() {
+		// The setting is saved in PHP serialized format
+		return unserialize( this.setting.get() );
+	},
+
+	/**
+	 * Set a new value for the setting
+	 *
+	 * @param newValue Object
+	 * @param refresh If we want to refresh the previewer or not
+	 */
+	setValue: function( newValue, refresh ) {
+		newValue = serialize( newValue );
+		this.setting.set( newValue );
+
+		// Update the hidden field
+		this.settingField.val( newValue );
+
+		if ( refresh ) {
+			// Trigger the change event on the hidden field so
+			// previewer refresh the website on Customizer
+			this.settingField.trigger('change');
+		}
+	}
 
 });
-
-function kirkiUpdateSortable(ul) {
-	"use strict";
-	var $ = jQuery;
-	var values = [];
-	ul.find('li').each(function() {
-		if (!$(this).is('.invisible')) {
-			values.push($(this).attr('data-value'));
-		}
-	});
-	ul.siblings('input').eq(0).val(serialize(values)).trigger('change');
-}
 /**
  * KIRKI CONTROL: SPACING
  */
