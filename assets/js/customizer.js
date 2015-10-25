bfbc658e3b1a6a6a550a28b8f07301f98a6c50fa
Aristeides Stathopoulos <aristath@gmail.com>
2015-10-25 13:26:04 +0200

    migrate scripts for number controls to use jquery-ui-spinner

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -226,14 +226,22 @@ wp.customize.controlConstructor['multicheck'] = wp.customize.Control.extend( {
 /**
  * KIRKI CONTROL: NUMBER
  */
-jQuery(document).ready(function($) {
-	"use strict";
-	$( ".customize-control-number input[type='number']").number();
-});
-
 wp.customize.controlConstructor['number'] = wp.customize.Control.extend( {
 	ready: function() {
 		var control = this;
+		var element = this.container.find( 'input' );
+
+		jQuery( element ).spinner();
+		if ( control.params.choices.min ) {
+			jQuery( element ).spinner( 'option', 'min', control.params.choices.min );
+		}
+		if ( control.params.choices.min ) {
+			jQuery( element ).spinner( 'option', 'max', control.params.choices.max );
+		}
+		if ( control.params.choices.min ) {
+			var control_step = ( 'any' == control.params.choises.step ) ? '0.001' : control.params.choices.step;
+			jQuery( element ).spinner( 'option', 'step', control_step );
+		}
 		this.container.on( 'change', 'input', function() {
 			control.setting.set( jQuery( this ).val() );
 		});
