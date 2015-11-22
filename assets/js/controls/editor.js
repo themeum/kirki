41106070de288e44ece741af7a726bed946a316d
Aristeides Stathopoulos <aristath@gmail.com>
2015-11-22 14:26:01 +0200

    editor control improvements

--- assets/js/controls/editor.js
+++ assets/js/controls/editor.js
@@ -1,47 +1,38 @@
 /**
  * KIRKI CONTROL: EDITOR
  */
-(function($) {
+( function( $ ) {
 	wp.customizerCtrlEditor = {
-
 		init: function() {
-
-			$(window).load(function() {
-
-				$('textarea.wp-editor-area').each(function() {
-					var tArea = $(this),
-						id = tArea.attr('id'),
-						input = $('input[data-customize-setting-link="' + id + '"]'),
-						editor = tinyMCE.get(id),
+			$( window ).load( function() {
+				$( 'textarea.wp-editor-area' ).each( function() {
+					var tArea  = $( this ),
+					    id     = tArea.attr( 'id' ),
+						editor = tinyMCE.get( id ),
 						setChange,
 						content;
 
-					if (editor) {
-						editor.on('change', function(e) {
-							editor.save();
+					if ( editor ) {
+						editor.onChange.add( function(ed, e) {
+							ed.save();
 							content = editor.getContent();
-							clearTimeout(setChange);
-							setChange = setTimeout(function() {
-								input.val(content).trigger('change');
-							}, 500);
+							clearTimeout( setChange );
+							setChange = setTimeout( function() {
+								tArea.val( content ).trigger( 'change' );
+							}, 500 );
 						});
 					}
 
-					tArea.css({
-						visibility: 'visible'
-					}).on('keyup', function() {
+					tArea.css({ visibility: 'visible' }).on('keyup', function() {
 						content = tArea.val();
-						clearTimeout(setChange);
-						setChange = setTimeout(function() {
-							input.val(content).trigger('change');
-						}, 500);
+						clearTimeout( setChange );
+						setChange = setTimeout( function() {
+							content.trigger( 'change' );
+						}, 500 );
 					});
 				});
 			});
 		}
-
 	};
-
 	wp.customizerCtrlEditor.init();
-
-})(jQuery);
+})( jQuery );
