86f369c75b54bd3ba5a1626789bf5766ed4d1a24
Aristeides Stathopoulos <aristath@gmail.com>
2015-12-19 14:08:04 +0200

    fixes #527

--- assets/js/controls/code.js
+++ assets/js/controls/code.js
@@ -7,8 +7,14 @@ wp.customize.controlConstructor['code'] = wp.customize.Control.extend( {
 		var element = control.container.find( '#kirki-codemirror-editor-' + control.id );
 		var editor  = CodeMirror.fromTextArea( element[0] );
 
+		if ( control.params.choices.language == 'html' ) {
+			var language = { name: "htmlmixed" };
+		} else {
+			language = control.params.choices.language;
+		}
+
 		editor.setOption( "value", control.setting._value );
-		editor.setOption( "mode", control.params.choices.language );
+		editor.setOption( "mode", language );
 		editor.setOption( "lineNumbers", true );
 		editor.setOption( "theme", control.params.choices.theme );
 		editor.setOption( "height", control.params.choices.height + 'px' );
