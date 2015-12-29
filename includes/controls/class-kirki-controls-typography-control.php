42340b53b155ca69603d922f25d51f129e3e1423
Andrés Villarreal <andrezrv@gmail.com>
2015-12-29 19:01:04 -0300

    Rollback implementation of font size units.

--- includes/controls/class-kirki-controls-typography-control.php
+++ includes/controls/class-kirki-controls-typography-control.php
@@ -126,17 +126,16 @@ class Kirki_Controls_Typography_Control extends Kirki_Customize_Control {
 				<div class="font-size">
 					<h5>{{ data.l10n['font-size'] }}</h5>
 					<input type="number" min="0" step="any" value="{{ parseFloat( data.value['font-size'] ) }}"/>
-					<# var unit = data.value['unit'] ? data.value['unit'] : data.value['font-size'].replace( parseFloat( data.value['font-size'] ), '' ); #>
-					<# var units = data.choices['font-size']['units']; #>
 					<select>
-						<# if ( units.length ) { #>
-							<# for ( key in units ) { #>
-								<option value="{{ units[ key ] }}" <# if ( unit == units[ key ] ) { #> selected <# } #>>{{ units[ key ] }}</option>
+						<# if ( data.choices['units'] ) { #>
+							<# for ( key in data.choices['units'] ) { #>
+								<option value="{{ data.choices['units'][ key ] }}" <# if ( _.contains( data.value['font-size'], data.choices['units'][ key ] ) ) { #> selected <# } #>>{{ data.choices['units'][ key ] }}</option>
 							<# } #>
 						<# } else { #>
-							<option value="px" <# if ( unit == 'px' ) { #> selected <# } #>>px</option>
-							<option value="em" <# if ( unit == 'em' ) { #> selected <# } #>>em</option>
-							<option value="%" <# if ( unit == '%' ) { #> selected <# } #>>%</option>
+							<# var units = data.value['font-size'].replace( parseFloat( data.value['font-size'] ), '' ); #>
+							<option value="px" <# if ( units == 'px' ) { #> selected <# } #>>px</option>
+							<option value="em" <# if ( units == 'em' ) { #> selected <# } #>>em</option>
+							<option value="%" <# if ( units == '%' ) { #> selected <# } #>>%</option>
 						<# } #>
 					</select>
 				</div>
