38101fbb63b02dc87686f098ef1c2b26c33adf1a
Aristeides Stathopoulos <aristath@gmail.com>
2016-02-17 04:46:22 +0200

    grunt

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -63,6 +63,27 @@ wp.customize.controlConstructor['color-alpha'] = wp.customize.Control.extend( {
 	}
 });
 /**
+ * KIRKI CONTROL: COLOR
+ */
+wp.customize.controlConstructor['kirki-color'] = wp.customize.Control.extend( {
+	ready: function() {
+		var control   = this;
+		var picker    = this.container.find( '.kirki-color-control' );
+		var new_color = picker.val();
+
+		picker.wpColorPicker({
+			change: function( event, ui ) {
+				setTimeout( function(){
+					control.setting.set( picker.val() );
+					if ( undefined !== control.params.js_vars && 0 < control.params.js_vars.length ) {
+						KirkiPostMessage( control.params.js_vars, picker.val() );
+					}
+				}, 100 );
+			},
+		});
+	}
+});
+/**
  * KIRKI CONTROL: DIMENSION
  */
 wp.customize.controlConstructor['dimension'] = wp.customize.Control.extend( {
