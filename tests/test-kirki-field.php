177bd9f227b405bc0b2c232c1a3bc6ceba4f9479
Aristeides Stathopoulos <aristath@gmail.com>
2016-02-19 04:38:26 +0200

    unit tests

--- tests/test-kirki-field.php
+++ tests/test-kirki-field.php
@@ -13,6 +13,10 @@ class Test_Kirki_Field extends WP_UnitTestCase {
 		$this->wp_customize = $GLOBALS['wp_customize'];
 	}
 
+	/**
+	 * Color & color-alpha fields use the same control.
+	 * Their difference is the ['choices']['alpha'] argument.
+	 */
 	public function test_color_field_tweaks() {
 
 		Kirki::add_field( 'global', array(
@@ -28,5 +32,56 @@ class Test_Kirki_Field extends WP_UnitTestCase {
 		) );
 		$this->assertEquals( 'color-alpha', Kirki::$fields['my_setting']['type'] );
 		$this->assertEquals( true, Kirki::$fields['my_setting']['choices']['alpha'] );
+	}
+
+	/**
+	 * Normal select fields should have a "multiple" argument = 1
+	 * multiselect fields control the number of selectable options
+	 * via this argument so we need to make sure it's properly sanitized
+	 */
+	public function test_multiselect_field_tweaks() {
+
+		Kirki::add_field( 'global', array(
+			'setting' => 'my_setting',
+			'type'    => 'select',
+		) );
+		$this->assertEquals( 1, Kirki::$fields['my_setting']['multiple'] );
+
+		Kirki::add_field( 'global', array(
+			'setting'  => 'my_setting',
+			'type'     => 'select',
+			'multiple' => 9,
+		) );
+		$this->assertEquals( 9, Kirki::$fields['my_setting']['multiple'] );
+
+		Kirki::add_field( 'global', array(
+			'setting'  => 'my_setting',
+			'type'     => 'select',
+			'multiple' => 'tralala',
+		) );
+		$this->assertEquals( 0, Kirki::$fields['my_setting']['multiple'] );
+	}
+
+	/**
+	 * If we define the "kirki_config" argument on a field,
+	 * we have to make sure that it overrides the config set in $config_id.
+	 * If the "kirki_config" defined does not exist, then we have to fallback to "global".
+	 */
+	public function test_override_kirki_config() {
+
+		Kirki::add_field( 'global', array(
+			'setting'      => 'my_setting',
+			'type'         => 'text',
+			'kirki_config' => 'tralala',
+		) );
+		$this->assertEquals( 'global', Kirki::$fields['my_setting']['kirki_config'] );
+
+		Kirki::add_config( 'new_config', array() );
+		Kirki::add_field( 'global', array(
+			'setting'      => 'my_setting',
+			'type'         => 'text',
+			'kirki_config' => 'new_config',
+		) );
+		$this->assertEquals( 'new_config', Kirki::$fields['my_setting']['kirki_config'] );
 	}
 }
