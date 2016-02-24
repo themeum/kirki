5152906a5935e3571bbe8463a73e3cc180ecd19b
Aristeides Stathopoulos <aristath@gmail.com>
2016-02-24 05:07:22 +0200

    remove invalid tests

--- tests/test-deprecated.php
+++ tests/test-deprecated.php
@@ -29,31 +29,6 @@ class Test_Kirki_Deprecated extends WP_UnitTestCase {
 			Kirki_Color::get_brightness( '#ffffff' )
 		);
 
-		$this->assertEquals(
-			Kirki_Fonts::get_all_fonts(),
-			Kirki_GoogleFonts::get_all_fonts()
-		);
-
-		$this->assertEquals(
-			Kirki_Fonts::is_google_font( 'foo' ),
-			Kirki_GoogleFonts::is_google_font( 'foo' )
-		);
-
-		$this->assertEquals(
-			Kirki_Fonts::get_google_font_subsets(),
-			Kirki_GoogleFonts::get_google_font_subsets()
-		);
-
-		$this->assertEquals(
-			Kirki_Fonts::get_standard_fonts(),
-			Kirki_GoogleFonts::get_standard_fonts()
-		);
-
-		$this->assertEquals(
-			Kirki_Fonts::get_google_fonts(),
-			Kirki_GoogleFonts::get_google_fonts()
-		);
-
 	}
 
 }
