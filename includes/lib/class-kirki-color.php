8755270946c75b455a60728bec4c53c131ee4c7c
Aristeides Stathopoulos <aristath@gmail.com>
2016-02-26 00:06:42 +0200

    start OOP, refactored sanitizing hex

--- includes/lib/class-kirki-color.php
+++ includes/lib/class-kirki-color.php
@@ -19,186 +19,221 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'Kirki_Color' ) ) {
 	class Kirki_Color {
 
+		private $color;
+
+		private $word_colors = array(
+			'aliceblue'            => 'F0F8FF',
+			'antiquewhite'         => 'FAEBD7',
+			'aqua'                 => '00FFFF',
+			'aquamarine'           => '7FFFD4',
+			'azure'                => 'F0FFFF',
+			'beige'                => 'F5F5DC',
+			'bisque'               => 'FFE4C4',
+			'black'                => '000000',
+			'blanchedalmond'       => 'FFEBCD',
+			'blue'                 => '0000FF',
+			'blueviolet'           => '8A2BE2',
+			'brown'                => 'A52A2A',
+			'burlywood'            => 'DEB887',
+			'cadetblue'            => '5F9EA0',
+			'chartreuse'           => '7FFF00',
+			'chocolate'            => 'D2691E',
+			'coral'                => 'FF7F50',
+			'cornflowerblue'       => '6495ED',
+			'cornsilk'             => 'FFF8DC',
+			'crimson'              => 'DC143C',
+			'cyan'                 => '00FFFF',
+			'darkblue'             => '00008B',
+			'darkcyan'             => '008B8B',
+			'darkgoldenrod'        => 'B8860B',
+			'darkgray'             => 'A9A9A9',
+			'darkgreen'            => '006400',
+			'darkgrey'             => 'A9A9A9',
+			'darkkhaki'            => 'BDB76B',
+			'darkmagenta'          => '8B008B',
+			'darkolivegreen'       => '556B2F',
+			'darkorange'           => 'FF8C00',
+			'darkorchid'           => '9932CC',
+			'darkred'              => '8B0000',
+			'darksalmon'           => 'E9967A',
+			'darkseagreen'         => '8FBC8F',
+			'darkslateblue'        => '483D8B',
+			'darkslategray'        => '2F4F4F',
+			'darkslategrey'        => '2F4F4F',
+			'darkturquoise'        => '00CED1',
+			'darkviolet'           => '9400D3',
+			'deeppink'             => 'FF1493',
+			'deepskyblue'          => '00BFFF',
+			'dimgray'              => '696969',
+			'dimgrey'              => '696969',
+			'dodgerblue'           => '1E90FF',
+			'firebrick'            => 'B22222',
+			'floralwhite'          => 'FFFAF0',
+			'forestgreen'          => '228B22',
+			'fuchsia'              => 'FF00FF',
+			'gainsboro'            => 'DCDCDC',
+			'ghostwhite'           => 'F8F8FF',
+			'gold'                 => 'FFD700',
+			'goldenrod'            => 'DAA520',
+			'gray'                 => '808080',
+			'green'                => '008000',
+			'greenyellow'          => 'ADFF2F',
+			'grey'                 => '808080',
+			'honeydew'             => 'F0FFF0',
+			'hotpink'              => 'FF69B4',
+			'indianred'            => 'CD5C5C',
+			'indigo'               => '4B0082',
+			'ivory'                => 'FFFFF0',
+			'khaki'                => 'F0E68C',
+			'lavender'             => 'E6E6FA',
+			'lavenderblush'        => 'FFF0F5',
+			'lawngreen'            => '7CFC00',
+			'lemonchiffon'         => 'FFFACD',
+			'lightblue'            => 'ADD8E6',
+			'lightcoral'           => 'F08080',
+			'lightcyan'            => 'E0FFFF',
+			'lightgoldenrodyellow' => 'FAFAD2',
+			'lightgray'            => 'D3D3D3',
+			'lightgreen'           => '90EE90',
+			'lightgrey'            => 'D3D3D3',
+			'lightpink'            => 'FFB6C1',
+			'lightsalmon'          => 'FFA07A',
+			'lightseagreen'        => '20B2AA',
+			'lightskyblue'         => '87CEFA',
+			'lightslategray'       => '778899',
+			'lightslategrey'       => '778899',
+			'lightsteelblue'       => 'B0C4DE',
+			'lightyellow'          => 'FFFFE0',
+			'lime'                 => '00FF00',
+			'limegreen'            => '32CD32',
+			'linen'                => 'FAF0E6',
+			'magenta'              => 'FF00FF',
+			'maroon'               => '800000',
+			'mediumaquamarine'     => '66CDAA',
+			'mediumblue'           => '0000CD',
+			'mediumorchid'         => 'BA55D3',
+			'mediumpurple'         => '9370D0',
+			'mediumseagreen'       => '3CB371',
+			'mediumslateblue'      => '7B68EE',
+			'mediumspringgreen'    => '00FA9A',
+			'mediumturquoise'      => '48D1CC',
+			'mediumvioletred'      => 'C71585',
+			'midnightblue'         => '191970',
+			'mintcream'            => 'F5FFFA',
+			'mistyrose'            => 'FFE4E1',
+			'moccasin'             => 'FFE4B5',
+			'navajowhite'          => 'FFDEAD',
+			'navy'                 => '000080',
+			'oldlace'              => 'FDF5E6',
+			'olive'                => '808000',
+			'olivedrab'            => '6B8E23',
+			'orange'               => 'FFA500',
+			'orangered'            => 'FF4500',
+			'orchid'               => 'DA70D6',
+			'palegoldenrod'        => 'EEE8AA',
+			'palegreen'            => '98FB98',
+			'paleturquoise'        => 'AFEEEE',
+			'palevioletred'        => 'DB7093',
+			'papayawhip'           => 'FFEFD5',
+			'peachpuff'            => 'FFDAB9',
+			'peru'                 => 'CD853F',
+			'pink'                 => 'FFC0CB',
+			'plum'                 => 'DDA0DD',
+			'powderblue'           => 'B0E0E6',
+			'purple'               => '800080',
+			'red'                  => 'FF0000',
+			'rosybrown'            => 'BC8F8F',
+			'royalblue'            => '4169E1',
+			'saddlebrown'          => '8B4513',
+			'salmon'               => 'FA8072',
+			'sandybrown'           => 'F4A460',
+			'seagreen'             => '2E8B57',
+			'seashell'             => 'FFF5EE',
+			'sienna'               => 'A0522D',
+			'silver'               => 'C0C0C0',
+			'skyblue'              => '87CEEB',
+			'slateblue'            => '6A5ACD',
+			'slategray'            => '708090',
+			'slategrey'            => '708090',
+			'snow'                 => 'FFFAFA',
+			'springgreen'          => '00FF7F',
+			'steelblue'            => '4682B4',
+			'tan'                  => 'D2B48C',
+			'teal'                 => '008080',
+			'thistle'              => 'D8BFD8',
+			'tomato'               => 'FF6347',
+			'turquoise'            => '40E0D0',
+			'violet'               => 'EE82EE',
+			'wheat'                => 'F5DEB3',
+			'white'                => 'FFFFFF',
+			'whitesmoke'           => 'F5F5F5',
+			'yellow'               => 'FFFF00',
+			'yellowgreen'          => '9ACD32'
+		);
+
+		private $hex;
+
+		private $red   = null;
+		private $green = null;
+		private $blue  = null;
+		private $alpha = 1;
+
+		/**
+		 * The class constructor
+		 */
+		public function __construct( $color = '', $mode = 'hex' ) {
+			$this->color = $color;
+			if ( 'hex' == $mode ) {
+				$this->from_hex();
+			}
+
+		}
+
+		private function from_hex() {
+
+			if ( ! function_exists( 'sanitize_hex_color' ) ) {
+				require_once ABSPATH . WPINC . '/class-wp-customize-manager.php';
+			}
+			// Is this perhaps a word-color?
+			if ( array_key_exists( $this->color, $this->word_colors ) ) {
+				$this->color = '#' . $this->word_colors[ $this->color ];
+			}
+			// Sanitize color
+			$this->hex = maybe_hash_hex_color( $this->color );
+			$this->hex = sanitize_hex_color( $this->hex );
+
+			$hex = $this->hex;
+			// Make sure we have 6 digits for the below calculations
+			if ( 3 == strlen( ltrim( $this->hex, '#' ) ) ) {
+				$hex = ltrim( $this->hex, '#' );
+				$hex = substr( $hex, 0, 1 ) . substr( $hex, 0, 1 ) . substr( $hex, 1, 1 ) . substr( $hex, 1, 1 ) . substr( $hex, 2, 1 ) . substr( $hex, 2, 1 );
+			}
+			$this->red   = hexdec( substr( $hex, 0, 2 ) );
+			$this->green = hexdec( substr( $hex, 2, 2 ) );
+			$this->blue  = hexdec( substr( $hex, 4, 2 ) );
+			$this->alpha = 1;
+
+		}
+
+		private function get_css( $mode = 'hex' ) {
+			if ( 'hex' == $mode ) {
+				return $this->hex;
+			}
+		}
+
 		/**
 		 * Sanitises a HEX value.
-		 * The way this works is by splitting the string in 6 substrings.
-		 * Each sub-string is individually sanitized, and the result is then returned.
 		 *
 		 * @var     string      The hex value of a color
 		 * @param   boolean     Whether we want to include a hash (#) at the beginning or not
 		 * @return  string      The sanitized hex color.
 		 */
 		 public static function sanitize_hex( $color = '#FFFFFF', $hash = true ) {
-
-			$word_colors = array(
-				'aliceblue'            => 'F0F8FF',
-				'antiquewhite'         => 'FAEBD7',
-				'aqua'                 => '00FFFF',
-				'aquamarine'           => '7FFFD4',
-				'azure'                => 'F0FFFF',
-				'beige'                => 'F5F5DC',
-				'bisque'               => 'FFE4C4',
-				'black'                => '000000',
-				'blanchedalmond'       => 'FFEBCD',
-				'blue'                 => '0000FF',
-				'blueviolet'           => '8A2BE2',
-				'brown'                => 'A52A2A',
-				'burlywood'            => 'DEB887',
-				'cadetblue'            => '5F9EA0',
-				'chartreuse'           => '7FFF00',
-				'chocolate'            => 'D2691E',
-				'coral'                => 'FF7F50',
-				'cornflowerblue'       => '6495ED',
-				'cornsilk'             => 'FFF8DC',
-				'crimson'              => 'DC143C',
-				'cyan'                 => '00FFFF',
-				'darkblue'             => '00008B',
-				'darkcyan'             => '008B8B',
-				'darkgoldenrod'        => 'B8860B',
-				'darkgray'             => 'A9A9A9',
-				'darkgreen'            => '006400',
-				'darkgrey'             => 'A9A9A9',
-				'darkkhaki'            => 'BDB76B',
-				'darkmagenta'          => '8B008B',
-				'darkolivegreen'       => '556B2F',
-				'darkorange'           => 'FF8C00',
-				'darkorchid'           => '9932CC',
-				'darkred'              => '8B0000',
-				'darksalmon'           => 'E9967A',
-				'darkseagreen'         => '8FBC8F',
-				'darkslateblue'        => '483D8B',
-				'darkslategray'        => '2F4F4F',
-				'darkslategrey'        => '2F4F4F',
-				'darkturquoise'        => '00CED1',
-				'darkviolet'           => '9400D3',
-				'deeppink'             => 'FF1493',
-				'deepskyblue'          => '00BFFF',
-				'dimgray'              => '696969',
-				'dimgrey'              => '696969',
-				'dodgerblue'           => '1E90FF',
-				'firebrick'            => 'B22222',
-				'floralwhite'          => 'FFFAF0',
-				'forestgreen'          => '228B22',
-				'fuchsia'              => 'FF00FF',
-				'gainsboro'            => 'DCDCDC',
-				'ghostwhite'           => 'F8F8FF',
-				'gold'                 => 'FFD700',
-				'goldenrod'            => 'DAA520',
-				'gray'                 => '808080',
-				'green'                => '008000',
-				'greenyellow'          => 'ADFF2F',
-				'grey'                 => '808080',
-				'honeydew'             => 'F0FFF0',
-				'hotpink'              => 'FF69B4',
-				'indianred'            => 'CD5C5C',
-				'indigo'               => '4B0082',
-				'ivory'                => 'FFFFF0',
-				'khaki'                => 'F0E68C',
-				'lavender'             => 'E6E6FA',
-				'lavenderblush'        => 'FFF0F5',
-				'lawngreen'            => '7CFC00',
-				'lemonchiffon'         => 'FFFACD',
-				'lightblue'            => 'ADD8E6',
-				'lightcoral'           => 'F08080',
-				'lightcyan'            => 'E0FFFF',
-				'lightgoldenrodyellow' => 'FAFAD2',
-				'lightgray'            => 'D3D3D3',
-				'lightgreen'           => '90EE90',
-				'lightgrey'            => 'D3D3D3',
-				'lightpink'            => 'FFB6C1',
-				'lightsalmon'          => 'FFA07A',
-				'lightseagreen'        => '20B2AA',
-				'lightskyblue'         => '87CEFA',
-				'lightslategray'       => '778899',
-				'lightslategrey'       => '778899',
-				'lightsteelblue'       => 'B0C4DE',
-				'lightyellow'          => 'FFFFE0',
-				'lime'                 => '00FF00',
-				'limegreen'            => '32CD32',
-				'linen'                => 'FAF0E6',
-				'magenta'              => 'FF00FF',
-				'maroon'               => '800000',
-				'mediumaquamarine'     => '66CDAA',
-				'mediumblue'           => '0000CD',
-				'mediumorchid'         => 'BA55D3',
-				'mediumpurple'         => '9370D0',
-				'mediumseagreen'       => '3CB371',
-				'mediumslateblue'      => '7B68EE',
-				'mediumspringgreen'    => '00FA9A',
-				'mediumturquoise'      => '48D1CC',
-				'mediumvioletred'      => 'C71585',
-				'midnightblue'         => '191970',
-				'mintcream'            => 'F5FFFA',
-				'mistyrose'            => 'FFE4E1',
-				'moccasin'             => 'FFE4B5',
-				'navajowhite'          => 'FFDEAD',
-				'navy'                 => '000080',
-				'oldlace'              => 'FDF5E6',
-				'olive'                => '808000',
-				'olivedrab'            => '6B8E23',
-				'orange'               => 'FFA500',
-				'orangered'            => 'FF4500',
-				'orchid'               => 'DA70D6',
-				'palegoldenrod'        => 'EEE8AA',
-				'palegreen'            => '98FB98',
-				'paleturquoise'        => 'AFEEEE',
-				'palevioletred'        => 'DB7093',
-				'papayawhip'           => 'FFEFD5',
-				'peachpuff'            => 'FFDAB9',
-				'peru'                 => 'CD853F',
-				'pink'                 => 'FFC0CB',
-				'plum'                 => 'DDA0DD',
-				'powderblue'           => 'B0E0E6',
-				'purple'               => '800080',
-				'red'                  => 'FF0000',
-				'rosybrown'            => 'BC8F8F',
-				'royalblue'            => '4169E1',
-				'saddlebrown'          => '8B4513',
-				'salmon'               => 'FA8072',
-				'sandybrown'           => 'F4A460',
-				'seagreen'             => '2E8B57',
-				'seashell'             => 'FFF5EE',
-				'sienna'               => 'A0522D',
-				'silver'               => 'C0C0C0',
-				'skyblue'              => '87CEEB',
-				'slateblue'            => '6A5ACD',
-				'slategray'            => '708090',
-				'slategrey'            => '708090',
-				'snow'                 => 'FFFAFA',
-				'springgreen'          => '00FF7F',
-				'steelblue'            => '4682B4',
-				'tan'                  => 'D2B48C',
-				'teal'                 => '008080',
-				'thistle'              => 'D8BFD8',
-				'tomato'               => 'FF6347',
-				'turquoise'            => '40E0D0',
-				'violet'               => 'EE82EE',
-				'wheat'                => 'F5DEB3',
-				'white'                => 'FFFFFF',
-				'whitesmoke'           => 'F5F5F5',
-				'yellow'               => 'FFFF00',
-				'yellowgreen'          => '9ACD32'
-			);
-
-			if ( ! function_exists( 'sanitize_hex_color' ) ) {
-				require_once ABSPATH . WPINC . '/class-wp-customize-manager.php';
-			}
-
-			// Remove any spaces and special characters before and after the string
-			$color = trim( $color );
-			// Check if the color is a standard word-color.
-			// If it is, then convert to hex.
-			if ( array_key_exists( $color, $word_colors ) ) {
-				$color = $word_colors[ $color ];
-			}
-			// Make sure color has "#" in the beginning for sanitize_hex to work ok
-			$color = maybe_hash_hex_color( $color );
-			$color = sanitize_hex_color( $color );
-			if ( ! $hash ) {
-				return str_replace( '#', '', $color );
-			}
-			return $color;
-
+		 	$obj = new Kirki_Color( $color, 'hex' );
+		 	if ( ! $hash ) {
+		 		return ltrim( $obj->get_css( 'hex' ), '#' );
+		 	}
+		 	return $obj->get_css( 'hex' );
 		}
 
 		public static function sanitize_rgba( $value ) {
