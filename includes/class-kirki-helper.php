71dd81900aef188d7cf25749b8d9185a031c5a38
Raghav Joshi <ragzor9999@gmail.com>
2016-02-29 00:40:04 +0530

    Get categories helper function
    Added a helper function to get a list of available categories, useful when adding a select picker for categories

--- includes/class-kirki-helper.php
+++ includes/class-kirki-helper.php
@@ -170,6 +170,22 @@ if ( ! class_exists( 'Kirki_Helper' ) ) {
 			return $items;
 
 		}
+		
+		public static function get_categories() {
+
+			$items = array();
+
+			// Get the post types
+			$categories = get_categories( array( 'public' => true ) );
+
+			// Build the array
+			foreach ( $categories as $category ) {
+				$items[ $category->term_id ] = $category->name;
+			}
+
+			return $items;
+
+		}
 
 	}
 }
