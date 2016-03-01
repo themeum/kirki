bcb3489be23472edb06b59c4d1df8ce829e903d3
Aristeides Stathopoulos <aristath@gmail.com>
2016-03-01 23:45:35 +0200

    add filter to include custom section types in Kirki

--- includes/class-kirki-section.php
+++ includes/class-kirki-section.php
@@ -3,7 +3,17 @@
 if ( ! class_exists( 'Kirki_Section' ) ) {
 	class Kirki_Section extends Kirki_Customizer {
 
+		/**
+		 * An array of our section types
+		 */
+		private $section_types = array(
+			'default'  => 'Kirki_Sections_Default_Section',
+			'expanded' => 'Kirki_Sections_Expanded_Section',
+		);
+
 		public function __construct( $args ) {
+
+			$this->section_types = apply_filters( 'kirki/section_types', $this->section_types );
 
 			parent::__construct( $args );
 			$this->add_section( $args );
@@ -12,23 +22,18 @@ if ( ! class_exists( 'Kirki_Section' ) ) {
 
 		public function add_section( $args ) {
 
-			if ( isset( $args['type'] ) && 'expanded' == $args['type'] ) {
-				$this->wp_customize->add_section( new Kirki_Sections_Expanded_Section( $this->wp_customize, sanitize_key( $args['id'] ), array(
-					'title'           => $args['title'], // already escaped in WP Core
-					'priority'        => absint( $args['priority'] ),
-					'panel'           => sanitize_key( $args['panel'] ),
-					'description'     => $args['description'], // already escaped in WP Core
-					'active_callback' => $args['active_callback'],
-				) ) );
-			} else {
-				$this->wp_customize->add_section( sanitize_key( $args['id'] ), array(
-					'title'           => $args['title'], // already escaped in WP Core
-					'priority'        => absint( $args['priority'] ),
-					'panel'           => sanitize_key( $args['panel'] ),
-					'description'     => $args['description'], // already escaped in WP Core
-					'active_callback' => $args['active_callback'],
-				) );
+			if ( ! isset( $args['type'] ) || ! array_key_exists( $args['type'], $this->section_types ) ) {
+				$args['type'] = 'default';
 			}
+			$section_classname = $this->section_types[ $args['type'] ];
+
+			$this->wp_customize->add_section( new $section_classname( $this->wp_customize, sanitize_key( $args['id'] ), array(
+				'title'           => $args['title'], // already escaped in WP Core
+				'priority'        => absint( $args['priority'] ),
+				'panel'           => sanitize_key( $args['panel'] ),
+				'description'     => $args['description'], // already escaped in WP Core
+				'active_callback' => $args['active_callback'],
+			) ) );
 
 			if ( isset( $args['icon'] ) ) {
 				$args['context'] = 'section';
