ebd35784e649bdaf3d3d7f86cb4d6c95c03627b4
Aristeides Stathopoulos <aristath@gmail.com>
2016-03-01 06:02:22 +0200

    added the type argument to section tests

--- tests/test-kirki-add_section.php
+++ tests/test-kirki-add_section.php
@@ -30,6 +30,7 @@ class Test_Kirki_Add_Section extends WP_UnitTestCase {
 				'priority'        => 10,
 				'active_callback' => '__return_true',
 				'panel'           => '',
+				'type'            => '',
 			),
 			Kirki::$sections['test_empty']
 		);
@@ -41,6 +42,7 @@ class Test_Kirki_Add_Section extends WP_UnitTestCase {
 				'priority'        => '4',
 				'active_callback' => '__return_true',
 				'panel'           => 'my_panel'
+				'type'            => '',
 			),
 			Kirki::$sections['test']
 		);
