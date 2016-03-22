777a780726ede1d08cae993096db5657a8a62041
Aristeides Stathopoulos <aristath@gmail.com>
2016-03-22 22:18:03 +0200

    fixes #598

--- assets/js/kirki-postmessage.js
+++ assets/js/kirki-postmessage.js
@@ -13,48 +13,48 @@
 					$.each( jsVars, function( i, js_var ) {
 
 						// Make sure everything is properly defined.
-						if ( undefined === jsVars[ i ]["element"] ) {
-							jsVars[ i ]["element"] = "";
+						if ( undefined === jsVars[ i ]['element'] ) {
+							jsVars[ i ]['element'] = '';
 						}
-						if ( undefined === jsVars[ i ]["property"] ) {
-							jsVars[ i ]["property"] = "";
+						if ( undefined === jsVars[ i ]['property'] ) {
+							jsVars[ i ]['property'] = '';
 						}
-						if ( undefined === jsVars[ i ]["prefix"] ) {
-							jsVars[ i ]["prefix"] = "";
+						if ( undefined === jsVars[ i ]['prefix'] ) {
+							jsVars[ i ]['prefix'] = '';
 						}
-						if ( undefined === jsVars[ i ]["suffix"] ) {
-							jsVars[ i ]["suffix"] = "";
+						if ( undefined === jsVars[ i ]['suffix'] ) {
+							jsVars[ i ]['suffix'] = '';
 						}
-						if ( undefined === jsVars[ i ]["units"] ) {
-							jsVars[ i ]["units"] = "";
+						if ( undefined === jsVars[ i ]['units'] ) {
+							jsVars[ i ]['units'] = '';
 						}
-						if ( undefined === jsVars[ i ]["function"] ) {
-							jsVars[ i ]["function"] = "css";
+						if ( undefined === jsVars[ i ]['function'] ) {
+							jsVars[ i ]['function'] = 'css';
 						}
 
 						$.each( jsVars, function( i, args ) {
-							if ( "html" === args.functionName ) {
 
-								$( args.element ).html( args.prefix + newval + args.units + args.suffix );
-
-							} else if ( "style" === args.functionName ) {
-
-								if ( ! $( "#kirki-style-" + args.id ).size() ) {
-									$( "head" ).append( '<style id="#kirki-style-' + args.id + '"></style>' );
+							// Value is a string
+							if ( 'string' == typeof newval ) {
+								// Inject HTML
+								if ( 'html' === args.function ) {
+									$( args.element ).html( args.prefix + newval + args.units + args.suffix );
+								// Attach to <head>
+								} else if ( 'style' === args.function ) {
+									if ( newval !== '' ) {
+										$( 'head' ).append( '<style>' + args.element + '{' + args.property + ':' + args.prefix + newval + args.units + args.suffix + ';}</style>' );
+									}
+								// CSS
+								} else if ( 'css' === args.function ) {
+									$( args.element ).css( args.property, args.prefix + newval + args.units + args.suffix );
 								}
 
-								if ( newval !== "" ) {
-									$( "#kirki-style-" + args.id ).text( args.element + "{" + args.property + ":" + args.prefix + args.newval + args.units + args.suffix + ";}" );
-								} else {
-									$( "#kirki-style-" + args.id ).text( "" );
-								}
-
-							} else {
-
-								$( args.element ).css( args.property, args.prefix + newval + args.units + args.suffix );
-
+							// Value is an object
+							} else if ( 'object' == typeof newval ) {
+								$.each( jsVars, function( subValueKey, subValueValue ) {
+									$( args.element ).css( subValueKey, args.prefix + subValueValue + args.units + args.suffix );
+								} );
 							}
-
 						});
 
 					});
