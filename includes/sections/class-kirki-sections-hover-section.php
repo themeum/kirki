8623f2617233b1f02360bef77e9b239efba2d97c
Aristeides Stathopoulos <aristath@gmail.com>
2016-03-22 23:05:55 +0200

    fixes #757

--- includes/sections/class-kirki-sections-hover-section.php
+++ includes/sections/class-kirki-sections-hover-section.php
@@ -9,13 +9,13 @@ class Kirki_Sections_Hover_Section extends WP_Customize_Section {
 		<li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }}">
 			<h3 class="accordion-section-title" tabindex="0">
 				{{ data.title }}
-				<span class="screen-reader-text"><?php _e( 'Press return or enter to open this section' ); ?></span>
+				<span class="screen-reader-text"><?php _e( 'Press return or enter to open this section', 'kirki' ); ?></span>
 			</h3>
 			<ul class="accordion-section-content">
 				<li class="customize-section-description-container">
 					<div class="customize-section-title">
 						<button class="customize-section-back" tabindex="-1">
-							<span class="screen-reader-text"><?php _e( 'Back' ); ?></span>
+							<span class="screen-reader-text"><?php _e( 'Back', 'kirki' ); ?></span>
 						</button>
 						<h3>
 							<span class="customize-action">
