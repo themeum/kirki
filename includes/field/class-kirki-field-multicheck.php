cbd7f4ac5bb3d09cae4ff2a617901502c3c1d18a
Aristeides Stathopoulos <aristath@gmail.com>
2016-03-28 03:26:15 +0300

    more tweaks

---
+++ includes/field/class-kirki-field-multicheck.php
@@ -0,0 +1,46 @@
+<?php
+
+if ( ! class_exists( 'Kirki_Field_Multicheck' ) ) {
+
+	class Kirki_Field_Multicheck extends Kirki_Field {
+
+		/**
+		 * Sets the control type.
+		 *
+		 * @access protected
+		 */
+		protected function set_type() {
+
+			$this->type = 'multicheck';
+
+		}
+
+		/**
+		 * Sets the $sanitize_callback
+		 *
+		 * @access protected
+		 */
+		protected function set_sanitize_callback() {
+
+			// If a custom sanitize_callback has been defined,
+			// then we don't need to proceed any further.
+			if ( ! empty( $this->sanitize_callback ) ) {
+				return;
+			}
+			$this->sanitize_callback = array( $this, 'sanitize' );
+
+		}
+
+		/**
+		 * The sanitize method that will be used as a falback
+		 */
+		public function sanitize( $value ) {
+
+			$value = ( ! is_array( $value ) ) ? explode( ',', $value ) : $value;
+			return ( ! empty( $value ) ) ? array_map( 'sanitize_text_field', $value ) : array();
+
+		}
+
+	}
+
+}
