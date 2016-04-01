863c4759a41f95891d9c4817f50ddf7e7ef69ed2
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-01 15:33:55 +0300

    fixes #809

--- assets/js/postmessage.js
+++ assets/js/postmessage.js
@@ -3,6 +3,9 @@
 
 	$.each( js_vars, function( setting, jsVars ) {
 
+		var css      = '',
+		    cssArray = {};
+
 		api( setting, function( value ) {
 
 			value.bind( function( newval ) {
@@ -45,44 +48,61 @@
 							// Value is a string
 							if ( 'string' == typeof newval ) {
 								// Process the value pattern
-								var val = jsVars[ i ]['value_pattern'].replace( /\$/g, newval );
+								if ( undefined !== jsVars[ i ]['value_pattern'] ) {
+									var val = jsVars[ i ]['value_pattern'].replace( /\$/g, newval );
+								} else {
+									var val = newval;
+								}
 								// Inject HTML
 								if ( 'html' === args.function ) {
 									$( args.element ).html( args.prefix + val + args.units + args.suffix );
-								// Attach to <head>
+								// Add CSS
 								} else {
-									// make sure we have a stylesheet with the defined ID.
-									// If we don't then add it.
-									if ( ! $( '#kirki-customizer-postmessage' + setting ).size() ) {
-										$( 'head' ).append( '<style id="kirki-customizer-postmessage' + setting + '"></style>' );
-									}
 									// if we have new value, replace style contents with custom css
 									if ( val !== '' ) {
-										$( '#kirki-customizer-postmessage' + setting ).text( args.element + '{' + args.property + ':' + args.prefix + val + args.units + args.suffix + ';}' );
+										cssArray[ i ] = args.element + '{' + args.property + ':' + args.prefix + val + args.units + args.suffix + ';}';
 									}
 									// else let's clear it out
 									else {
-										$( '#kirki-customizer-postmessage' + setting ).text('' );
+										cssArray[ i ] = '';
 									}
 
 								}
 
 							// Value is an object
 							} else if ( 'object' == typeof newval ) {
+								cssArray[ i ] = '';
 								$.each( newval, function( subValueKey, subValueValue ) {
-									$( args.element ).css( subValueKey, args.prefix + subValueValue + args.units + args.suffix );
-								} );
+									cssArray[ i ] += subValueKey, args.prefix + subValueValue + args.units + args.suffix;
+								});
 							}
 						});
 
 					});
 
+					$.each( cssArray, function( i, singleCSS ) {
+
+						if ( '' !== singleCSS ) {
+							css += singleCSS;
+						}
+						// Attach to <head>
+						if ( '' !== css ) {
+							// make sure we have a stylesheet with the defined ID.
+							// If we don't then add it.
+							if ( ! $( '#kirki-customizer-postmessage' + setting ).size() ) {
+								$( 'head' ).append( '<style id="kirki-customizer-postmessage' + setting + '"></style>' );
+							}
+							$( '#kirki-customizer-postmessage' + setting ).text( css );
+						}
+
+					});
+
 				}
 
-			} );
+			});
 
-		} );
+		});
 
-	} );
+	});
 
-} )( jQuery );
+})( jQuery );
