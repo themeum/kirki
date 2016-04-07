15ad3935fa833327fe7ddb623bf6565530a1d177
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-08 01:55:13 +0300

    JS coding standards fixes

--- assets/js/controls/checkbox.js
+++ assets/js/controls/checkbox.js
@@ -9,14 +9,17 @@ wp.customize.controlConstructor['kirki-checkbox'] = wp.customize.Control.extend(
 		var control = this;
 
 		// Get the initial value
-		var checkbox_value = control.setting._value;
+		var value = control.setting._value;
 
 		// Change the value
 		this.container.on( 'change', 'input', function() {
+
 			// Get the checkbox status
-			checkbox_value = ( jQuery( this ).is( ':checked' ) ) ? true : false;
+			value = ( jQuery( this ).is( ':checked' ) ) ? true : false;
+
 			// Set the value in the WordPress API
-			control.setting.set( checkbox_value );
+			control.setting.set( value );
+
 		});
 
 	}
--- assets/js/reset.js
+++ assets/js/reset.js
@@ -4,16 +4,16 @@ jQuery( document ).ready( function( $ ) {
 
 	jQuery( 'a.kirki-reset-section' ).on( 'click', function() {
 
-		// Get the section ID
-		var id = jQuery( this ).data( 'reset-section-id' );
-
-		// Get controls inside the section
-		var controls = wp.customize.section( id ).controls();
+		var id       = jQuery( this ).data( 'reset-section-id' ),
+		    controls = wp.customize.section( id ).controls(),
+		    i;
 
 		// Loop controls
-		for ( var i = 0, len = controls.length; i < len; i++ ) {
-			// set value to default
+		for ( i = 0, len = controls.length; i < len; i++ ) {
+
+			// Set value to default
 			kirkiSetValue( controls[ i ].id, controls[ i ].params.default );
+
 		}
 
 	});
