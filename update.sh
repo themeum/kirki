205ca5923efbeef1baa5c8f377ed843112094418
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-10 03:19:33 +0300

    update script improvements

--- update.sh
+++ update.sh
@@ -6,6 +6,10 @@ if [ $# -lt 3 ]; then
 	exit 1
 fi
 
+rm -Rf ../kirki-release
+git clone git@github.com:aristath/kirki.git ../kirki-release
+cd ../kirki-release
+
 # Replace version number in the files
 find . -name kirki.php -exec sed -i "s/Version:       $1/Version:       $2/g" {} \;
 find . -name readme.txt -exec sed -i "s/Stable tag: $1/Stable tag: $2/g" {} \;
@@ -22,46 +26,44 @@ if [[ $3 == 'push' ]]; then
 	git add . && git commit -a -m "Version $2" && git push
 fi
 
-# Remove existing kirki-svn folder and pull a fresh copy
-rm -rf ../kirki-svn
-mkdir ../kirki-svn
-svn co https://plugins.svn.wordpress.org/kirki ../kirki-svn
+mkdir svn
+svn co https://plugins.svn.wordpress.org/kirki svn
 
 # remove trunk
-rm -rf ../kirki-svn/trunk
+rm -rf svn/trunk
 
 # copy fresh copy to trunk
-rsync -av . ../kirki-svn/trunk --exclude svn
+rsync -av . svn/trunk --exclude svn
 
 # remove unnecessary files
-rm -rf ../kirki-svn/trunk/node_modules
-rm -rf ../kirki-svn/trunk/.sass-cache
-rm -rf ../kirki-svn/trunk/.git
-rm -rf ../kirki-svn/trunk/.github
-# rm -rf ../kirki-svn/trunk/tests
-rm -rf ../kirki-svn/trunk/*.sh
-rm -rf ../kirki-svn/trunk/.codeclimate.yml
-rm -rf ../kirki-svn/trunk/.coveralls.yml
-rm -rf ../kirki-svn/trunk/.csslintrc
-rm -rf ../kirki-svn/trunk/.editorconfig
-rm -rf ../kirki-svn/trunk/.gitignore
-rm -rf ../kirki-svn/trunk/.simplecov
-rm -rf ../kirki-svn/trunk/.travis.yml
-rm -rf ../kirki-svn/trunk/Gruntfile.js
-rm -rf ../kirki-svn/trunk/composer.json
-rm -rf ../kirki-svn/trunk/package.json
-rm -rf ../kirki-svn/trunk/phpunit.xml
-rm -rf ../kirki-svn/trunk/assets/scss/
-rm -rf ../kirki-svn/trunk/vendor/
+rm -rf svn/trunk/node_modules
+rm -rf svn/trunk/.sass-cache
+rm -rf svn/trunk/.git
+rm -rf svn/trunk/.github
+# rm -rf svn/trunk/tests
+rm -rf svn/trunk/*.sh
+rm -rf svn/trunk/.codeclimate.yml
+rm -rf svn/trunk/.coveralls.yml
+rm -rf svn/trunk/.csslintrc
+rm -rf svn/trunk/.editorconfig
+rm -rf svn/trunk/.gitignore
+rm -rf svn/trunk/.simplecov
+rm -rf svn/trunk/.travis.yml
+rm -rf svn/trunk/Gruntfile.js
+rm -rf svn/trunk/composer.json
+rm -rf svn/trunk/package.json
+rm -rf svn/trunk/phpunit.xml
+rm -rf svn/trunk/assets/scss/
+rm -rf svn/trunk/vendor/
 
 # Update svn
-cd ../kirki-svn
+cd svn
 if [[ $3 == 'push' ]]; then
 	rm tags/$2
 	cp -r trunk tags/$2
-fi
-svn rm $( svn status | sed -e '/^!/!d' -e 's/^!//' )
-svn add * --force
-if [[ $3 == 'push' ]]; then
+	svn rm $( svn status | sed -e '/^!/!d' -e 's/^!//' )
+	svn add * --force
 	svn ci -m "v$2"
+	cd ../..
+	rm -rf kirki-release
 fi
