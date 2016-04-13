51dd3fc80d52b23d2b8482a235373491cda4d23f
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-13 03:06:42 +0300

    fixes #877

--- assets/js/postmessage.js
+++ assets/js/postmessage.js
@@ -70,30 +70,30 @@
 
 									// If we have new value, replace style contents with custom css
 									if ( '' !== val ) {
-										cssArray.i = args.element + '{' + args.property + ':' + args.prefix + val + args.units + args.suffix + ';}';
+										cssArray[ i ] = args.element + '{' + args.property + ':' + args.prefix + val + args.units + args.suffix + ';}';
 									}
 
 									// Else let's clear it out
 									else {
-										cssArray.i = '';
+										cssArray[ i ] = '';
 									}
 
 								}
 
-						// Value is an object
-						} else if ( 'object' === typeof newval ) {
+							// Value is an object
+							} else if ( 'object' === typeof newval ) {
 
-								cssArray.i = '';
+								cssArray[ i ] = '';
 								_.each( newval, function( subValueValue, subValueKey ) {
 									if ( undefined !== args.choice ) {
 										if ( args.choice === subValueKey ) {
-											cssArray.i += args.element + '{' + args.property + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
+											cssArray[ i ] += args.element + '{' + args.property + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
 										}
 									} else {
 										if ( _.contains( [ 'top', 'bottom', 'left', 'right' ], subValueKey ) ) {
-											cssArray.i += args.element + '{' + args.property + '-' + subValueKey + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
+											cssArray[ i ] += args.element + '{' + args.property + '-' + subValueKey + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
 										} else {
-											cssArray.i += args.element + '{' + args.property + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
+											cssArray[ i ] += args.element + '{' + args.property + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
 										}
 									}
 								});
@@ -125,7 +125,7 @@
 								jQuery( '#kirki-customizer-postmessage' + setting ).text( css );
 							}
 
-						}, 300 );
+						}, 100 );
 
 					});
 
