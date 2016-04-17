6a1bc29fdaaabf6010ef901a5637dad9865eb082
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-17 14:23:25 +0300

    test initializing classes

---
+++ tests/test-kirki-classes-init.php
@@ -0,0 +1,13 @@
+<?php
+
+class Test_Kirki_Classes_Init extends WP_UnitTestCase {
+
+	public function test() {
+
+		$l10n = new Kirki_l10n();
+
+		$this->assertTrue( is_object( $l10n ) );
+
+	}
+
+}
