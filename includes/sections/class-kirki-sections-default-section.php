1b7bb089857ed8cf5f1ffa5982bcb6af105cda29
hijackfl <hijackerfl@gmail.com>
2016-06-01 22:13:17 +0500

    fixes #1009

--- includes/sections/class-kirki-sections-default-section.php
+++ includes/sections/class-kirki-sections-default-section.php
@@ -51,13 +51,17 @@ if ( ! class_exists( 'Kirki_Sections_Default_Section' ) ) {
 			<li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }}">
 				<h3 class="accordion-section-title" tabindex="0">
 					{{ data.title }}
-					<span class="screen-reader-text">{{ window.kirki.l10n['open-section'] }}</span>
+					<span class="screen-reader-text">
+						<?php echo esc_attr( $l10n['open-section'] ); ?>
+					</span>
 				</h3>
 				<ul class="accordion-section-content">
 					<li class="customize-section-description-container">
 						<div class="customize-section-title">
 							<button class="customize-section-back" tabindex="-1">
-								<span class="screen-reader-text">{{ window.kirki.l10n.back }}</span>
+								<span class="screen-reader-text">
+									<?php echo esc_attr( $l10n['back'] ); ?>
+								</span>
 							</button>
 							<h3>
 								<span class="customize-action">
@@ -65,7 +69,7 @@ if ( ! class_exists( 'Kirki_Sections_Default_Section' ) ) {
 								</span>
 								{{ data.title }}
 								<a href="#" class="kirki-reset-section" data-reset-section-id="{{ data.id }}">
-									{{{ window.kirki.l10n['reset-with-icon'] }}}
+									<?php echo $l10n['reset-with-icon']; ?>
 								</a>
 							</h3>
 						</div>
