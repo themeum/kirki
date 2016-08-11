62accb745414dbb74a3821e649e6c35bef0da6d7
majick777 <majick777@gmail.com>
2016-08-11 18:31:50 +1000

    Check font variant and subset arrays before looping.
    Getting a whole bunch of "invalid argument supplied foreach()" warnings here.
    Seems with a fresh install sometimes the argument is set but not an array?
    Not sure if there is something deeper here or not but this fixes the warnings.

--- includes/class-kirki-enqueue.php
+++ includes/class-kirki-enqueue.php
@@ -183,16 +183,20 @@ if ( ! class_exists( 'Kirki_Enqueue' ) ) {
 				$subsets  = ( isset( $args['subsets'] ) ) ? $args['subsets'] : array();
 
 				$available_variants = array();
-				foreach ( $variants as $variant ) {
-					if ( array_key_exists( $variant, $all_variants ) ) {
-						$available_variants[] = array( 'id' => $variant, 'label' => $all_variants[ $variant ] );
+				if (is_array($variants)) {
+					foreach ( $variants as $variant ) {
+						if ( array_key_exists( $variant, $all_variants ) ) {
+							$available_variants[] = array( 'id' => $variant, 'label' => $all_variants[ $variant ] );
+						}
 					}
 				}
 
 				$available_subsets = array();
-				foreach ( $subsets as $subset ) {
-					if ( array_key_exists( $subset, $all_subsets ) ) {
-						$available_subsets[] = array( 'id' => $subset, 'label' => $all_subsets[ $subset ] );
+				if (is_array($subsets)) {
+					foreach ( $subsets as $subset ) {
+						if ( array_key_exists( $subset, $all_subsets ) ) {
+							$available_subsets[] = array( 'id' => $subset, 'label' => $all_subsets[ $subset ] );
+						}
 					}
 				}
 
