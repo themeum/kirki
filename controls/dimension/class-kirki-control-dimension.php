6c5a52867c4270da800c7646295e4e000f0e9d27
Aristeides Stathopoulos <aristath@gmail.com>
2016-11-26 18:40:43 +0200

    fix for dimension field

--- controls/dimension/class-kirki-control-dimension.php
+++ controls/dimension/class-kirki-control-dimension.php
@@ -28,6 +28,55 @@ class Kirki_Control_Dimension extends WP_Customize_Control {
 	public $type = 'kirki-dimension';
 
 	/**
+	 * Tooltips content.
+	 *
+	 * @access public
+	 * @var string
+	 */
+	public $tooltip = '';
+
+	/**
+	 * Used to automatically generate all postMessage scripts.
+	 *
+	 * @access public
+	 * @var array
+	 */
+	public $js_vars = array();
+
+	/**
+	 * Used to automatically generate all CSS output.
+	 *
+	 * @access public
+	 * @var array
+	 */
+	public $output = array();
+
+	/**
+	 * Data type
+	 *
+	 * @access public
+	 * @var string
+	 */
+	public $option_type = 'theme_mod';
+
+	/**
+	 * The kirki_config we're using for this control
+	 *
+	 * @access public
+	 * @var string
+	 */
+	public $kirki_config = 'global';
+
+	/**
+	 * The translation strings.
+	 *
+	 * @access protected
+	 * @since 2.3.5
+	 * @var array
+	 */
+	protected $l10n = array();
+
+	/**
 	 * Enqueue control related scripts/styles.
 	 *
 	 * @access public
