b6add359465eb276f7b9b6cd234747e64513bd02
Aristeides Stathopoulos <aristath@gmail.com>
2016-12-10 21:06:16 +0200

    radio-buttonset accessibility & styling tweaks

--- controls/radio-buttonset/class-kirki-control-radio-buttonset.php
+++ controls/radio-buttonset/class-kirki-control-radio-buttonset.php
@@ -122,7 +122,7 @@ class Kirki_Control_Radio_Buttonset extends WP_Customize_Control {
 		<# } #>
 		<div id="input_{{ data.id }}" class="buttonset">
 			<# for ( key in data.choices ) { #>
-				<input {{{ data.inputAttrs }}} class="switch-input" type="radio" value="{{ key }}" name="_customize-radio-{{{ data.id }}}" id="{{ data.id }}{{ key }}" {{{ data.link }}}<# if ( key === data.value ) { #> checked="checked" <# } #>>
+				<input {{{ data.inputAttrs }}} class="switch-input screen-reader-text" type="radio" value="{{ key }}" name="_customize-radio-{{{ data.id }}}" id="{{ data.id }}{{ key }}" {{{ data.link }}}<# if ( key === data.value ) { #> checked="checked" <# } #>>
 					<label class="switch-label switch-label-<# if ( key === data.value ) { #>on <# } else { #>off<# } #>" for="{{ data.id }}{{ key }}">
 						{{ data.choices[ key ] }}
 					</label>
