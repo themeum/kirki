cf16ccb24e2f244b6202f7b6e48dd287f58b7df7
Aristeides Stathopoulos <aristath@gmail.com>
2016-12-18 17:01:27 +0200

    Allow defining a style for the label on radio-image controls

--- controls/radio-image/class-kirki-control-radio-image.php
+++ controls/radio-image/class-kirki-control-radio-image.php
@@ -95,8 +95,13 @@ class Kirki_Control_Radio_Image extends WP_Customize_Control {
 		}
 
 		$this->json['inputAttrs'] = '';
+		$this->json['labelStyle'] = '';
 		foreach ( $this->input_attrs as $attr => $value ) {
-			$this->json['inputAttrs'] .= $attr . '="' . esc_attr( $value ) . '" ';
+			if ( 'style' !== $attr ) {
+				$this->json['inputAttrs'] .= $attr . '="' . esc_attr( $value ) . '" ';
+			} else {
+				$this->json['labelStyle'] = 'style="' . esc_attr( $value ) . '" ';
+			}
 		}
 
 	}
@@ -124,7 +129,7 @@ class Kirki_Control_Radio_Image extends WP_Customize_Control {
 		<div id="input_{{ data.id }}" class="image">
 			<# for ( key in data.choices ) { #>
 				<input {{{ data.inputAttrs }}} class="image-select" type="radio" value="{{ key }}" name="_customize-radio-{{ data.id }}" id="{{ data.id }}{{ key }}" {{{ data.link }}}<# if ( data.value === key ) { #> checked="checked"<# } #>>
-					<label for="{{ data.id }}{{ key }}">
+					<label for="{{ data.id }}{{ key }}" {{{ data.labelStyle }}}>
 						<img src="{{ data.choices[ key ] }}">
 						<span class="image-clickable"></span>
 					</label>
