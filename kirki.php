5297b13eb4f7b8683bb60825b54b4bddd70c59d4
Aristeides Stathopoulos <aristath@gmail.com>
2017-02-04 16:11:15 +0200

    tagging beta2

--- kirki.php
+++ kirki.php
@@ -5,7 +5,7 @@
  * Description:   The ultimate WordPress Customizer Toolkit
  * Author:        Aristeides Stathopoulos
  * Author URI:    http://aristeides.com
- * Version:       2.4.0-beta.1
+ * Version:       2.4.0-beta.2
  * Text Domain:   kirki
  *
  * GitHub Plugin URI: aristath/kirki
