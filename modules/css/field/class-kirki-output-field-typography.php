e2da3d8f8f86c81237f73a9fea796ab3ccf6e2a2
abouolia <a.bouhuolia@Live.com>
2017-02-13 06:40:31 +0200

    Fix when typography control has no font-family default value

--- modules/css/field/class-kirki-output-field-typography.php
+++ modules/css/field/class-kirki-output-field-typography.php
@@ -33,28 +33,28 @@ if ( ! class_exists( 'Kirki_Output_Field_Typography' ) ) {
 			if ( isset( $value['font-family'] ) && ! empty( $value['font-family'] ) ) {
 				$output['media_query'] = ( isset( $output['media_query'] ) ) ? $output['media_query'] : 'global';
 				$this->styles[ $output['media_query'] ][ $output['element'] ]['font-family'] = $output['prefix'] . $this->process_property_value( 'font-family', $value['font-family'] ) . $output['suffix'];
-			}
+				
+				// Add support for the older font-weight parameter.
+				// This has been deprecated so the code below is just to add some backwards-compatibility.
+				// Once a user visits their customizer and make changes to their typography,
+				// new values are saved and this one is no longer used.
+				if ( isset( $value['font-weight'] ) && ! empty( $value['font-weight'] ) ) {
+					$this->styles[ $output['media_query'] ][ $output['element'] ]['font-weight'] = $output['prefix'] . $value['font-weight'] . $output['suffix'];
+				}
+				
+				// Take care of variants.
+				if ( isset( $value['variant'] ) && ! empty( $value['variant'] ) ) {
 
-			// Add support for the older font-weight parameter.
-			// This has been deprecated so the code below is just to add some backwards-compatibility.
-			// Once a user visits their customizer and make changes to their typography,
-			// new values are saved and this one is no longer used.
-			if ( isset( $value['font-weight'] ) && ! empty( $value['font-weight'] ) ) {
-				$this->styles[ $output['media_query'] ][ $output['element'] ]['font-weight'] = $output['prefix'] . $value['font-weight'] . $output['suffix'];
-			}
+					// Get the font_weight.
+					$font_weight = str_replace( 'italic', '', $value['variant'] );
+					$font_weight = ( in_array( $font_weight, array( '', 'regular' ) ) ) ? '400' : $output['prefix'] . $font_weight . $output['suffix'];
 
-			// Take care of variants.
-			if ( isset( $value['variant'] ) && ! empty( $value['variant'] ) ) {
-
-				// Get the font_weight.
-				$font_weight = str_replace( 'italic', '', $value['variant'] );
-				$font_weight = ( in_array( $font_weight, array( '', 'regular' ) ) ) ? '400' : $output['prefix'] . $font_weight . $output['suffix'];
-
-				// Is this italic?
-				$is_italic = ( false !== strpos( $value['variant'], 'italic' ) );
-				$this->styles[ $output['media_query'] ][ $output['element'] ]['font-weight'] = $output['prefix'] . $font_weight . $output['suffix'];
-				$font_style = $is_italic ? 'italic' : 'normal' ;
-				$this->styles[ $output['media_query'] ][ $output['element'] ]['font-style'] = $output['prefix'] . $font_style . $output['suffix'];
+					// Is this italic?
+					$is_italic = ( false !== strpos( $value['variant'], 'italic' ) );
+					$this->styles[ $output['media_query'] ][ $output['element'] ]['font-weight'] = $output['prefix'] . $font_weight . $output['suffix'];
+					$font_style = $is_italic ? 'italic' : 'normal' ;
+					$this->styles[ $output['media_query'] ][ $output['element'] ]['font-style'] = $output['prefix'] . $font_style . $output['suffix'];
+				}
 			}
 
 			// Take care of font-size.
