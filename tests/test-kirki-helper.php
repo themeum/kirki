18c9f354f7be22d53327962018494356703d68c8
Aristeides Stathopoulos <aristath@gmail.com>
2017-05-13 14:08:25 +0300

    basic tests

---
+++ tests/test-kirki-helper.php
@@ -0,0 +1,56 @@
+<?php
+
+class Test_Kirki_Helper extends WP_UnitTestCase {
+
+	public function test() {
+		$tests = array(
+			'primary',
+			'red',
+			'pink',
+			'purple',
+			'deep-purple',
+			'indigo',
+			'blue',
+			'light_blue',
+			'cyan',
+			'teal',
+			'green',
+			'light-green',
+			'lime',
+			'yellow',
+			'amber',
+			'orange',
+			'deep-orange',
+			'brown',
+			'grey',
+			'blue-grey',
+			'50',
+			'100',
+			'200',
+			'300',
+			'400',
+			'500',
+			'600',
+			'700',
+			'800',
+			'900',
+			'A100',
+			'A200',
+			'A400',
+			'A700',
+			'all',
+		);
+		foreach ( $tests as $test ) {
+			$result = Kirki_Helper::get_material_design_colors( $test );
+			$this->assertTrue( is_array( $result ) );
+			$this->assertTrue( ! empty( $result ) );
+		}
+
+		global $wp_filesystem;
+		Kirki_Helper::init_filesystem();
+		$this->assertTrue( ! is_null( $wp_filesystem ) );
+
+		$this->assertTrue( is_array( Kirki_Helper::get_posts( array() ) ) );
+		$this->assertTrue( is_array( Kirki_Helper::get_taxonomies() ) );
+	}
+}
