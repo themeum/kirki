0ac0bb6593fd11d4958193656bd724bcb6a85cf1
Aristeides Stathopoulos <aristath@gmail.com>
2017-05-24 01:23:23 +0300

    Refactor of postMessage script (again)

--- modules/postmessage/postmessage.js
+++ modules/postmessage/postmessage.js
@@ -1,127 +1,231 @@
-( function() {
-	var api = wp.customize;
+var kirkiPostMessage = {
 
 	/**
-	 * Gets the font-weight from the variant.
+	 * Helper function to correctly get the font-weight from a variant
+	 * when using a typography field.
+	 *
+	 * @since  3.0.0
+	 * @param  object value The value of the setting.
+	 * @return int|string
 	 */
-	function kirkiGetFontWeight( value ) {
+	getFontWeight: function( value ) {
 		var calculated;
 
+		// Return 400 if invalid input.
 		if ( ! _.isString( value ) ) {
 			return 400;
 		}
+
+		// Get only digits. If non-numeric value, then return 400.
 		calculated = value.match( /\d/g );
 		return ( ! _.isObject( calculated ) ) ? 400 : calculated.join( '' );
-	}
+	},
 
-	_.each( jsvars, function( jsVars, setting ) {
-		var css      = '',
-		    cssArray = {};
+	/**
+	 * Helper function to correctly get the vars with all defaults set.
+	 *
+	 * @since  3.0.0
+	 * @param  object jsVar The arguments of the jsVars we'll be using.
+	 * @return object
+	 */
+	getVarComplete: function( jsVar ) {
+		return _.defaults( jsVar, {
+			element: '',
+			property: '',
+			prefix: '',
+			suffix: '',
+			units: '',
+			'function': 'css',
+			value_pattern: '$'
+		});
+	},
 
-		api( setting, function( value ) {
-			value.bind( function( newval ) {
-				if ( ! _.isUndefined( jsVars ) && 0 < jsVars.length ) {
-					_.each( jsVars, function( jsVar ) {
-						var val = newval;
+	/**
+	 * Make sure images have url("{image}") instead of just the URL.
+	 *
+	 * @since  3.0.0
+	 * @param  string property The CSS property to check against.
+	 * @param  string value    The setting value.
+	 * @return string
+	 */
+	applyBackgroundImage: function( property, value ) {
+		if ( 'background-image' === property && 0 > value.indexOf( 'url(' ) ) {
+			value = 'url("' + value + '")';
+		}
+		return value;
+	},
 
-						jsVar = _.defaults( jsVar, {
-							element: '',
-							property: '',
-							prefix: '',
-							suffix: '',
-							units: '',
-							'function': 'css',
-							value_pattern: '$'
-						});
+	/**
+	 * Adds google-fonts scripts and returns the CSS for the typography field.
+	 *
+	 * @since  3.0.0
+	 * @param  object value The setting value.
+	 * @param  object args  The arguments we're using to auto-calculate the CSS.
+	 * @return object
+	 */
+	applyTypography: function( value, args ) {
+		var $this         = this,
+		    subsetsString = '',
+		    css           = '';
 
-						_.each( jsVars, function( args, i ) {
+		// Early exit if this is not an object.
+		if ( ! _.isObject( value ) ) {
+			return;
+		}
 
-							// Value is a string
-							if ( _.isString( newval ) ) {
-								val = args.value_pattern.replace( /\$/g, args.prefix + newval + args.units + args.suffix );
+		// If 'font-family' is not defined then this is not a typography field.
+		if ( _.isUndefined( value['font-family'] ) ) {
+			return;
+		}
 
-								// Simple tweak for background-image properties.
-								if ( 'background-image' === args.property && 0 > val.indexOf( 'url(' ) ) {
-									val = 'url("' + val + '")';
-								}
+		// Make sure variants and subsets are defined.
+		value.variant = ( _.isUndefined( value.variant ) ) ? 400 : value.variant;
+		value.subsets = ( _.isUndefined( value.subsets ) ) ? [] : value.subsets;
+		subsetsString = ( _.isObject( value.subsets ) ) ? ':' + value.subsets.join( ',' ) : '';
 
-								// Inject HTML
-								if ( 'html' === args['function'] ) {
+		// Load the font using WenFontloader.
+		jQuery( 'head' ).append( '<script>if(!_.isUndefined(WebFont)){WebFont.load({google:{families:[\'' + value['font-family'] + ':' + value.variant + subsetsString + '\']}});}</script>' );
 
+		// Add the CSS.
+		css += args.element + '{font-weight:' + $this.getFontWeight( value.variant ) + '}';
+		css += ( -1 !== value.variant.indexOf( 'italic' ) ) ? args.element + '{font-style:italic;}' : args.element + '{font-style:normal;}';
+		return css;
+	},
+
+	/**
+	 * Generates the CSS.
+	 *
+	 * @since  3.0.0
+	 * @param  mixed  value   The setting value.
+	 * @param  object args    The arguments.
+	 * @param  string setting The setting we're checking.
+	 * @return object
+	 */
+	generateCSS: function( value, args, setting ) {
+		var $this = this,
+		    css = '',
+		    regex;
+
+		// Value is string.
+		if ( _.isString( value ) ) {
+
+			// No need to proceed if empty.
+			if ( '' === value ) {
+				return '';
+			}
+			/*
+			If ( ! _.isUndefined( args.value_pattern ) && '' !== args.value_pattern && '$' !== args.value_pattern && ! _.isUndefined( args.replacements ) ) {
+				_.each( args.replacements, function( replace, search ) {
+					if ( 'this' === replace ) {
+						// TODO: Get current value
+					} else {
+						// TODO: Get other value
+					}
+					regex = new RegExp( search, 'g' );
+					args.value_pattern = args.value_pattern.replace( regex, replace );
+				});
+				args.value_pattern.replace( /\$/g, args.prefix + value + args.units + args.suffix );
+			}
+			*/
+
+			// Calculate and return the CSS.
+			value = args.value_pattern.replace( /\$/g, args.prefix + value + args.units + args.suffix );
+			value = $this.applyBackgroundImage( args.property, value );
+			return args.element + '{' + args.property + ':' + value + ';}';
+
+		// Value is an object.
+		} else if ( _.isObject( value ) ) {
+			_.each( value, function( subVal, subKey ) {
+
+				// Make sure that if 'background-image' is used, it's properly formatted.
+				subVal = $this.applyBackgroundImage( subKey, subVal );
+
+				// Apply css and webfonts for typography controls.
+				css += $this.applyTypography( value, args );
+
+				// Hack for the "choice" argument.
+				if ( ! _.isUndefined( args.choice ) ) {
+					if ( args.choice === subKey ) {
+						css += args.element + '{' + args.property + ':' + args.prefix + subVal + args.units + args.suffix + ';}';
+					}
+				} else {
+
+					// Mostly used for padding, margin & position properties.
+					if ( _.contains( [ 'top', 'bottom', 'left', 'right' ], subKey ) ) {
+						css += args.element + '{' + args.property + '-' + subKey + ':' + args.prefix + subVal + args.units + args.suffix + ';}';
+					} else if ( 'subsets' !== subKey && 'variant' !== subKey ) {
+
+						// This is where most object-based fields will go.
+						css += args.element + '{' + subKey + ':' + args.prefix + subVal + args.units + args.suffix + ';}';
+					}
+				}
+			});
+			return css;
+		}
+		return '';
+	},
+
+	/**
+	 * Loops our fields and does what needs to be done.
+	 *
+	 * @since  3.0.0
+	 */
+	init: function() {
+		var $this = this;
+		_.each( jsvars, function( jsVars, setting ) {
+			var css      = '',
+			    cssArray = {};
+
+			wp.customize( setting, function( value ) {
+				value.bind( function( newval ) {
+					if ( ! _.isUndefined( jsVars ) && 0 < jsVars.length ) {
+						_.each( jsVars, function( jsVar ) {
+							var val = newval;
+
+							jsVar = $this.getVarComplete( jsVar );
+
+							_.each( jsVars, function( args, i ) {
+
+								// If we're using "html" instead of "css" then add the HTML.
+								if ( _.isString( newval ) && 'html' === args['function'] ) {
 									if ( ! _.isUndefined( args.attr ) ) {
 										jQuery( args.element ).attr( args.attr, val );
 									} else {
 										jQuery( args.element ).html( val );
 									}
-
-								// Add CSS
 								} else {
 
-									// If we have new value, replace style contents with custom css.
-									cssArray[ i ] = ( '' !== val ) ? args.element + '{' + args.property + ':' + val + ';}' : '';
+									// Generate the CSS.
+									cssArray[ i ] = $this.generateCSS( newval, args, setting );
+								}
+							});
+						});
+
+						// Adds the CSS to the document.
+						_.each( cssArray, function( singleCSS ) {
+							css = '';
+							setTimeout( function() {
+
+								if ( '' !== singleCSS ) {
+									css += singleCSS;
 								}
 
-								// Value is an object
-							} else if ( _.isObject( newval ) ) {
-
-								cssArray[ i ] = '';
-								_.each( newval, function( subValueValue, subValueKey ) {
-
-									// Helper for google-fonts if needed.
-									var subsetsString = '';
-
-									// Simple tweak for background-image properties.
-									if ( 'background-image' === subValueKey && 0 > subValueValue.indexOf( 'url(' ) ) {
-										subValueValue = 'url("' + subValueValue + '")';
-									}
-
-									// Dirty hack to make typography fields work.
-									if ( 'font-family' === subValueKey || 'variant' === subValueKey || 'subsets' === subValueKey ) {
-										newval['font-family'] = ( _.isUndefined( newval['font-family'] ) ) ? '' : newval['font-family'];
-										newval.variant        = ( _.isUndefined( newval.variant ) ) ? 400 : newval.variant;
-										newval.subsets        = ( _.isUndefined( newval.subsets ) ) ? [] : newval.subsets;
-										subsetsString         = ( _.isObject( newval.subsets ) ) ? ':' + newval.subsets.join( ',' ) : '';
-
-										jQuery( 'head' ).append( '<script>if(!_.isUndefined(WebFont)){WebFont.load({google:{families:[\'' + newval['font-family'] + ':' + newval.variant + subsetsString + '\']}});}</script>' );
-
-										cssArray[ i ] += args.element + '{font-weight:' + kirkiGetFontWeight( newval.variant ) + '}';
-										cssArray[ i ] += ( -1 !== newval.variant.indexOf( 'italic' ) ) ? args.element + '{font-style:italic;}' : args.element + '{font-style:normal;}';
-									}
-
-									if ( ! _.isUndefined( args.choice ) ) {
-										if ( args.choice === subValueKey ) {
-											cssArray[ i ] += args.element + '{' + args.property + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
-										}
-									} else {
-										if ( _.contains( [ 'top', 'bottom', 'left', 'right' ], subValueKey ) ) {
-											cssArray[ i ] += args.element + '{' + args.property + '-' + subValueKey + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
-										} else if ( 'subsets' !== subValueKey && 'variant' !== subValueKey ) {
-											cssArray[ i ] += args.element + '{' + subValueKey + ':' + args.prefix + subValueValue + args.units + args.suffix + ';}';
-										}
-									}
-								});
-							}
+								// Attach to <head>. Make sure we have a stylesheet with the defined ID,
+								// and if we don't then add it.
+								if ( ! jQuery( '#kirki-customizer-postmessage' + setting.replace( /\[/g, '-' ).replace( /\]/g, '' ) ).size() ) {
+									jQuery( 'head' ).append( '<style id="kirki-customizer-postmessage' + setting.replace( /\[/g, '-' ).replace( /\]/g, '' ) + '"></style>' );
+								}
+								jQuery( '#kirki-customizer-postmessage' + setting.replace( /\[/g, '-' ).replace( /\]/g, '' ) ).text( css );
+							}, 100 );
 						});
-					});
-
-					_.each( cssArray, function( singleCSS ) {
-						css = '';
-						setTimeout( function() {
-
-							if ( '' !== singleCSS ) {
-								css += singleCSS;
-							}
-
-							// Attach to <head>. Make sure we have a stylesheet with the defined ID,
-							// and if we don't then add it.
-							if ( ! jQuery( '#kirki-customizer-postmessage' + setting.replace( /\[/g, '-' ).replace( /\]/g, '' ) ).size() ) {
-								jQuery( 'head' ).append( '<style id="kirki-customizer-postmessage' + setting.replace( /\[/g, '-' ).replace( /\]/g, '' ) + '"></style>' );
-							}
-							jQuery( '#kirki-customizer-postmessage' + setting.replace( /\[/g, '-' ).replace( /\]/g, '' ) ).text( css );
-						}, 100 );
-					});
-				}
+					}
+				});
 			});
 		});
-	});
-})( jQuery );
+	}
+};
+
+// Init.
+jQuery( document ).ready( function() {
+	kirkiPostMessage.init();
+});
