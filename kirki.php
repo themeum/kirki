4444076c079bab7ac12e1ed0f6e4cd7160e55a39
Aristeides Stathopoulos <aristath@gmail.com>
2017-05-28 21:26:17 +0300

    fix failing tests

--- kirki.php
+++ kirki.php
@@ -89,6 +89,7 @@ if ( file_exists( $custom_config_path ) ) {
  * Fires at the end of the update message container in each
  * row of the plugins list table.
  * Allows us to add important notices about updates should they be needed.
+ * Notices should be added using "== Upgrade Notice ==" in readme.txt.
  *
  * @since 2.3.8
  * @param array $plugin_data An array of plugin metadata.
@@ -96,23 +97,13 @@ if ( file_exists( $custom_config_path ) ) {
  */
 function kirki_show_upgrade_notification( $plugin_data, $response ) {
 
-    // Check "upgrade_notice".
-    if ( isset( $response->upgrade_notice ) && strlen( trim( $response->upgrade_notice ) ) > 0 ) : ?>
-        <style>
-        .kirki-upgrade-notification {
-            background-color: #d54e21;
-            padding: 10px;
-            color: #f9f9f9;
-            margin-top: 10px;
-        }
-        .kirki-upgrade-notification + p {
-            display: none;
-        }
-        </style>
-        <div class="kirki-upgrade-notification">
-            <strong><?php esc_attr_e( 'Important Upgrade Notice:', 'kirki' ); ?></strong>
-            <?php echo wp_strip_all_tags( $response->upgrade_notice ); ?>
-        </div>
-    <?php endif;
+	// Check "upgrade_notice".
+	if ( isset( $response->upgrade_notice ) && strlen( trim( $response->upgrade_notice ) ) > 0 ) : ?>
+		<style>.kirki-upgrade-notification {background-color:#d54e21;padding:10px;color:#f9f9f9;margin-top:10px;margin-bottom:10px;}.kirki-upgrade-notification + p {display:none;}</style>
+		<div class="kirki-upgrade-notification">
+			<strong><?php esc_attr_e( 'Important Upgrade Notice:', 'kirki' ); ?></strong>
+			<?php echo wp_strip_all_tags( $response->upgrade_notice ); ?>
+		</div>
+	<?php endif;
 }
 add_action( 'in_plugin_update_message-' . plugin_basename( __FILE__ ), 'kirki_show_upgrade_notification', 10, 2 );
