b29ee13b0f5c81c86b68d1b1fbdc345acdddd186
Aristeides Stathopoulos <aristath@gmail.com>
2017-05-30 01:52:27 +0300

    Inline comments.

--- custom-build.sh
+++ custom-build.sh
@@ -1,9 +1,13 @@
+# Add the custom colorpicker if it exists.
 if [ -f assets/vendor/wp-color-picker-alpha/wp-color-picker-alpha.js ]; then
 	cat assets/vendor/wp-color-picker-alpha/wp-color-picker-alpha.js >> build.js
 	rm assets/vendor/wp-color-picker-alpha/wp-color-picker-alpha.js
 fi
+
+# Loop controls.
 for controlDir in ./controls/*; do
 	if [ -d "${controlDir}" ]; then
+		# Compile all SCSS files to a single build.scss file and delete them.
 		for scssFile in "${controlDir}"/*.scss; do
 			if [ -f "${scssFile}" ]; then
 				cat "$scssFile" >> build.scss
@@ -12,6 +16,7 @@ for controlDir in ./controls/*; do
 				rm "${controlDir}"/*.css
 			fi
 		done
+		# Compile all JS files to a single build.js file and delete them.
 		for jsFile in "${controlDir}"/*.js; do
 			if [ -f "${jsFile}" ]; then
 				cat "$jsFile" >> build.js
