2d9e91bca1a4fbf653e901b534738cdc821989df
Aristeides Stathopoulos <aristath@gmail.com>
2017-05-29 19:53:43 +0300

    bugfix for baground-image: gradient

--- modules/postmessage/class-kirki-modules-postmessage.php
+++ modules/postmessage/class-kirki-modules-postmessage.php
@@ -149,7 +149,7 @@ class Kirki_Modules_PostMessage {
 		}
 
 		// Tweak to add url() for background-images.
-		if ( 'background-image' === $args['property'] ) {
+		if ( 'background-image' === $args['property'] && ( ! isset( $args['value_pattern'] ) || false === strpos( $args['value_pattern'], 'gradient' ) ) ) {
 			$script .= 'if(-1===' . $value_key . '.indexOf(\'url(\')){' . $value_key . '=\'url("\'+' . $value_key . '+\'");}';
 		}
 
