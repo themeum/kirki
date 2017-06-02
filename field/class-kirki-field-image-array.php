06977cd685c0a121a5ea9785cd3f1a384d2f5374
Aristeides Stathopoulos <aristath@gmail.com>
2017-06-02 15:30:20 +0300

    Sanitization for image-array control.

--- field/class-kirki-field-image-array.php
+++ field/class-kirki-field-image-array.php
@@ -48,7 +48,11 @@ class Kirki_Field_Image_Array extends Kirki_Field {
 	 */
 	public function sanitize( $value ) {
 
-		return $value;
-
+		return array(
+			'id'     => ( isset( $value['id'] && '' !== $value['id'] ) ) ? (int) $value['id'] : '',
+			'url'    => ( isset( $value['url'] && '' !== $value['url'] ) ) ? esc_url_raw( $value['url'] ) : '',
+			'width'  => ( isset( $value['width'] && '' !== $value['width'] ) ) ? (int) $value['width'] : '',
+			'height' => ( isset( $value['height'] && '' !== $value['height'] ) ) ? (int) $value['height'] : '',
+		)
 	}
 }
