1c1aab81dcbd29718422db8e082e7e0055e7a698
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-04 23:55:51 +0300

    Add Kirki_Util class.

--- kirki.php
+++ kirki.php
@@ -58,7 +58,7 @@ $kirki->modules = new Kirki_Modules();
 Kirki::$path = wp_normalize_path( dirname( __FILE__ ) );
 
 // If Kirki is installed as a plugin, use plugin_dir_url().
-$kirki_is_plugin = Kirki_Init::is_plugin();
+$kirki_is_plugin = Kirki_Util::is_plugin();
 if ( $kirki_is_plugin ) {
 	Kirki::$url = plugin_dir_url( __FILE__ );
 } elseif ( function_exists( 'is_link' ) && is_link( dirname( __FILE__ ) ) && function_exists( 'readlink' ) ) {
