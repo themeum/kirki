a4c41cfd42fd7e3ac90144bd10518f9686f7e5a2
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-08 02:40:09 +0300

    remove unused code remnants

--- controls/dimensions/class-kirki-control-dimensions.php
+++ controls/dimensions/class-kirki-control-dimensions.php
@@ -99,7 +99,6 @@ class Kirki_Control_Dimensions extends WP_Customize_Control {
 	 */
 	public function enqueue() {
 
-		$script_to_localize = ;
 		wp_enqueue_script( 'kirki-dimensions', trailingslashit( Kirki::$url ) . 'controls/dimensions/dimensions.js', array( 'jquery', 'customize-base' ), false, true );
 		wp_enqueue_style( 'kirki-dimensions-css', trailingslashit( Kirki::$url ) . 'controls/dimensions/dimensions.css', null );
 		wp_localize_script( 'kirki-dimensions', 'dimensionskirkiL10n', $this->l10n() );
