7ddae00a0d1abfa5c9938bdd06fca65f2eef6ec9
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-08 19:04:52 +0300

    see #1453

--- core/deprecated.php
+++ core/deprecated.php
@@ -21,53 +21,42 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! function_exists( 'kirki_get_option' ) ) {
 	/**
 	 * Get the value of a field.
-	 * This is a deprecated function that we in use when there was no API.
+	 * This is a deprecated function that we useD when there was no API.
 	 * Please use the Kirki::get_option() method instead.
 	 * Documentation is available for the new method on https://github.com/aristath/kirki/wiki/Getting-the-values
 	 *
 	 * @return mixed
 	 */
 	function kirki_get_option( $option = '' ) {
+		_deprecated_function( __FUNCTION__, '1.0.0', esc_attr__( 'kirki_get_option detected. Please use get_theme_mod() or get_option() instead.', 'kirki' ) );
 		return Kirki::get_option( '', $option );
 	}
 }
 
 if ( ! function_exists( 'kirki_sanitize_hex' ) ) {
 	function kirki_sanitize_hex( $color ) {
+		_deprecated_function( __FUNCTION__, '1.0.0', esc_attr__( 'kirki_sanitize_hex detected. Please use the ariColor class instead. More info on http://aristath.github.io/ariColor/', 'kirki' ) );
 		return Kirki_Color::sanitize_hex( $color );
 	}
 }
 
 if ( ! function_exists( 'kirki_get_rgb' ) ) {
 	function kirki_get_rgb( $hex, $implode = false ) {
+		_deprecated_function( __FUNCTION__, '1.0.0', esc_attr__( 'kirki_get_rgb detected. Please use the ariColor class instead. More info on http://aristath.github.io/ariColor/', 'kirki' ) );
 		return Kirki_Color::get_rgb( $hex, $implode );
 	}
 }
 
 if ( ! function_exists( 'kirki_get_rgba' ) ) {
 	function kirki_get_rgba( $hex = '#fff', $opacity = 100 ) {
+		_deprecated_function( __FUNCTION__, '1.0.0', esc_attr__( 'kirki_get_rgba detected. Please use the ariColor class instead. More info on http://aristath.github.io/ariColor/', 'kirki' ) );
 		return Kirki_Color::get_rgba( $hex, $opacity );
 	}
 }
 
 if ( ! function_exists( 'kirki_get_brightness' ) ) {
 	function kirki_get_brightness( $hex ) {
+		_deprecated_function( __FUNCTION__, '1.0.0', esc_attr__( 'kirki_get_brightness detected. Please use the ariColor class instead. More info on http://aristath.github.io/ariColor/', 'kirki' ) );
 		return Kirki_Color::get_brightness( $hex );
-	}
-}
-
-/**
- * Class was deprecated in 2.2.7
- *
- * @see https://github.com/aristath/kirki/commit/101805fd689fa8828920b789347f13efc378b4a7
- */
-if ( ! class_exists( 'Kirki_Colourlovers' ) ) {
-	/**
-	 * Deprecated.
-	 */
-	class Kirki_Colourlovers {
-		public static function get_palettes( $palettes_nr = 5 ) {
-			return array();
-		}
 	}
 }
