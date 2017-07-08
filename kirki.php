3c80505b51a9f684a217e887c0e21177e885fd75
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-08 15:41:16 +0300

    Version 3.0.9

--- kirki.php
+++ kirki.php
@@ -5,7 +5,7 @@
  * Description:   The ultimate WordPress Customizer Toolkit
  * Author:        Aristeides Stathopoulos
  * Author URI:    http://aristeides.com
- * Version:       3.0.8
+ * Version:       3.0.9
  * Text Domain:   kirki
  *
  * GitHub Plugin URI: aristath/kirki
