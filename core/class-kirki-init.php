a6d72c94b7f11f21356612d12f975aecad58c869
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-15 09:20:25 +0300

    Don't add this message for now, we'll see how it goes.

--- core/class-kirki-init.php
+++ core/class-kirki-init.php
@@ -285,7 +285,7 @@ class Kirki_Init {
 	 * @return bool
 	 */
 	public static function is_plugin() {
-		_doing_it_wrong( __METHOD__, esc_attr__( 'We detected you\'re using Kirki_Init::is_plugin(). Please use Kirki_Util::is_plugin() instead.', 'kirki' ), '3.0.10' );
+		/* _doing_it_wrong( __METHOD__, esc_attr__( 'We detected you\'re using Kirki_Init::is_plugin(). Please use Kirki_Util::is_plugin() instead.', 'kirki' ), '3.0.10' ); */
 		// Return result using the Kirki_Util class.
 		return Kirki_Util::is_plugin();
 	}
