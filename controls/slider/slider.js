8a2fe59afda91235e8735ff2a0f5f74b4af501f4
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-20 14:32:11 +0300

    slider

--- controls/slider/slider.js
+++ controls/slider/slider.js
@@ -1,55 +1,211 @@
-wp.customize.controlConstructor['kirki-slider'] = wp.customize.Control.extend({
+( function( api, $ ) {
+	'use strict';
 
-	// When we're finished loading continue processing
-	ready: function() {
+	/**
+	 * A dynamic slider control.
+	 *
+	 * @class
+	 * @augments wp.customize.Control
+	 * @augments wp.customize.Class
+	 */
+	api.kirkiSliderControl = api.Control.extend({
 
-		'use strict';
+		initialize: function( id, options ) {
+			var control = this,
+			    args    = options || {};
 
-		var control = this;
+			args.params = args.params || {};
+			if ( ! args.params.type ) {
+				args.params.type = 'kirki-slider';
+			}
+			if ( ! args.params.content ) {
+				args.params.content = jQuery( '<li></li>' );
+				args.params.content.attr( 'id', 'customize-control-' + id.replace( /]/g, '' ).replace( /\[/g, '-' ) );
+				args.params.content.attr( 'class', 'customize-control customize-control-' + args.params.type );
+			}
 
-		// Init the control.
-		if ( ! _.isUndefined( window.kirkiControlLoader ) && _.isFunction( kirkiControlLoader ) ) {
-			kirkiControlLoader( control );
-		} else {
-			control.initKirkiControl();
-		}
-	},
+			control.propertyElements = [];
+			api.Control.prototype.initialize.call( control, id, args );
+		},
 
-	initKirkiControl: function() {
+		/**
+		 * Add bidirectional data binding links between inputs and the setting(s).
+		 *
+		 * This is copied from wp.customize.Control.prototype.initialize(). It
+		 * should be changed in Core to be applied once the control is embedded.
+		 *
+		 * @private
+		 * @returns {void}
+		 */
+		_setUpSettingRootLinks: function() {
+			var control = this,
+			    nodes   = control.container.find( '[data-customize-setting-link]' );
 
-		'use strict';
+			nodes.each( function() {
+				var node = jQuery( this ),
+				    name;
 
-		var control = this,
-		    value,
-		    thisInput,
-		    inputDefault,
-		    changeAction;
-
-		control.container.find( '.kirki-controls-loading-spinner' ).hide();
-
-		// Update the text value
-		jQuery( 'input[type=range]' ).on( 'mousedown', function() {
-			value = jQuery( this ).attr( 'value' );
-			jQuery( this ).mousemove( function() {
-				value = jQuery( this ).attr( 'value' );
-				jQuery( this ).closest( 'label' ).find( '.kirki_range_value .value' ).text( value );
+				api( node.data( 'customizeSettingLink' ), function( setting ) {
+					var element = new api.Element( node );
+					control.elements.push( element );
+					element.sync( setting );
+					element.set( setting() );
+				});
 			});
-		});
+		},
 
-		// Handle the reset button
-		jQuery( '.kirki-slider-reset' ).click( function() {
-			thisInput    = jQuery( this ).closest( 'label' ).find( 'input' );
-			inputDefault = thisInput.data( 'reset_value' );
-			thisInput.val( inputDefault );
-			thisInput.change();
-			jQuery( this ).closest( 'label' ).find( '.kirki_range_value .value' ).text( inputDefault );
-		});
+		/**
+		 * Add bidirectional data binding links between inputs and the setting properties.
+		 *
+		 * @private
+		 * @returns {void}
+		 */
+		_setUpSettingPropertyLinks: function() {
+			var control = this,
+			    nodes;
 
-		changeAction = ( 'postMessage' === control.setting.transport ) ? 'mousemove change' : 'change';
+			if ( ! control.setting ) {
+				return;
+			}
 
-		// Save changes.
-		this.container.on( changeAction, 'input', function() {
-			control.setting.set( jQuery( this ).val() );
-		});
-	}
-});
+			nodes = control.container.find( '[data-customize-setting-property-link]' );
+
+			nodes.each( function() {
+				var node = jQuery( this ),
+				    name,
+				    element,
+				    propertyName = node.data( 'customizeSettingPropertyLink' );
+
+				element = new api.Element( node );
+				control.propertyElements.push( element );
+				element.set( control.setting()[ propertyName ] );
+
+				element.bind( function( newPropertyValue ) {
+					var newSetting = control.setting();
+					if ( newPropertyValue === newSetting[ propertyName ] ) {
+						return;
+					}
+					newSetting = _.clone( newSetting );
+					newSetting[ propertyName ] = newPropertyValue;
+					control.setting.set( newSetting );
+				} );
+				control.setting.bind( function( newValue ) {
+					if ( newValue[ propertyName ] !== element.get() ) {
+						element.set( newValue[ propertyName ] );
+					}
+				} );
+			});
+		},
+
+		/**
+		 * @inheritdoc
+		 */
+		ready: function() {
+			var control = this;
+
+			control._setUpSettingRootLinks();
+			control._setUpSettingPropertyLinks();
+
+			api.Control.prototype.ready.call( control );
+
+			control.deferred.embedded.done( function() {
+				control.initKirkiControl();
+			});
+		},
+
+		/**
+		 * Embed the control in the document.
+		 *
+		 * Override the embed() method to do nothing,
+		 * so that the control isn't embedded on load,
+		 * unless the containing section is already expanded.
+		 *
+		 * @returns {void}
+		 */
+		embed: function() {
+			var control   = this,
+			    sectionId = control.section();
+
+			if ( ! sectionId ) {
+				return;
+			}
+
+			api.section( sectionId, function( section ) {
+				if ( section.expanded() || api.settings.autofocus.control === control.id ) {
+					control.actuallyEmbed();
+				} else {
+					section.expanded.bind( function( expanded ) {
+						if ( expanded ) {
+							control.actuallyEmbed();
+						}
+					} );
+				}
+			} );
+		},
+
+		/**
+		 * Deferred embedding of control when actually
+		 *
+		 * This function is called in Section.onChangeExpanded() so the control
+		 * will only get embedded when the Section is first expanded.
+		 *
+		 * @returns {void}
+		 */
+		actuallyEmbed: function() {
+			var control = this;
+			if ( 'resolved' === control.deferred.embedded.state() ) {
+				return;
+			}
+			control.renderContent();
+			control.deferred.embedded.resolve(); // This triggers control.ready().
+		},
+
+		/**
+		 * This is not working with autofocus.
+		 *
+		 * @param {object} [args] Args.
+		 * @returns {void}
+		 */
+		focus: function( args ) {
+			var control = this;
+			control.actuallyEmbed();
+			api.Control.prototype.focus.call( control, args );
+		},
+
+		initKirkiControl: function() {
+			var control = this,
+			    value,
+			    thisInput,
+			    inputDefault,
+			    changeAction;
+
+			// Update the text value
+			jQuery( 'input[type=range]' ).on( 'mousedown', function() {
+				value = jQuery( this ).attr( 'value' );
+				jQuery( this ).mousemove( function() {
+					value = jQuery( this ).attr( 'value' );
+					jQuery( this ).closest( 'label' ).find( '.kirki_range_value .value' ).text( value );
+				});
+			});
+
+			// Handle the reset button
+			jQuery( '.kirki-slider-reset' ).click( function() {
+				thisInput    = jQuery( this ).closest( 'label' ).find( 'input' );
+				inputDefault = thisInput.data( 'reset_value' );
+				thisInput.val( inputDefault );
+				thisInput.change();
+				jQuery( this ).closest( 'label' ).find( '.kirki_range_value .value' ).text( inputDefault );
+			});
+
+			changeAction = ( 'postMessage' === control.setting.transport ) ? 'mousemove change' : 'change';
+
+			// Save changes.
+			control.container.on( changeAction, 'input', function() {
+				control.setting.set( jQuery( this ).val() );
+			});
+		}
+	});
+
+	api.controlConstructor['kirki-slider'] = api.kirkiSliderControl;
+
+})( wp.customize, jQuery );
