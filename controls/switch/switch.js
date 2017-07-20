3e43326c89b0f1b9a2207b316edbbed8a807f3a4
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-20 14:26:10 +0300

    switch

--- controls/switch/switch.js
+++ controls/switch/switch.js
@@ -1,41 +1,192 @@
-wp.customize.controlConstructor['kirki-switch'] = wp.customize.Control.extend({
+( function( api, $ ) {
+	'use strict';
 
-	// When we're finished loading continue processing
-	ready: function() {
+	/**
+	 * A dynamic switch control.
+	 *
+	 * @class
+	 * @augments wp.customize.Control
+	 * @augments wp.customize.Class
+	 */
+	api.kirkiSwitchControl = api.Control.extend({
 
-		'use strict';
+		initialize: function( id, options ) {
+			var control = this,
+			    args    = options || {};
 
-		var control = this;
+			args.params = args.params || {};
+			if ( ! args.params.type ) {
+				args.params.type = 'kirki-switch';
+			}
+			if ( ! args.params.content ) {
+				args.params.content = jQuery( '<li></li>' );
+				args.params.content.attr( 'id', 'customize-control-' + id.replace( /]/g, '' ).replace( /\[/g, '-' ) );
+				args.params.content.attr( 'class', 'customize-control customize-control-' + args.params.type );
+			}
 
-		// Init the control.
-		if ( ! _.isUndefined( window.kirkiControlLoader ) && _.isFunction( kirkiControlLoader ) ) {
-			kirkiControlLoader( control );
-		} else {
-			control.initKirkiControl();
+			control.propertyElements = [];
+			api.Control.prototype.initialize.call( control, id, args );
+		},
+
+		/**
+		 * Add bidirectional data binding links between inputs and the setting(s).
+		 *
+		 * This is copied from wp.customize.Control.prototype.initialize(). It
+		 * should be changed in Core to be applied once the control is embedded.
+		 *
+		 * @private
+		 * @returns {void}
+		 */
+		_setUpSettingRootLinks: function() {
+			var control = this,
+			    nodes   = control.container.find( '[data-customize-setting-link]' );
+
+			nodes.each( function() {
+				var node = jQuery( this ),
+				    name;
+
+				api( node.data( 'customizeSettingLink' ), function( setting ) {
+					var element = new api.Element( node );
+					control.elements.push( element );
+					element.sync( setting );
+					element.set( setting() );
+				});
+			});
+		},
+
+		/**
+		 * Add bidirectional data binding links between inputs and the setting properties.
+		 *
+		 * @private
+		 * @returns {void}
+		 */
+		_setUpSettingPropertyLinks: function() {
+			var control = this,
+			    nodes;
+
+			if ( ! control.setting ) {
+				return;
+			}
+
+			nodes = control.container.find( '[data-customize-setting-property-link]' );
+
+			nodes.each( function() {
+				var node = jQuery( this ),
+				    name,
+				    element,
+				    propertyName = node.data( 'customizeSettingPropertyLink' );
+
+				element = new api.Element( node );
+				control.propertyElements.push( element );
+				element.set( control.setting()[ propertyName ] );
+
+				element.bind( function( newPropertyValue ) {
+					var newSetting = control.setting();
+					if ( newPropertyValue === newSetting[ propertyName ] ) {
+						return;
+					}
+					newSetting = _.clone( newSetting );
+					newSetting[ propertyName ] = newPropertyValue;
+					control.setting.set( newSetting );
+				} );
+				control.setting.bind( function( newValue ) {
+					if ( newValue[ propertyName ] !== element.get() ) {
+						element.set( newValue[ propertyName ] );
+					}
+				} );
+			});
+		},
+
+		/**
+		 * @inheritdoc
+		 */
+		ready: function() {
+			var control = this;
+
+			control._setUpSettingRootLinks();
+			control._setUpSettingPropertyLinks();
+
+			api.Control.prototype.ready.call( control );
+
+			control.deferred.embedded.done( function() {
+				var checkboxValue = control.setting._value,
+				    on            = jQuery( control.container.find( '.switch-on' ) ),
+				    off           = jQuery( control.container.find( '.switch-off' ) );
+
+				// CSS modifications depending on label sizes.
+				jQuery( control.container.find( '.switch label ' ) ).css( 'width', ( on.width() + off.width() + 40 ) + 'px' );
+				jQuery( '#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) ).append(
+					'<style>#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) + ' .switch label:after{width:' + ( on.width() + 10 ) + 'px;}#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) + ' .switch input:checked + label:after{left:' + ( on.width() + 25 ) + 'px;width:' + ( off.width() + 10 ) + 'px;}</style>'
+				);
+
+				// Save the value
+				control.container.on( 'change', 'input', function() {
+					checkboxValue = ( jQuery( this ).is( ':checked' ) ) ? true : false;
+					control.setting.set( checkboxValue );
+				});
+			});
+		},
+
+		/**
+		 * Embed the control in the document.
+		 *
+		 * Override the embed() method to do nothing,
+		 * so that the control isn't embedded on load,
+		 * unless the containing section is already expanded.
+		 *
+		 * @returns {void}
+		 */
+		embed: function() {
+			var control   = this,
+			    sectionId = control.section();
+
+			if ( ! sectionId ) {
+				return;
+			}
+
+			api.section( sectionId, function( section ) {
+				if ( section.expanded() || api.settings.autofocus.control === control.id ) {
+					control.actuallyEmbed();
+				} else {
+					section.expanded.bind( function( expanded ) {
+						if ( expanded ) {
+							control.actuallyEmbed();
+						}
+					} );
+				}
+			} );
+		},
+
+		/**
+		 * Deferred embedding of control when actually
+		 *
+		 * This function is called in Section.onChangeExpanded() so the control
+		 * will only get embedded when the Section is first expanded.
+		 *
+		 * @returns {void}
+		 */
+		actuallyEmbed: function() {
+			var control = this;
+			if ( 'resolved' === control.deferred.embedded.state() ) {
+				return;
+			}
+			control.renderContent();
+			control.deferred.embedded.resolve(); // This triggers control.ready().
+		},
+
+		/**
+		 * This is not working with autofocus.
+		 *
+		 * @param {object} [args] Args.
+		 * @returns {void}
+		 */
+		focus: function( args ) {
+			var control = this;
+			control.actuallyEmbed();
+			api.Control.prototype.focus.call( control, args );
 		}
-	},
+	});
 
-	initKirkiControl: function() {
+	api.controlConstructor['kirki-switch'] = api.kirkiSwitchControl;
 
-		'use strict';
-
-		var control       = this,
-		    checkboxValue = control.setting._value,
-		    on            = jQuery( control.container.find( '.switch-on' ) ),
-		    off           = jQuery( control.container.find( '.switch-off' ) );
-
-		control.container.find( '.kirki-controls-loading-spinner' ).hide();
-
-		// CSS modifications depending on label sizes.
-		jQuery( control.container.find( '.switch label ' ) ).css( 'width', ( on.width() + off.width() + 40 ) + 'px' );
-		jQuery( '#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) ).append(
-			'<style>#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) + ' .switch label:after{width:' + ( on.width() + 10 ) + 'px;}#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) + ' .switch input:checked + label:after{left:' + ( on.width() + 25 ) + 'px;width:' + ( off.width() + 10 ) + 'px;}</style>'
-		);
-
-		// Save the value
-		this.container.on( 'change', 'input', function() {
-			checkboxValue = ( jQuery( this ).is( ':checked' ) ) ? true : false;
-			control.setting.set( checkboxValue );
-		});
-	}
-});
+})( wp.customize, jQuery );
