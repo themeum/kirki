ce2b616057a92afb4e9acb810d18f365d3830e07
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-22 14:07:29 +0300

    code: use kirkiDynamicControl

--- controls/code/code.js
+++ controls/code/code.js
@@ -1,215 +1,41 @@
-( function( api, $ ) {
-	'use strict';
+wp.customize.controlConstructor['kirki-code'] = wp.customize.kirkiDynamicControl.extend({
 
-	/**
-	 * A dynamic code control.
-	 *
-	 * @class
-	 * @augments wp.customize.Control
-	 * @augments wp.customize.Class
-	 */
-	api.kirkiCodeControl = api.Control.extend({
+	initKirkiControl: function() {
 
-		initialize: function( id, options ) {
-			var control = this,
-			    args    = options || {};
+		var control  = this,
+		    element  = control.container.find( '.kirki-codemirror-editor' ),
+		    language = ( 'html' === control.params.choices.language ) ? { name: 'htmlmixed' } : control.params.choices.language,
+		    editor,
+		    container,
+		    height;
 
-			args.params = args.params || {};
-			if ( ! args.params.type ) {
-				args.params.type = 'kirki-code';
-			}
-			if ( ! args.params.content ) {
-				args.params.content = jQuery( '<li></li>' );
-				args.params.content.attr( 'id', 'customize-control-' + id.replace( /]/g, '' ).replace( /\[/g, '-' ) );
-				args.params.content.attr( 'class', 'customize-control customize-control-' + args.params.type );
-			}
+		editor = CodeMirror.fromTextArea( element[0], {
+			value:        control.setting._value,
+			mode:         language,
+			lineNumbers:  true,
+			lineWrapping: true,
+			theme:        control.params.choices.theme
+		});
 
-			control.propertyElements = [];
-			api.Control.prototype.initialize.call( control, id, args );
-		},
-
-		/**
-		 * Add bidirectional data binding links between inputs and the setting(s).
-		 *
-		 * This is copied from wp.customize.Control.prototype.initialize(). It
-		 * should be changed in Core to be applied once the control is embedded.
-		 *
-		 * @private
-		 * @returns {void}
-		 */
-		_setUpSettingRootLinks: function() {
-			var control = this,
-			    nodes   = control.container.find( '[data-customize-setting-link]' );
-
-			nodes.each( function() {
-				var node = jQuery( this );
-
-				api( node.data( 'customizeSettingLink' ), function( setting ) {
-					var element = new api.Element( node );
-					control.elements.push( element );
-					element.sync( setting );
-					element.set( setting() );
-				});
-			});
-		},
-
-		/**
-		 * Add bidirectional data binding links between inputs and the setting properties.
-		 *
-		 * @private
-		 * @returns {void}
-		 */
-		_setUpSettingPropertyLinks: function() {
-			var control = this,
-			    nodes;
-
-			if ( ! control.setting ) {
-				return;
-			}
-
-			nodes = control.container.find( '[data-customize-setting-property-link]' );
-
-			nodes.each( function() {
-				var node = jQuery( this ),
-				    element,
-				    propertyName = node.data( 'customizeSettingPropertyLink' );
-
-				element = new api.Element( node );
-				control.propertyElements.push( element );
-				element.set( control.setting()[ propertyName ] );
-
-				element.bind( function( newPropertyValue ) {
-					var newSetting = control.setting();
-					if ( newPropertyValue === newSetting[ propertyName ] ) {
-						return;
-					}
-					newSetting = _.clone( newSetting );
-					newSetting[ propertyName ] = newPropertyValue;
-					control.setting.set( newSetting );
+		if ( ! _.isUndefined( control.params.choices.height ) ) {
+			height = Number( control.params.choices.height );
+			if ( ! isNaN( height ) ) {
+				container = control.container.find( '.codemirror-kirki-wrapper' );
+				jQuery( container ).css( 'max-height', function() {
+					return control.params.choices.height;
 				} );
-				control.setting.bind( function( newValue ) {
-					if ( newValue[ propertyName ] !== element.get() ) {
-						element.set( newValue[ propertyName ] );
-					}
-				} );
-			});
-		},
-
-		/**
-		 * @inheritdoc
-		 */
-		ready: function() {
-			var control = this;
-
-			control._setUpSettingRootLinks();
-			control._setUpSettingPropertyLinks();
-
-			api.Control.prototype.ready.call( control );
-
-			control.deferred.embedded.done( function() {
-				control.initKirkiControl();
-			});
-		},
-
-		/**
-		 * Embed the control in the document.
-		 *
-		 * Override the embed() method to do nothing,
-		 * so that the control isn't embedded on load,
-		 * unless the containing section is already expanded.
-		 *
-		 * @returns {void}
-		 */
-		embed: function() {
-			var control   = this,
-			    sectionId = control.section();
-
-			if ( ! sectionId ) {
-				return;
+				editor.setSize( null, control.params.choices.height );
 			}
-
-			api.section( sectionId, function( section ) {
-				if ( section.expanded() || api.settings.autofocus.control === control.id ) {
-					control.actuallyEmbed();
-				} else {
-					section.expanded.bind( function( expanded ) {
-						if ( expanded ) {
-							control.actuallyEmbed();
-						}
-					} );
-				}
-			} );
-		},
-
-		/**
-		 * Deferred embedding of control when actually
-		 *
-		 * This function is called in Section.onChangeExpanded() so the control
-		 * will only get embedded when the Section is first expanded.
-		 *
-		 * @returns {void}
-		 */
-		actuallyEmbed: function() {
-			var control = this;
-			if ( 'resolved' === control.deferred.embedded.state() ) {
-				return;
-			}
-			control.renderContent();
-			control.deferred.embedded.resolve(); // This triggers control.ready().
-		},
-
-		/**
-		 * This is not working with autofocus.
-		 *
-		 * @param {object} [args] Args.
-		 * @returns {void}
-		 */
-		focus: function( args ) {
-			var control = this;
-			control.actuallyEmbed();
-			api.Control.prototype.focus.call( control, args );
-		},
-
-		initKirkiControl: function() {
-
-			var control  = this,
-			    element  = control.container.find( '.kirki-codemirror-editor' ),
-			    language = ( 'html' === control.params.choices.language ) ? { name: 'htmlmixed' } : control.params.choices.language,
-			    editor,
-			    container,
-			    height;
-
-			editor = CodeMirror.fromTextArea( element[0], {
-				value:        control.setting._value,
-				mode:         language,
-				lineNumbers:  true,
-				lineWrapping: true,
-				theme:        control.params.choices.theme
-			});
-
-			if ( ! _.isUndefined( control.params.choices.height ) ) {
-				height = Number( control.params.choices.height );
-				if ( ! isNaN( height ) ) {
-					container = control.container.find( '.codemirror-kirki-wrapper' );
-					jQuery( container ).css( 'max-height', function() {
-						return control.params.choices.height;
-					} );
-					editor.setSize( null, control.params.choices.height );
-				}
-			}
-
-			// On change make sure we infor the Customizer API
-			editor.on( 'change', function() {
-				control.setting.set( editor.getValue() );
-			});
-
-			// Hack to refresh the editor when we open a section
-			element.parents( '.accordion-section' ).on( 'click', function() {
-				editor.refresh();
-			});
 		}
-	});
 
-	api.controlConstructor['kirki-code'] = api.kirkiCodeControl;
+		// On change make sure we infor the Customizer API
+		editor.on( 'change', function() {
+			control.setting.set( editor.getValue() );
+		});
 
-})( wp.customize, jQuery );
+		// Hack to refresh the editor when we open a section
+		element.parents( '.accordion-section' ).on( 'click', function() {
+			editor.refresh();
+		});
+	}
+});
