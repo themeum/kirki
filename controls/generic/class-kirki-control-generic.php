f4ae60d369b5b8274470fa18f7bbaf5dc6e0285c
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-22 14:30:32 +0300

    generic: use kirkiDynamicControl

--- controls/generic/class-kirki-control-generic.php
+++ controls/generic/class-kirki-control-generic.php
@@ -51,7 +51,8 @@ class Kirki_Control_Generic extends WP_Customize_Control {
 	 */
 	public function enqueue() {
 
-		wp_enqueue_script( 'kirki-generic', trailingslashit( Kirki::$url ) . 'controls/generic/generic.js', array( 'jquery', 'customize-base' ), false, true );
+		wp_enqueue_script( 'kirki-dynamic-control', trailingslashit( Kirki::$url ) . 'assets/js/dynamic-control.js', array( 'jquery', 'customize-base' ), false, true );
+		wp_enqueue_script( 'kirki-generic', trailingslashit( Kirki::$url ) . 'controls/generic/generic.js', array( 'jquery', 'customize-base', 'kirki-dynamic-control' ), false, true );
 		wp_enqueue_style( 'kirki-generic-css', trailingslashit( Kirki::$url ) . 'controls/generic/generic.css', null );
 	}
 
