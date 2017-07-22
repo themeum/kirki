e28b9079b0e7a4ae98baa718d36d4c77b21262ef
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-22 14:38:31 +0300

    palette: use kirkiDynamicControl

--- controls/palette/class-kirki-control-palette.php
+++ controls/palette/class-kirki-control-palette.php
@@ -50,7 +50,8 @@ class Kirki_Control_Palette extends WP_Customize_Control {
 	 */
 	public function enqueue() {
 
-		wp_enqueue_script( 'kirki-palette', trailingslashit( Kirki::$url ) . 'controls/palette/palette.js', array( 'jquery', 'customize-base', 'jquery-ui-button' ), false, true );
+		wp_enqueue_script( 'kirki-dynamic-control', trailingslashit( Kirki::$url ) . 'assets/js/dynamic-control.js', array( 'jquery', 'customize-base' ), false, true );
+		wp_enqueue_script( 'kirki-palette', trailingslashit( Kirki::$url ) . 'controls/palette/palette.js', array( 'jquery', 'customize-base', 'kirki-dynamic-control', 'jquery-ui-button' ), false, true );
 		wp_enqueue_style( 'kirki-palette-css', trailingslashit( Kirki::$url ) . 'controls/palette/palette.css', null );
 	}
 
