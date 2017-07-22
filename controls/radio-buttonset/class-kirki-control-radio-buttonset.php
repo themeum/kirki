a57ac68e92df53ccf903ca8c29faafd294a90249
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-22 14:50:40 +0300

    radio-buttonset: use kirkiDynamicControl

--- controls/radio-buttonset/class-kirki-control-radio-buttonset.php
+++ controls/radio-buttonset/class-kirki-control-radio-buttonset.php
@@ -50,7 +50,8 @@ class Kirki_Control_Radio_Buttonset extends WP_Customize_Control {
 	 */
 	public function enqueue() {
 
-		wp_enqueue_script( 'kirki-radio-buttonset', trailingslashit( Kirki::$url ) . 'controls/radio-buttonset/radio-buttonset.js', array( 'jquery', 'customize-base' ), false, true );
+		wp_enqueue_script( 'kirki-dynamic-control', trailingslashit( Kirki::$url ) . 'assets/js/dynamic-control.js', array( 'jquery', 'customize-base' ), false, true );
+		wp_enqueue_script( 'kirki-radio-buttonset', trailingslashit( Kirki::$url ) . 'controls/radio-buttonset/radio-buttonset.js', array( 'jquery', 'customize-base', 'kirki-dynamic-control' ), false, true );
 		wp_enqueue_style( 'kirki-radio-buttonset-css', trailingslashit( Kirki::$url ) . 'controls/radio-buttonset/radio-buttonset.css', null );
 	}
 
