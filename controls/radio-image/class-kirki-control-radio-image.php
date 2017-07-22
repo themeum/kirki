741c4902edc6a209b15fad94c803d15de73b526f
Aristeides Stathopoulos <aristath@gmail.com>
2017-07-22 14:50:53 +0300

    radio-image: use kirkiDynamicControl

--- controls/radio-image/class-kirki-control-radio-image.php
+++ controls/radio-image/class-kirki-control-radio-image.php
@@ -50,7 +50,8 @@ class Kirki_Control_Radio_Image extends WP_Customize_Control {
 	 */
 	public function enqueue() {
 
-		wp_enqueue_script( 'kirki-radio-image', trailingslashit( Kirki::$url ) . 'controls/radio-image/radio-image.js', array( 'jquery', 'customize-base' ), false, true );
+		wp_enqueue_script( 'kirki-dynamic-control', trailingslashit( Kirki::$url ) . 'assets/js/dynamic-control.js', array( 'jquery', 'customize-base' ), false, true );
+		wp_enqueue_script( 'kirki-radio-image', trailingslashit( Kirki::$url ) . 'controls/radio-image/radio-image.js', array( 'jquery', 'customize-base', 'kirki-dynamic-control' ), false, true );
 		wp_enqueue_style( 'kirki-radio-image-css', trailingslashit( Kirki::$url ) . 'controls/radio-image/radio-image.css', null );
 	}
 
