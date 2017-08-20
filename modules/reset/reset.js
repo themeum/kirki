f00bb73fd483630e58491c8cbeff4f6e3da566ff
Aristeides Stathopoulos <aristath@gmail.com>
2017-08-20 17:39:36 +0300

    Refactoring the reset module to use the JS functions from the controls.

--- modules/reset/reset.js
+++ modules/reset/reset.js
@@ -1,28 +1,29 @@
 jQuery( document ).ready( function() {
-
 	'use strict';
 
+	// Add reset buttons to sections.
 	wp.customize.section.each( function( section ) {
-
 		var link = '<a href="#" class="kirki-reset-section" data-reset-section-id="' + section.id + '">' + kirkiResetButtonLabel['reset-with-icon'] + '</a>';
-
 		jQuery( link ).appendTo( '#sub-accordion-section-' + section.id + ' .customize-section-title > h3' );
-
 	});
 
+	// Reset controls on click.
 	jQuery( 'a.kirki-reset-section' ).on( 'click', function() {
-
 		var id       = jQuery( this ).data( 'reset-section-id' ),
 		    controls = wp.customize.section( id ).controls();
 
-		// Loop controls
 		_.each( controls, function( control, i ) {
 
 			// Set value to default
-			kirkiSetSettingValue.set( controls[ i ].id, control.params['default'] );
+			control.setting.set( control.params['default'] );
+			if ( _.isFunction( control.kirkiSetValue ) ) {
+				control.kirkiSetValue( control.params['default'] );
+			}
 
+			// Reset the controls visually.
+			if ( _.isFunction( control.kirkiSetControlValue ) ) {
+				control.kirkiSetControlValue( control.params['default'] );
+			}
 		});
-
 	});
-
 });
