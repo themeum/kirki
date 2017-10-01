f7f423d0c8fb54d7ea7e0cec2230daf31be32fc3
Aristeides Stathopoulos <aristath@gmail.com>
2017-10-01 17:43:00 +0300

    Add a Kirki_Control_Base class.

--- controls/php/class-kirki-control-color.php
+++ controls/php/class-kirki-control-color.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://github.com/23r9i0/wp-color-picker-alpha
  */
-class Kirki_Control_Color extends WP_Customize_Control {
+class Kirki_Control_Color extends Kirki_Control_Base {
 
 	/**
 	 * The control type.
@@ -38,69 +38,6 @@ class Kirki_Control_Color extends WP_Customize_Control {
 	public $palette = true;
 
 	/**
-	 * Used to automatically generate all CSS output.
-	 *
-	 * @access public
-	 * @var array
-	 */
-	public $output = array();
-
-	/**
-	 * Data type
-	 *
-	 * @access public
-	 * @var string
-	 */
-	public $option_type = 'theme_mod';
-
-	/**
-	 * Constructor.
-	 *
-	 * Supplied `$args` override class property defaults.
-	 *
-	 * If `$args['settings']` is not defined, use the $id as the setting ID.
-	 *
-	 * @since 3.0.0
-	 *
-	 * @param WP_Customize_Manager $manager Customizer bootstrap instance.
-	 * @param string               $id      Control ID.
-	 * @param array                $args    {
-	 *     Optional. Arguments to override class property defaults.
-	 *
-	 *     @type int                  $instance_number Order in which this instance was created in relation
-	 *                                                 to other instances.
-	 *     @type WP_Customize_Manager $manager         Customizer bootstrap instance.
-	 *     @type string               $id              Control ID.
-	 *     @type array                $settings        All settings tied to the control. If undefined, `$id` will
-	 *                                                 be used.
-	 *     @type string               $setting         The primary setting for the control (if there is one).
-	 *                                                 Default 'default'.
-	 *     @type int                  $priority        Order priority to load the control. Default 10.
-	 *     @type string               $section         Section the control belongs to. Default empty.
-	 *     @type string               $label           Label for the control. Default empty.
-	 *     @type string               $description     Description for the control. Default empty.
-	 *     @type array                $choices         List of choices for 'radio' or 'select' type controls, where
-	 *                                                 values are the keys, and labels are the values.
-	 *                                                 Default empty array.
-	 *     @type array                $input_attrs     List of custom input attributes for control output, where
-	 *                                                 attribute names are the keys and values are the values. Not
-	 *                                                 used for 'checkbox', 'radio', 'select', 'textarea', or
-	 *                                                 'dropdown-pages' control types. Default empty array.
-	 *     @type array                $json            Deprecated. Use WP_Customize_Control::json() instead.
-	 *     @type string               $type            Control type. Core controls include 'text', 'checkbox',
-	 *                                                 'textarea', 'radio', 'select', and 'dropdown-pages'. Additional
-	 *                                                 input types such as 'email', 'url', 'number', 'hidden', and
-	 *                                                 'date' are supported implicitly. Default 'text'.
-	 * }
-	 */
-	public function __construct( $manager, $id, $args = array() ) {
-
-		parent::__construct( $manager, $id, $args );
-		add_action( 'customize_controls_enqueue_scripts', array( $this, 'enqueue_scripts' ), 999 );
-
-	}
-
-	/**
 	 * Refresh the parameters passed to the JavaScript via JSON.
 	 *
 	 * @access public
@@ -108,23 +45,21 @@ class Kirki_Control_Color extends WP_Customize_Control {
 	public function to_json() {
 		parent::to_json();
 
-		$this->json['default'] = $this->setting->default;
-		if ( isset( $this->default ) ) {
-			$this->json['default'] = $this->default;
-		}
-		$this->json['output']  = $this->output;
-		$this->json['value']   = $this->value();
-		$this->json['choices'] = $this->choices;
-		$this->json['link']    = $this->get_link();
-		$this->json['id']      = $this->id;
-
-		$this->json['inputAttrs'] = '';
-		foreach ( $this->input_attrs as $attr => $value ) {
-			$this->json['inputAttrs'] .= $attr . '="' . esc_attr( $value ) . '" ';
-		}
-
 		$this->json['palette'] = $this->palette;
 		$this->choices['alpha'] = ( isset( $this->choices['alpha'] ) && $this->choices['alpha'] ) ? 'true' : 'false';
+	}
+
+	/**
+	 * Extra script dependencies.
+	 *
+	 * @since 3.1.0
+	 * @return array
+	 */
+	public function kirki_script_dependencies() {
+
+		return array(
+			'wp-color-picker-alpha',
+		);
 	}
 
 	/**
@@ -132,31 +67,9 @@ class Kirki_Control_Color extends WP_Customize_Control {
 	 *
 	 * @access public
 	 */
-	public function enqueue_scripts() {
+	public function enqueue() {
 
 		wp_enqueue_script( 'wp-color-picker-alpha', trailingslashit( Kirki::$url ) . 'assets/vendor/wp-color-picker-alpha/wp-color-picker-alpha.js', array( 'wp-color-picker' ), '1.2', true );
 		wp_enqueue_style( 'wp-color-picker' );
-
-		wp_enqueue_script( 'kirki-control', trailingslashit( Kirki::$url ) . 'controls/js/kirki.control.js', array( 'jquery', 'customize-base', 'wp-color-picker-alpha' ), false, true );
-		wp_enqueue_script( 'kirki-input', trailingslashit( Kirki::$url ) . 'controls/js/kirki.input.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-setting', trailingslashit( Kirki::$url ) . 'controls/js/kirki.setting.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-util', trailingslashit( Kirki::$url ) . 'controls/js/kirki.util.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-value', trailingslashit( Kirki::$url ) . 'controls/js/kirki.value.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-dynamic-control', trailingslashit( Kirki::$url ) . 'controls/js/dynamic-control.js', array( 'jquery', 'customize-base', 'kirki-control', 'kirki-input', 'kirki-setting', 'kirki-util', 'kirki-value' ), false, true );
-
-		wp_enqueue_script( 'kirki-controls', trailingslashit( Kirki::$url ) . 'controls/js/controls.js', array( 'jquery', 'kirki-dynamic-control', 'customize-base' ), false, true );
-		wp_enqueue_style( 'kirki-controls-css', trailingslashit( Kirki::$url ) . 'controls/css/controls.css', null );
 	}
-
-	/**
-	 * An Underscore (JS) template for this control's content (but not its container).
-	 *
-	 * Class variables for this control class are available in the `data` JS object;
-	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
-	 *
-	 * @see WP_Customize_Control::print_template()
-	 *
-	 * @access protected
-	 */
-	protected function content_template() {}
 }
--- controls/php/class-kirki-control-radio.php
+++ controls/php/class-kirki-control-radio.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Radio control
  */
-class Kirki_Control_Radio extends WP_Customize_Control {
+class Kirki_Control_Radio extends Kirki_Control_Base {
 
 	/**
 	 * The control type.
@@ -26,75 +26,4 @@ class Kirki_Control_Radio extends WP_Customize_Control {
 	 * @var string
 	 */
 	public $type = 'kirki-radio';
-
-	/**
-	 * Used to automatically generate all CSS output.
-	 *
-	 * @access public
-	 * @var array
-	 */
-	public $output = array();
-
-	/**
-	 * Data type
-	 *
-	 * @access public
-	 * @var string
-	 */
-	public $option_type = 'theme_mod';
-
-	/**
-	 * Enqueue control related scripts/styles.
-	 *
-	 * @access public
-	 */
-	public function enqueue() {
-
-		wp_enqueue_script( 'kirki-control', trailingslashit( Kirki::$url ) . 'controls/js/kirki.control.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-input', trailingslashit( Kirki::$url ) . 'controls/js/kirki.input.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-setting', trailingslashit( Kirki::$url ) . 'controls/js/kirki.setting.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-util', trailingslashit( Kirki::$url ) . 'controls/js/kirki.util.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-value', trailingslashit( Kirki::$url ) . 'controls/js/kirki.value.js', array( 'jquery', 'customize-base' ), false, true );
-		wp_enqueue_script( 'kirki-dynamic-control', trailingslashit( Kirki::$url ) . 'controls/js/dynamic-control.js', array( 'jquery', 'customize-base', 'kirki-control', 'kirki-input', 'kirki-setting', 'kirki-util', 'kirki-value' ), false, true );
-
-		wp_enqueue_script( 'kirki-controls', trailingslashit( Kirki::$url ) . 'controls/js/controls.js', array( 'jquery', 'kirki-dynamic-control', 'customize-base' ), false, true );
-		wp_enqueue_style( 'kirki-controls-css', trailingslashit( Kirki::$url ) . 'controls/css/controls.css', null );
-	}
-
-	/**
-	 * Refresh the parameters passed to the JavaScript via JSON.
-	 *
-	 * @see WP_Customize_Control::to_json()
-	 */
-	public function to_json() {
-		parent::to_json();
-
-		$this->json['default'] = $this->setting->default;
-		if ( isset( $this->default ) ) {
-			$this->json['default'] = $this->default;
-		}
-		$this->json['output']  = $this->output;
-		$this->json['value']   = $this->value();
-		$this->json['choices'] = $this->choices;
-		$this->json['link']    = $this->get_link();
-		$this->json['id']      = $this->id;
-
-		$this->json['inputAttrs'] = '';
-		foreach ( $this->input_attrs as $attr => $value ) {
-			$this->json['inputAttrs'] .= $attr . '="' . esc_attr( $value ) . '" ';
-		}
-
-	}
-
-	/**
-	 * An Underscore (JS) template for this control's content (but not its container).
-	 *
-	 * Class variables for this control class are available in the `data` JS object;
-	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
-	 *
-	 * @see WP_Customize_Control::print_template()
-	 *
-	 * @access protected
-	 */
-	protected function content_template() {}
 }
