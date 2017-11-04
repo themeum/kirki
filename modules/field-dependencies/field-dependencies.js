47bc5917b5dac878a0670b9cef8ca372d8ea67b4
Aristeides Stathopoulos <aristath@gmail.com>
2017-11-04 13:43:31 +0200

    See https://github.com/aristath/kirki/pull/1515
    props @krishna19

--- modules/field-dependencies/field-dependencies.js
+++ modules/field-dependencies/field-dependencies.js
@@ -2,6 +2,9 @@
 jQuery( document ).ready( function() {
 
 	function kirkiCompareValues( value1, value2, operator, extras ) {
+		var found = false,
+		    value;
+
 		switch ( operator ) {
 			case '===':
 				return value1 === value2;
@@ -31,7 +34,17 @@ jQuery( document ).ready( function() {
 				return value2 < value1;
 			case 'contains':
 			case 'in':
-				if ( _.isObject( value2 ) ) {
+				if ( _.isArray( value2 ) ) {
+					_.each( value2, function( index, value ) {
+						if ( _.isNumeric( value ) ) {
+							value = parseInt( value, 10 );
+						}
+						if ( value1.indexOf( value ) > -1 ) {
+							found = true;
+						}
+					} );
+					return found;
+				} else if ( _.isObject( value2 ) ) {
 					if ( ! _.isUndefined( value2[ value1 ] ) ) {
 						return true;
 					}
@@ -43,7 +56,7 @@ jQuery( document ).ready( function() {
 					});
 					return window.kirkiControlDependencies[ extras[0] ][ extras[1] ];
 				} else if ( _.isString( value2 ) ) {
-					return value2.indexOf( value1 );
+					return value1.indexOf( value2 ) > -1;
 				}
 				break;
 			default:
