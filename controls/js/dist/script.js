eb6c2925a23d4e463f419749a765e05f98b5a5a1
Aristeides Stathopoulos <aristath@gmail.com>
2017-11-16 13:23:54 +0200

    grunt

--- controls/js/dist/script.js
+++ controls/js/dist/script.js
@@ -423,6 +423,97 @@ if ( _.isUndefined( window.kirkiSetSettingValue ) ) {
 		}
 	});
 })();
+;var kirki = {
+
+	/**
+	 * An object containing definitions for input fields.
+	 *
+	 * @since 3.1.0
+	 */
+	input: {
+
+		/**
+		 * Color input fields.
+		 *
+		 * @since 3.1.0
+		 */
+		color: {
+
+			/**
+			 * Get the HTML for color inputs.
+			 *
+			 * @since 3.1.0
+			 * @param {object} [data] The arguments.
+			 * @returns {string}
+			 */
+			template: function( data ) {
+
+				var html = '';
+
+				data = _.defaults( data, {
+					label: '',
+					description: '',
+					mode: 'full',
+					inputAttrs: '',
+					'data-palette': data['data-palette'] ? data['data-palette'] : true,
+					'data-default-color': data['data-default-color'] ? data['data-default-color'] : '',
+					'data-alpha': data['data-alpha'] ? data['data-alpha'] : false,
+					value: '',
+					link: ''
+				} );
+
+				html += '<label>';
+				if ( data.label ) {
+					html += '<span class="customize-control-title">' + data.label + '</span>';
+				}
+				if ( data.description ) {
+					html += '<span class="description customize-control-description">' + data.description + '</span>';
+				}
+				html += '</label>';
+				html += '<input type="text" data-type="' + data.mode + '" ' + data.inputAttrs + ' data-palette="' +  data['data-palette'] + '" data-default-color="' +  data['data-default-color'] + '" data-alpha="' + data['data-alpha'] + '" value="' + data.value + '" class="kirki-color-control" ' + data.link + '/>';
+
+				return html;
+			},
+
+			/**
+			 * Init the control.
+			 *
+			 * @since 3.1.0
+			 * @param {object} [control] The control object.
+			 * @returns {void}
+			 */
+			init: function( control ) {
+
+				var picker = control.container.find( '.kirki-color-control' ),
+				    clear;
+
+				// If we have defined any extra choices, make sure they are passed-on to Iris.
+				if ( ! _.isUndefined( control.params.choices ) ) {
+					picker.wpColorPicker( control.params.choices );
+				}
+
+				// Tweaks to make the "clear" buttons work.
+				setTimeout( function() {
+					clear = control.container.find( '.wp-picker-clear' );
+					clear.click( function() {
+						control.setting.set( '' );
+					});
+				}, 200 );
+
+				// Saves our settings to the WP API
+				picker.wpColorPicker({
+					change: function() {
+
+						// Small hack: the picker needs a small delay
+						setTimeout( function() {
+							control.setting.set( picker.val() );
+						}, 20 );
+					}
+				});
+			}
+		}
+	}
+};
 ;/* global kirkiControlLoader */
 wp.customize.controlConstructor['kirki-background'] = wp.customize.Control.extend({
 
@@ -600,36 +691,33 @@ wp.customize.controlConstructor['kirki-background'] = wp.customize.Control.exten
 	}
 });
 ;wp.customize.controlConstructor['kirki-color-palette'] = wp.customize.kirkiDynamicControl.extend({});
-;wp.customize.controlConstructor['kirki-color'] = wp.customize.kirkiDynamicControl.extend({
+;/* global kirki */
+wp.customize.controlConstructor['kirki-color'] = wp.customize.kirkiDynamicControl.extend({
 
 	initKirkiControl: function() {
 		var control = this,
-		    picker  = control.container.find( '.kirki-color-control' ),
+			data    = {
+				label: control.params.label,
+				description: control.params.description,
+				mode: control.params.mode,
+				inputAttrs: control.params.inputAttrs,
+				'data-palette': control.params.palette,
+				'data-default-color': control.params['default'],
+				'data-alpha': control.params.choices.alpha,
+				value: control.setting._value,
+				link: control.params.link
+		    },
+		    html = kirki.input.color.template( data ),
+		    picker,
 		    clear;
+console.log( control.params );
+console.log( data );
+		// Add the HTML for the control.
+		control.container.html( html );
 
-		// If we have defined any extra choices, make sure they are passed-on to Iris.
-		if ( ! _.isUndefined( control.params.choices ) ) {
-			picker.wpColorPicker( control.params.choices );
-		}
+		// Init the control.
+		kirki.input.color.init( control );
 
-		// Tweaks to make the "clear" buttons work.
-		setTimeout( function() {
-			clear = control.container.find( '.wp-picker-clear' );
-			clear.click( function() {
-				control.setting.set( '' );
-			});
-		}, 200 );
-
-		// Saves our settings to the WP API
-		picker.wpColorPicker({
-			change: function() {
-
-				// Small hack: the picker needs a small delay
-				setTimeout( function() {
-					control.setting.set( picker.val() );
-				}, 20 );
-			}
-		});
 	}
 });
 ;wp.customize.controlConstructor['kirki-dashicons'] = wp.customize.kirkiDynamicControl.extend({});
