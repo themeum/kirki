80ec109ed159adef91dd8c5cc7b371ee7704d6ac
Aristeides Stathopoulos <aristath@gmail.com>
2017-11-20 22:09:15 +0200

    Add kirki.util

--- controls/js/src/kirki.js
+++ controls/js/src/kirki.js
@@ -716,5 +716,140 @@ var kirki = {
 			}
 			wp.customize.control( foundNode ).setting.set( value );
 		}
+	},
+
+	/**
+	 * A collection of utility methods.
+	 *
+	 * @since 3.0.17
+	 */
+	util: {
+
+		/**
+		 * A collection of utility methods for webfonts.
+		 *
+		 * @since 3.0.17
+		 */
+		webfonts: {
+
+			/**
+			 * Google-fonts related methods.
+			 *
+			 * @since 3.0.17
+			 */
+			google: {
+
+				/**
+				 * An object containing all Google fonts.
+				 *
+				 * to set this call this.setFonts();
+				 *
+				 * @since 3.0.17
+				 */
+				fonts: {},
+
+				/**
+				 * Init for google-fonts.
+				 *
+				 * @since 3.0.17
+				 * @returns {void}
+				 */
+				initialize: function() {
+					var self = this;
+
+					self.setFonts();
+				},
+
+				/**
+				 * Set fonts in this.fonts
+				 *
+				 * @since 3.0.17
+				 * @returns {void}
+				 */
+				setFonts: function() {
+					var self = this,
+					    fonts;
+
+					// No need to run if we already have the fonts.
+					if ( ! _.isEmpty( self.fonts ) ) {
+						return;
+					}
+
+					// Make an AJAX call to set the fonts object.
+					jQuery.post( ajaxurl, { 'action': 'kirki_fonts_google_all_get' }, function( response ) {
+
+						// Get fonts from the JSON array.
+						fonts = JSON.parse( response );
+
+						_.each( fonts.items, function( font ) {
+							self.fonts[ font.family ] = font;
+						} );
+					} );
+				},
+
+				/**
+				 * Gets all properties of a font-family.
+				 *
+				 * @since 3.0.17
+				 * @param {string} family - The font-family we're interested in.
+				 * @returns {Object}
+				 */
+				getFont: function( family ) {
+					var self = this;
+
+					return _.isUndefined( self.fonts[ family ] ) ? false : self.fonts[ family ];
+				},
+
+				/**
+				 * Gets the variants for a font-family.
+				 *
+				 * @since 3.0.17
+				 * @param {string} family - The font-family we're interested in.
+				 * @returns {Object}
+				 */
+				getVariants: function( family ) {
+					var self = this,
+					    font = self.getFont( family );
+
+					// Early exit if font was not found.
+					if ( ! font ) {
+						return false;
+					}
+
+					// Early exit if font doesn't have variants.
+					if ( _.isUndefined( font.variants ) ) {
+						return false;
+					}
+
+					// Return the variants.
+					return font.variants;
+				},
+
+				/**
+				 * Get the subsets for a font-family.
+				 *
+				 * @since 3.0.17
+				 * @param {string} family - The font-family we're interested in.
+				 * @returns {Object}
+				 */
+				getSubsets: function( family ) {
+					var self = this,
+					    font = self.getFont( family );
+
+					// Early exit if font was not found.
+					if ( ! font ) {
+						return false;
+					}
+
+					// Early exit if font doesn't have subsets.
+					if ( _.isUndefined( font.subsets ) ) {
+						return false;
+					}
+
+					// Return the variants.
+					return font.subsets;
+				}
+			}
+		}
 	}
 };
