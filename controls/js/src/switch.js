57127e6863140847b907f2a68876b85911a4f6e1
Aristeides Stathopoulos <aristath@gmail.com>
2017-11-26 23:26:00 +0200

    Simplify switches styling

--- controls/js/src/switch.js
+++ controls/js/src/switch.js
@@ -5,15 +5,7 @@ wp.customize.controlConstructor['kirki-switch'] = wp.customize.kirkiDynamicContr
 		'use strict';
 
 		var control       = this,
-		    checkboxValue = control.setting._value,
-		    on            = jQuery( control.container.find( '.switch-on' ) ),
-		    off           = jQuery( control.container.find( '.switch-off' ) );
-
-		// CSS modifications depending on label sizes.
-		jQuery( control.container.find( '.switch label ' ) ).css( 'width', ( on.width() + off.width() + 40 ) + 'px' );
-		jQuery( '#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) ).append(
-			'<style>#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) + ' .switch label:after{width:' + ( on.width() + 13 ) + 'px;}#customize-control-' + control.id.replace( '[', '-' ).replace( ']', '' ) + ' .switch input:checked + label:after{left:' + ( on.width() + 22 ) + 'px;width:' + ( off.width() + 13 ) + 'px;}</style>'
-		);
+		    checkboxValue = control.setting._value;
 
 		// Save the value
 		this.container.on( 'change', 'input', function() {
