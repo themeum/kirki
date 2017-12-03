d69590bc455776275b5f56f916f4820ce4ccc849
Aristeides Stathopoulos <aristath@gmail.com>
2017-12-03 18:42:11 +0200

    ugh... where is my mind

--- core/class-kirki.php
+++ core/class-kirki.php
@@ -259,7 +259,7 @@ class Kirki extends Kirki_Init {
 	 * @since 3.0.17
 	 * @param string $id The field ID.
 	 */
-	public static function remove_field( $id ) {
+	public static function remove control( $id ) {
 		if ( ! in_array( $id, self::$controls_to_remove, true ) ) {
 			self::$controls_to_remove[] = $id;
 		}
