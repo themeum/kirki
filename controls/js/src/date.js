31b2503712ac939ae366e9a6b4c32519038912e9
Aristeides Stathopoulos <aristath@gmail.com>
2018-01-20 17:36:34 +0200

    fixes #1750

--- controls/js/src/date.js
+++ controls/js/src/date.js
@@ -2,16 +2,22 @@ wp.customize.controlConstructor['kirki-date'] = wp.customize.kirkiDynamicControl
 
 	initKirkiControl: function() {
 
-		var control  = this;
+		var control = this,
+		    includeTime;
 
 		// Only add in WP 4.9+.
 		if ( _.isUndefined( wp.customize.DateTimeControl ) ) {
 			return;
 		}
 
+		// Do we want to include time?
+		includeTime = ( control.params && control.params.chices && control.params.choices.include_time );
+		includeTime = ( true === includeTime );
+
 		// New method for the DateTime control.
 		wp.customize.control.add( new wp.customize.DateTimeControl( control.id, {
 			section: control.params.section,
+			includeTime: includeTime,
 			priority: control.params.priority,
 			label: control.params.label,
 			description: control.params.description,
