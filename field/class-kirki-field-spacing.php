2c9ce045a9466d8dbb29bc4f2ec50eaade168c73
Aristeides Stathopoulos <aristath@gmail.com>
2018-01-20 16:58:29 +0200

    Simplify dimension field
    See #1735

--- field/class-kirki-field-spacing.php
+++ field/class-kirki-field-spacing.php
@@ -12,51 +12,7 @@
 /**
  * Field overrides.
  */
-class Kirki_Field_Spacing extends Kirki_Field_Number {
-
-	/**
-	 * Sets the control type.
-	 *
-	 * @access protected
-	 */
-	protected function set_type() {
-
-		$this->type = 'kirki-dimensions';
-
-	}
-
-	/**
-	 * Sets the $sanitize_callback.
-	 *
-	 * @access protected
-	 */
-	protected function set_sanitize_callback() {
-
-		// If a custom sanitize_callback has been defined,
-		// then we don't need to proceed any further.
-		if ( ! empty( $this->sanitize_callback ) ) {
-			return;
-		}
-		$this->sanitize_callback = array( $this, 'sanitize_value' );
-
-	}
-
-	/**
-	 * Sanitizes the value.
-	 *
-	 * @access public
-	 * @param array $value The value.
-	 * @return array
-	 */
-	public function sanitize_value( $value ) {
-
-		// Sanitize each sub-value separately.
-		foreach ( $value as $key => $sub_value ) {
-			$value[ $key ] = Kirki_Sanitize_Values::css_dimension( $sub_value );
-		}
-		return $value;
-
-	}
+class Kirki_Field_Spacing extends Kirki_Field_Dimensions {
 
 	/**
 	 * Set the choices.
