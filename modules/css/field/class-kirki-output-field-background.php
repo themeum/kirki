95ca010588878363a7d2042f09428bae260cb602
Aristeides Stathopoulos <aristath@gmail.com>
2018-06-24 03:01:20 +0300

    fix: undefined index PHP notice for background-color

--- modules/css/field/class-kirki-output-field-background.php
+++ modules/css/field/class-kirki-output-field-background.php
@@ -36,7 +36,7 @@ class Kirki_Output_Field_Background extends Kirki_Output {
 		foreach ( array( 'background-image', 'background-color', 'background-repeat', 'background-position', 'background-size', 'background-attachment' ) as $property ) {
 
 			// See https://github.com/aristath/kirki/issues/1808.
-			if ( 'background-color' === $property && ( ! isset( $value['background-image'] ) || empty( $value['background-image'] ) ) ) {
+			if ( 'background-color' === $property && isset( $value['background-color'] ) && ( ! isset( $value['background-image'] ) || empty( $value['background-image'] ) ) ) {
 				$this->styles[ $output['media_query'] ][ $output['element'] ]['background'] = $output['prefix'] . $this->process_property_value( $property, $value[ $property ] ) . $output['suffix'];
 			}
 
