8f4302aaed39df37b4802bb5f2ff934f726aa77d
aristath <aristath@gmail.com>
2018-08-25 16:24:18 +0300

    Another test

--- tests/test-kirki-field.php
+++ tests/test-kirki-field.php
@@ -56,4 +56,31 @@ class Test_Kirki_Field extends WP_UnitTestCase {
 
 		$this->assertTrue( 'foo' === Kirki::$fields['test1']['capability'] );
 	}
+
+	public function test_set_active_callback() {
+
+		Kirki::add_field( 'global', array(
+			'settings'        => 'test1',
+			'type'            => 'text',
+			'active_callback' => array(
+				array(
+					'setting'  => 'foo',
+					'operator' => '===',
+					'value'    => 'bar',
+				),
+				'__return_true',
+			),
+		) );
+
+		$this->assertEquals(
+			array(
+				array(
+					'setting'  => 'foo',
+					'operator' => '===',
+					'value'    => 'bar',
+				),
+			),
+			Kirki::$fields['test1']['required'] 
+		);
+	}
 }
