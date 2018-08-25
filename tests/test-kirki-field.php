440c37974eff2a07ec3bf7686eced05d524b3fa2
aristath <aristath@gmail.com>
2018-08-25 16:17:24 +0300

    A couple more phpunits

--- tests/test-kirki-field.php
+++ tests/test-kirki-field.php
@@ -17,4 +17,43 @@ class Test_Kirki_Field extends WP_UnitTestCase {
 		$this->assertTrue( 'global' === Kirki::$fields['test1']['kirki_config'] );
 		$this->assertTrue( 'my_test_config' === Kirki::$fields['test2']['kirki_config'] );
 	}
+
+	public function test_set_input_attrs() {
+
+		Kirki::add_field( 'global', array(
+			'settings'    => 'test1',
+			'type'        => 'text',
+			'input_attrs' => 'test'
+		) );
+
+		$this->assertTrue( array() === Kirki::$fields['test1']['input_attrs'] );
+
+		Kirki::add_field( 'global', array(
+			'settings'    => 'test1',
+			'type'        => 'text',
+			'input_attrs' => array(
+				'foo' => 'bar',
+			),
+		) );
+
+		$this->assertTrue( array( 'foo' => 'bar' ) === Kirki::$fields['test1']['input_attrs'] );
+	}
+
+	public function test_set_capability() {
+
+		Kirki::add_field( 'global', array(
+			'settings' => 'test1',
+			'type'     => 'text',
+		) );
+
+		$this->assertTrue( 'edit_theme_options' === Kirki::$fields['test1']['capability'] );
+
+		Kirki::add_field( 'global', array(
+			'settings'   => 'test1',
+			'type'       => 'text',
+			'capability' => 'foo',
+		) );
+
+		$this->assertTrue( 'foo' === Kirki::$fields['test1']['capability'] );
+	}
 }
