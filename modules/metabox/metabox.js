ed7b6cdf43fca190484564c96b2b01a934524fd6
Nathaniel Glover <steelbladestudios@live.com>
2019-01-27 01:24:55 -0600

    file rename

---
+++ modules/metabox/metabox.js
@@ -0,0 +1,21 @@
+var kikriMetaboxControl = {
+	
+	params: null,
+	container: null,
+	
+	initKirkiMetabox: function(){
+	},
+	
+	set: function( value ) {
+		
+	}
+};
+
+( function() {
+	'use strict';
+
+	jQuery( document ).ready( function ( e )
+	{
+		
+	});
+} );
