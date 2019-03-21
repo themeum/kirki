2fd1bf53f482b46515318f112ca2d4a83dd4081e
Ari <aristath@gmail.com>
2019-03-21 17:34:50 +0200

    improve color controls styling

--- controls/views/color.php
+++ controls/views/color.php
@@ -24,7 +24,7 @@ data = _.defaults( data, {
 } );
 #>
 
-<div class="kirki-input-container" data-id="{{ data['data-id'] }}">
+<div class="kirki-input-container" data-id="{{ data['data-id'] }}" data-has-alpha="{{ data['data-alpha'] }}">
 	<label>
 		<# if ( data.label ) { #>
 			<span class="customize-control-title">{{{ data.label }}}</span>
