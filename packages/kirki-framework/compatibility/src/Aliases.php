0b6aaac449bfb36178a6a07d2bbf3024734a043e
Ari <aristath@gmail.com>
2019-05-12 12:25:01 +0300

    cs

--- packages/kirki-framework/compatibility/src/Aliases.php
+++ packages/kirki-framework/compatibility/src/Aliases.php
@@ -144,7 +144,7 @@ class Aliases {
 			[ 'Kirki\Modules\Custom_Sections\Section_Nested', 'Kirki_Sections_Nested_Section' ],
 			[ 'Kirki\Modules\Custom_Sections\Section_Link', 'Kirki_Sections_Link_Section' ],
 			[ 'Kirki\Modules\Custom_Panels\Panel_Nested', 'Kirki_Panels_Nested_Panel' ],
-		]
+		],
 	];
 
 	/**
