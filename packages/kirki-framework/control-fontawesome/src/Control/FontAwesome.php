247f5a49cb5a946e57a124b342ee5900d1ecb989
Ari <aristath@gmail.com>
2019-06-02 14:52:10 +0300

    dummy - test pre-push hook

--- packages/kirki-framework/control-fontawesome/src/Control/FontAwesome.php
+++ packages/kirki-framework/control-fontawesome/src/Control/FontAwesome.php
@@ -57,9 +57,9 @@ class FontAwesome extends Select {
 		ob_start();
 		$json_path = wp_normalize_path( dirname( __DIR__ ) . '/assets/json/fontawesome.json' );
 		include $json_path; // phpcs:ignore WPThemeReview.CoreFunctionality.FileInclude
-		$font_awesome_json = ob_get_clean();
+		$json = ob_get_clean();
 
-		wp_localize_script( 'kirki-control-fontawesome', 'fontAwesomeJSON', $font_awesome_json );
+		wp_localize_script( 'kirki-control-fontawesome', 'fontAwesomeJSON', $json );
 	}
 
 	/**
