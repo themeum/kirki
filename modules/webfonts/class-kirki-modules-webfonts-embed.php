d2588862255eca5e92405f2927a427629075bfa4
Ari <aristath@gmail.com>
2019-06-16 12:51:29 +0300

    See #2177

--- modules/webfonts/class-kirki-modules-webfonts-embed.php
+++ modules/webfonts/class-kirki-modules-webfonts-embed.php
@@ -146,7 +146,7 @@ final class Kirki_Modules_Webfonts_Embed {
 			$url     = "https://fonts.googleapis.com/css?family={$family}:{$weights}&subset=cyrillic,cyrillic-ext,devanagari,greek,greek-ext,khmer,latin,latin-ext,vietnamese,hebrew,arabic,bengali,gujarati,tamil,telugu,thai";
 
 			$transient_id = 'kirki_gfonts_' . md5( $url );
-			$contents     = get_site_transient( $transient_id );
+			$contents     = get_transient( $transient_id );
 
 			/**
 			 * Reset the cache if we're debugging, or if we force-reset caches using action=kirki-reset-cache in the URL.
@@ -204,7 +204,7 @@ final class Kirki_Modules_Webfonts_Embed {
 					);
 
 					// Set the transient for a day.
-					set_site_transient( $transient_id, $contents, DAY_IN_SECONDS );
+					set_transient( $transient_id, $contents, DAY_IN_SECONDS );
 				}
 			}
 			if ( $contents ) {
