83e13e436460718addd832a78a2f17e729755cc7
David Vongries <connect@mapsteps.com>
2020-05-19 18:35:52 +0200

    version bump

--- kirki.php
+++ kirki.php
@@ -5,7 +5,7 @@
  * Description:   The Ultimate WordPress Customizer Framework
  * Author:        David Vongries
  * Author URI:    https://wp-pagebuilderframework.com/
- * Version:       3.1.2
+ * Version:       3.1.3
  * Text Domain:   kirki
  * Requires WP:   4.9
  * Requires PHP:  5.3
@@ -40,7 +40,7 @@ if ( ! defined( 'KIRKI_PLUGIN_FILE' ) ) {
 
 // Define the KIRKI_VERSION constant.
 if ( ! defined( 'KIRKI_VERSION' ) ) {
-	define( 'KIRKI_VERSION', '3.1.2' );
+	define( 'KIRKI_VERSION', '3.1.3' );
 }
 
 // Make sure the path is properly set.
