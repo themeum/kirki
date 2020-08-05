d3cb2aa627176c4b3be332fc0f15cd216f660be6
mapsteps <connect@mapsteps.com>
2020-08-05 14:56:23 +0200

    add wp-i18n dependency to alpha color picker

--- controls/php/class-kirki-control-base.php
+++ controls/php/class-kirki-control-base.php
@@ -100,7 +100,7 @@ class Kirki_Control_Base extends WP_Customize_Control {
 		$kirki_url = trailingslashit( Kirki::$url );
 
 		// Enqueue ColorPicker.
-		wp_enqueue_script( 'wp-color-picker-alpha', trailingslashit( Kirki::$url ) . 'assets/vendor/wp-color-picker-alpha/wp-color-picker-alpha.js', array( 'wp-color-picker' ), KIRKI_VERSION, true );
+		wp_enqueue_script( 'wp-color-picker-alpha', trailingslashit( Kirki::$url ) . 'assets/vendor/wp-color-picker-alpha/wp-color-picker-alpha.js', array( 'wp-color-picker', 'wp-i18n' ), KIRKI_VERSION, true );
 		wp_enqueue_style( 'wp-color-picker' );
 
 		// Enqueue selectWoo.
