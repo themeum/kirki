27f5c747c16c6d3b8c7de0afa74051dd3065e39f
mapsteps <connect@mapsteps.com>
2023-02-03 14:02:46 +0100

    complete sidebar

--- packages/settings/templates/metaboxes/documentation.php
+++ packages/settings/templates/metaboxes/documentation.php
@@ -10,13 +10,30 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 <div class="heatbox">
 	<h2>
+		<?php _e( 'Why do I see this?', 'kirki' ); ?>
+	</h2>
+	<div class="heatbox-content">
+		<p class="description">
+			<?php _e( 'Kirki is a key component of the WordPress theme you have installed.', 'kirki' ); ?>
+		</p>
+		<p class="description">
+			<?php _e( 'It provides advanced customization options within the WordPress Customizer, allowing you to easily modify the appearance of your site.', 'kirki' ); ?>
+		</p>
+	</div>
+</div>
+
+<div class="heatbox">
+	<h2>
 		<?php _e( 'Documentation', 'kirki' ); ?>
 	</h2>
 	<div class="heatbox-content">
 		<p class="description">
-			<?php _e( 'Not sure how something works? Our extensive Documentation is a great place to get started with Kirki.', 'kirki' ); ?>
+			<?php _e( 'For more information, please refer to your theme\'s documentation.', 'kirki' ); ?>
 		</p>
-		<a href="https://kirki.org/docs/" target="_blank" class="button">
+		<p>
+			<?php _e( 'The following article provides a detailed explanation of the Clear Font Cache option in Kirki.', 'kirki' ); ?>
+		</p>
+		<a href="https://kirki.org/docs/modules/settings-page/" target="_blank" class="button">
 			<?php _e( 'Documentation', 'kirki' ); ?>
 		</a>
 	</div>
