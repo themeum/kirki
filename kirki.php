edf27a0b93348fad58c7356f4a38958bb3bb096f
Md Rashed Hossain <wprashed@icloud.com>
2024-03-21 15:05:28 +0600

    version-update

--- kirki.php
+++ kirki.php
@@ -5,7 +5,7 @@
  * Description: The Ultimate WordPress Customizer Framework
  * Author: Themeum
  * Author URI: https://themeum.com
- * Version: 5.0.0
+ * Version: 5.1.0
  * Text Domain: kirki
  * Requires at least: 5.2
  * Requires PHP: 7.1
