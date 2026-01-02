ff56371d090bf22e00ca89001a465a396fbd0bba
Sadman Soumique <saadman.ofc@gmail.com>
2026-01-02 11:36:18 +0600

    Refactor: Transition kirki-image control to vanilla JavaScript for event handling, improving performance and reducing jQuery dependency. Update element selection methods and enhance code organization with helper functions for better readability.

--- customizer/packages/control/image/src/control.js
+++ customizer/packages/control/image/src/control.js
@@ -1,14 +1,40 @@
 wp.customize.controlConstructor['kirki-image'] = wp.customize.kirkiDynamicControl.extend( {
 
 	initKirkiControl: function( control ) {
-		var value, saveAs, preview, previewImage, removeButton, defaultButton;
+		var value, saveAs, preview, previewImage, removeButton, defaultButton, container;
 		control       = control || this;
+		container     = control.container[0] || control.container;
 		value         = control.setting._value;
 		saveAs        = ( ! _.isUndefined( control.params.choices ) && ! _.isUndefined( control.params.choices.save_as ) ) ? control.params.choices.save_as : 'url';
-		preview       = control.container.find( '.placeholder, .thumbnail' );
+		preview       = container.querySelector( '.placeholder, .thumbnail' );
 		previewImage  = ( 'array' === saveAs ) ? value.url : value;
-		removeButton  = control.container.find( '.image-upload-remove-button' );
-		defaultButton = control.container.find( '.image-default-button' );
+		removeButton  = container.querySelector( '.image-upload-remove-button' );
+		defaultButton = container.querySelector( '.image-default-button' );
+
+		// Helper function to update preview element.
+		var updatePreview = function( element, imageUrl, className ) {
+			if ( element ) {
+				element.className = className;
+				element.innerHTML = '<img src="' + imageUrl + '" alt="" />';
+			}
+		};
+
+		// Helper function to show/hide elements (matching jQuery .show()/.hide() behavior).
+		var toggleElement = function( element, show ) {
+			if ( element ) {
+				if ( show ) {
+					// Remove hidden class if present (from template).
+					element.classList.remove( 'hidden' );
+					// Remove inline display style to show element.
+					element.style.display = '';
+				} else {
+					// Add hidden class for consistency with template.
+					element.classList.add( 'hidden' );
+					// Also set inline display to ensure it's hidden.
+					element.style.display = 'none';
+				}
+			}
+		};
 
 		// Make sure value is properly formatted.
 		value = ( 'array' === saveAs && _.isString( value ) ) ? { url: value } : value;
@@ -18,110 +44,130 @@ wp.customize.controlConstructor['kirki-image'] = wp.customize.kirkiDynamicContro
 			wp.media.attachment( value ).fetch().then( function() {
 				setTimeout( function() {
 					var url = wp.media.attachment( value ).get( 'url' );
-					preview.removeClass().addClass( 'thumbnail thumbnail-image' ).html( '<img src="' + url + '" alt="" />' );
+					updatePreview( preview, url, 'thumbnail thumbnail-image' );
 				}, 700 );
 			} );
 		}
 
 		// If value is not empty, hide the "default" button.
 		if ( ( 'url' === saveAs && '' !== value ) || ( 'array' === saveAs && ! _.isUndefined( value.url ) && '' !== value.url ) ) {
-			control.container.find( 'image-default-button' ).hide();
+			var defaultBtn = container.querySelector( '.image-default-button' );
+			toggleElement( defaultBtn, false );
 		}
 
 		// If value is empty, hide the "remove" button.
 		if ( ( 'url' === saveAs && '' === value ) || ( 'array' === saveAs && ( _.isUndefined( value.url ) || '' === value.url ) ) ) {
-			removeButton.hide();
+			toggleElement( removeButton, false );
 		}
 
 		// If value is default, hide the default button.
 		if ( value === control.params.default ) {
-			control.container.find( 'image-default-button' ).hide();
+			var defaultBtn = container.querySelector( '.image-default-button' );
+			toggleElement( defaultBtn, false );
 		}
 
 		if ( '' !== previewImage ) {
-			preview.removeClass().addClass( 'thumbnail thumbnail-image' ).html( '<img src="' + previewImage + '" alt="" />' );
+			updatePreview( preview, previewImage, 'thumbnail thumbnail-image' );
 		}
 
-		control.container.on( 'click', '.image-upload-button', function( e ) {
-			var image = wp.media( { multiple: false } ).open().on( 'select', function() {
+		// Handle image upload button click using event delegation.
+		container.addEventListener( 'click', function( e ) {
+			var uploadButton = e.target.closest( '.image-upload-button' );
+			if ( uploadButton ) {
+				e.preventDefault();
 
-				// This will return the selected image from the Media Uploader, the result is an object.
-				var uploadedImage = image.state().get( 'selection' ).first(),
-					jsonImg       = uploadedImage.toJSON();
+				var image = wp.media( { multiple: false } ).open().on( 'select', function() {
 
-				previewImage  = jsonImg.url;
+					// This will return the selected image from the Media Uploader, the result is an object.
+					var uploadedImage = image.state().get( 'selection' ).first(),
+						jsonImg       = uploadedImage.toJSON();
 
-				if ( ! _.isUndefined( jsonImg.sizes ) ) {
-					previewImage = jsonImg.sizes.full.url;
-					if ( ! _.isUndefined( jsonImg.sizes.medium ) ) {
-						previewImage = jsonImg.sizes.medium.url;
-					} else if ( ! _.isUndefined( jsonImg.sizes.thumbnail ) ) {
-						previewImage = jsonImg.sizes.thumbnail.url;
+					previewImage  = jsonImg.url;
+
+					if ( ! _.isUndefined( jsonImg.sizes ) ) {
+						previewImage = jsonImg.sizes.full.url;
+						if ( ! _.isUndefined( jsonImg.sizes.medium ) ) {
+							previewImage = jsonImg.sizes.medium.url;
+						} else if ( ! _.isUndefined( jsonImg.sizes.thumbnail ) ) {
+							previewImage = jsonImg.sizes.thumbnail.url;
+						}
+					}
+
+					if ( 'array' === saveAs ) {
+						control.setting.set( {
+							id: jsonImg.id,
+							url: !_.isUndefined(jsonImg.sizes)
+								? jsonImg.sizes.full.url
+								: jsonImg.url,
+							width: jsonImg.width,
+							height: jsonImg.height,
+						} );
+					} else if ( 'id' === saveAs ) {
+						control.setting.set( jsonImg.id );
+					} else {
+						control.setting.set( ( ! _.isUndefined( jsonImg.sizes ) ) ? jsonImg.sizes.full.url : jsonImg.url );
+					}
+
+					// Update preview after setting value.
+					preview = container.querySelector( '.placeholder, .thumbnail' );
+					removeButton = container.querySelector( '.image-upload-remove-button' );
+					defaultButton = container.querySelector( '.image-default-button' );
+
+					if ( preview ) {
+						updatePreview( preview, previewImage, 'thumbnail thumbnail-image' );
+					}
+					if ( removeButton ) {
+						toggleElement( removeButton, true );
+						toggleElement( defaultButton, false );
+					}
+				} );
+			}
+		} );
+
+		// Handle remove button click using event delegation.
+		container.addEventListener( 'click', function( e ) {
+			var removeBtn = e.target.closest( '.image-upload-remove-button' );
+			if ( removeBtn ) {
+				e.preventDefault();
+
+				control.setting.set( '' );
+
+				preview       = container.querySelector( '.placeholder, .thumbnail' );
+				removeButton  = container.querySelector( '.image-upload-remove-button' );
+				defaultButton = container.querySelector( '.image-default-button' );
+
+				if ( preview ) {
+					preview.className = 'placeholder';
+					preview.innerHTML = wp.i18n.__( 'No image selected', 'kirki' );
+				}
+				if ( removeButton ) {
+					toggleElement( removeButton, false );
+					if ( defaultButton && defaultButton.classList.contains( 'button' ) ) {
+						toggleElement( defaultButton, true );
 					}
 				}
-
-				if ( 'array' === saveAs ) {
-					control.setting.set( {
-						id: jsonImg.id,
-						url: !_.isUndefined(jsonImg.sizes)
-							? jsonImg.sizes.full.url
-							: jsonImg.url,
-						width: jsonImg.width,
-						height: jsonImg.height,
-					} );
-				} else if ( 'id' === saveAs ) {
-					control.setting.set( jsonImg.id );
-				} else {
-					control.setting.set( ( ! _.isUndefined( jsonImg.sizes ) ) ? jsonImg.sizes.full.url : jsonImg.url );
-				}
-
-				if ( preview.length ) {
-					preview.removeClass().addClass( 'thumbnail thumbnail-image' ).html( '<img src="' + previewImage + '" alt="" />' );
-				}
-				if ( removeButton.length ) {
-					removeButton.show();
-					defaultButton.hide();
-				}
-			} );
-
-			e.preventDefault();
-		} );
-
-		control.container.on( 'click', '.image-upload-remove-button', function( e ) {
-			e.preventDefault();
-
-			control.setting.set( '' );
-
-			preview       = control.container.find( '.placeholder, .thumbnail' );
-			removeButton  = control.container.find( '.image-upload-remove-button' );
-			defaultButton = control.container.find( '.image-default-button' );
-
-			if ( preview.length ) {
-				preview.removeClass().addClass( 'placeholder' ).html( wp.i18n.__( 'No image selected', 'kirki' ) );
-			}
-			if ( removeButton.length ) {
-				removeButton.hide();
-				if ( jQuery( defaultButton ).hasClass( 'button' ) ) {
-					defaultButton.show();
-				}
 			}
 		} );
 
-		control.container.on( 'click', '.image-default-button', function( e ) {
-			e.preventDefault();
+		// Handle default button click using event delegation.
+		container.addEventListener( 'click', function( e ) {
+			var defaultBtn = e.target.closest( '.image-default-button' );
+			if ( defaultBtn ) {
+				e.preventDefault();
 
-			control.setting.set( control.params.default );
+				control.setting.set( control.params.default );
 
-			preview       = control.container.find( '.placeholder, .thumbnail' );
-			removeButton  = control.container.find( '.image-upload-remove-button' );
-			defaultButton = control.container.find( '.image-default-button' );
+				preview       = container.querySelector( '.placeholder, .thumbnail' );
+				removeButton  = container.querySelector( '.image-upload-remove-button' );
+				defaultButton = container.querySelector( '.image-default-button' );
 
-			if ( preview.length ) {
-				preview.removeClass().addClass( 'thumbnail thumbnail-image' ).html( '<img src="' + control.params.default + '" alt="" />' );
-			}
-			if ( removeButton.length ) {
-				removeButton.show();
-				defaultButton.hide();
+				if ( preview ) {
+					updatePreview( preview, control.params.default, 'thumbnail thumbnail-image' );
+				}
+				if ( removeButton ) {
+					toggleElement( removeButton, true );
+					toggleElement( defaultButton, false );
+				}
 			}
 		} );
 	}
